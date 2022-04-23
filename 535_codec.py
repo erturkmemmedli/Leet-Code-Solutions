ab6eb90a6aba8cdd11d56735a4c3148bbd72e0a0
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-04-23 15:57:48 +0400

    Create 535_codec.py

---
+++ 535_codec.py
@@ -0,0 +1,11 @@
+class Codec:
+    
+    dictionary = {}
+
+    def encode(self, longUrl):
+        hash_val = str(hash(longUrl))
+        self.dictionary[hash_val] = longUrl
+        return hash_val
+        
+    def decode(self, shortUrl):
+        return self.dictionary[shortUrl]

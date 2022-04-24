472ae5e021cc62f2266d6c18c9a30425016c82a0
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-04-24 15:39:21 +0400

    Update 3_length_of_longest_substring.py

--- 3_length_of_longest_substring.py
+++ 3_length_of_longest_substring.py
@@ -6,14 +6,9 @@ class Solution(object):
         end = 0
         dictionary = {}
         for i in range(len(s)):
-            if s[i] not in dictionary:
-                dictionary[s[i]] = end
-                end += 1
-                maximum = max(maximum, end - start)
-            else:
-                if dictionary[s[i]] + 1 > start:
-                    start = dictionary[s[i]] + 1
-                dictionary[s[i]] = end
-                end += 1
-                maximum = max(maximum, end - start)
+            if s[i] in dictionary and dictionary[s[i]] + 1 > start:
+                start = dictionary[s[i]] + 1
+            dictionary[s[i]] = end
+            end += 1
+            maximum = max(maximum, end - start)
         return maximum

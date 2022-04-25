970e20946127f7be334bee818348b7d4c86e9be1
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-04-25 11:25:15 +0400

    Add files via upload

---
+++ 1396_underground_system.py
@@ -0,0 +1,20 @@
+class UndergroundSystem(object):
+    def __init__(self):
+        self.travel = {}
+        self.customer = {}
+
+    def checkIn(self, id, stationName, t):
+        self.customer[id] = (stationName, t)  
+
+    def checkOut(self, id, stationName, t):
+        start = self.customer[id][0]
+        time = t - self.customer[id][1]
+        if (start, stationName) not in self.travel:
+            self.travel[(start, stationName)] = (time, 1)
+        else:
+            period = self.travel[(start, stationName)][1]
+            total = self.travel[(start, stationName)][0] * period
+            self.travel[(start, stationName)] = ((total + time) / (period + 1), period + 1)
+
+    def getAverageTime(self, startStation, endStation):
+        return self.travel[(startStation, endStation)][0]

538996276d8be6d6abeb2dfd92b038c2d3f417d3
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-04-25 12:12:58 +0400

    Update 0002_add_two_numbers.py

--- 0002_add_two_numbers.py
+++ 0002_add_two_numbers.py
@@ -1,7 +1,8 @@
-class ListNode:
-    def __init__(self, val = 0, next = None):
-        self.val = val
-        self.next = next
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
 class Solution(object):
     def __init__(self):

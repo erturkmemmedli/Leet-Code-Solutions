f2c8bdf16dc8f367ab7ea01b0bfe2d884bcb0c24
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-04-25 12:16:23 +0400

    Update 0706_my_hash_map.py

--- 0706_my_hash_map.py
+++ 0706_my_hash_map.py
@@ -65,3 +65,9 @@ class MyHashMap(object):
             for i in temp:
                 if i != None or i != -1:
                     self.put(i[0], i[1])
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)

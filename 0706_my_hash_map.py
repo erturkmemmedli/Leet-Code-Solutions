e9fbb523d12a62c9f3cffa89dcf383ff70b5803b
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-04-27 10:06:07 +0400

    Update 0706_my_hash_map.py

--- 0706_my_hash_map.py
+++ 0706_my_hash_map.py
@@ -71,3 +71,22 @@ class MyHashMap(object):
 # obj.put(key,value)
 # param_2 = obj.get(key)
 # obj.remove(key)
+
+# Alternative solution
+
+class MyHashMap1(object):
+
+    def __init__(self):
+        self.map = [None] * (10 ** 6 + 1)
+
+    def put(self, key, value):
+        self.map[key] = value
+
+    def get(self, key):
+        if self.map[key] is None:
+            return -1
+        else:
+            return self.map[key]
+
+    def remove(self, key):
+        self.map[key] = None

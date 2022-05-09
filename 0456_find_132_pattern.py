55d6dddbb031619b4d3ea3dfbd8b3499b0407db0
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-09 14:44:07 +0400

    Create 0456_find_132_pattern.py

---
+++ 0456_find_132_pattern.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def find132pattern(self, nums):
+        stack = []
+        k = - float('inf')
+        for i in range(len(nums)-1, -1 , -1):
+            if nums[i] < k:
+                return True
+            while stack != [] and stack[-1] < nums[i]:
+                k = stack.pop()
+            stack.append(nums[i])
+        return False

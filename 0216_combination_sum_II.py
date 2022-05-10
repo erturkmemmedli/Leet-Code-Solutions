17065c030b0c958ffe4ee1902534ab441d374eb6
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-10 17:09:56 +0400

    Create 0216_combination_sum_II.py

---
+++ 0216_combination_sum_II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def combinationSum3(self, k, n):
+        output = []
+        self.dfs(range(1,10), output, [], k, n)
+        return output
+        
+    def dfs(self, nums, output, temp, k, n):
+        if k < 0 or n < 0:
+            return
+        if k == 0 and n == 0:
+            output.append(temp)
+        for i in range(len(nums)):
+            self.dfs(nums[i+1:], output, temp + [nums[i]], k - 1, n - nums[i])

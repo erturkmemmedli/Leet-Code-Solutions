962babb40bf87fffaa3acacd52da859fa796e4a3
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-12 10:51:53 +0400

    Update 0047_permute_unique.py

--- 0047_permute_unique.py
+++ 0047_permute_unique.py
@@ -1,12 +1,11 @@
 class Solution:
     def permuteUnique(self, nums):
-        length = len(nums)
         output = set()
-        self.dfs(nums, output, [], length)
+        self.dfs(nums, output, [])
         return output
     
-    def dfs(self, nums, output, path, length):
+    def dfs(self, nums, output, path):
         if not nums:
             output.add(tuple(path))
         for i in range(len(nums)):
-            self.dfs(nums[i+1:] + nums[:i], output, path + [nums[i]], length)
+            self.dfs(nums[i+1:] + nums[:i], output, path + [nums[i]])

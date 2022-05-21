a9d5be1d4156d0a07bc0f7492e7bb5059b931824
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-21 17:26:04 +0400

    Update 0322_coin_change.py

--- 0322_coin_change.py
+++ 0322_coin_change.py
@@ -8,3 +8,17 @@ class Solution:
                     minimum = min(minimum, dp[i - c] + 1)
             dp[i] = minimum
         return dp[-1] if dp[-1] != float('inf') else -1
+
+# Alternative solution
+
+class Solution1:
+    def coinChange(self, coins, amount):
+        MinimumCoins = [0] * (amount + 1)
+        for i in range(1, amount + 1):
+            MinimumCoins[i] = float('inf')
+            for j in coins:
+                if i >= j:
+                    min_value = MinimumCoins[i - j] + 1
+                    if min_value < MinimumCoins[i]:
+                        MinimumCoins[i] = min_value
+        return MinimumCoins[amount] if MinimumCoins[amount] != float('inf') else -1

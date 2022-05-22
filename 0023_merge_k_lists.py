4fe9c8393c029351e08060965144d9daf3adccb0
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-22 22:56:55 +0400

    Create 0023_merge_k_lists.py

---
+++ 0023_merge_k_lists.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if len(lists) == 0 or lists is None: return
+        if len(lists) == 1:
+            return lists[0]
+        if len(lists) == 2:
+            return self.merge(lists[0], lists[1])
+        left = 0
+        right = len(lists) - 1
+        mid = (right + left) // 2
+        list1 = self.mergeKLists(lists[:mid])
+        list2 = self.mergeKLists(lists[mid:])
+        return self.merge(list1, list2)
+        
+    def merge(self, l1, l2):
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        if l1.val <= l2.val:
+            l1.next = self.merge(l1.next, l2)
+            return l1
+        if l1.val > l2.val:
+            l2.next = self.merge(l1, l2.next)
+            return l2

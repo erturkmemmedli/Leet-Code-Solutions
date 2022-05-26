ec1c9ea5878fd6eef9615f1fb6a258268a6aef37
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-26 09:36:49 +0400

    Update and rename 300_length_of_LIS.py to 0300_length_of_LIS.py

---
+++ 0300_length_of_LIS.py
@@ -0,0 +1,30 @@
+from bisect import bisect_left
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = []
+        for num in nums:
+            index = bisect_left(dp, num)
+            if index == len(dp):
+                dp.append(num)
+            else:
+                dp[index] = num
+        return len(dp)
+
+# Alternative solution
+
+class Solution1:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        array = [0] * (n + 1)
+        array[1] = 1
+        for i in range(2, n + 1):
+            maximum = - float('inf')
+            for j in range(i-1, 0, -1):
+                if nums[i-1] > nums[j-1]:
+                    array[i] = array[j] + 1
+                else:
+                    array[i] = 1
+                maximum = max(array[i], maximum)
+            array[i] = maximum
+        return max(array)

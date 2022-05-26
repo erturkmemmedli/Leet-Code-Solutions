25de843ce22298a9519f5f986ecb403a55f6e100
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-26 09:22:13 +0400

    Update 0354_max_envelopes.py

--- 0354_max_envelopes.py
+++ 0354_max_envelopes.py
@@ -9,3 +9,17 @@ class Solution:
             index = bisect_left(LIS, height)
             LIS[index : index + 1] = [height]
         return len(LIS)
+
+# Alternative solution
+
+class Solution1:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        envelopes.sort(key = lambda x: [x[0], -x[1]])
+        DP = []
+        for width, height in envelopes:
+            index = bisect_left(DP, height)
+            if index == len(DP):
+                DP.append(height)
+            else:
+                DP[index] = height
+        return len(DP)

2d7375e251830e0b00bdd54bf37a550b800f7601
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-26 16:42:14 +0400

    Update 0191_hamming_weight.py

--- 0191_hamming_weight.py
+++ 0191_hamming_weight.py
@@ -6,3 +6,13 @@ class Solution:
                 count += 1
             n = n >> 1
         return count
+
+# Alternative solution
+
+class Solution1:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n:
+            count += n & 1
+            n = n >> 1
+        return count

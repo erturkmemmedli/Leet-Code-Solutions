543328aba5639676c83be5cc1d397268a3247cac
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-27 13:48:46 +0400

    Update 1342_number_of_steps.py

--- 1342_number_of_steps.py
+++ 1342_number_of_steps.py
@@ -6,3 +6,10 @@ class Solution:
             return 1 + self.numberOfSteps(num - 1)
         if num % 2 == 0:
             return 1 + self.numberOfSteps(num // 2)
+
+# Alternative solution
+
+class Solution1:
+    def numberOfSteps(self, num: int) -> int:
+        bitstring = bin(num)[2:]
+        return len(bitstring) - 1 + bitstring.count('1')

64cfdb94fe9aaf31caf2cdfb7bdf813061272ba3
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-27 15:37:54 +0400

    Create 0242_is_anagram.py

---
+++ 0242_is_anagram.py
@@ -0,0 +1,31 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return not (Counter(s) - Counter(t) or Counter(t) - Counter(s))
+      
+# Alternative solution
+
+class Solution1:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+      
+# Alternative solution   
+
+class Solution2:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dictionary = {}
+        for i in s:
+            if i not in dictionary:
+                dictionary[i] = 1
+            else:
+                dictionary[i] += 1
+        for i in t:
+            if i not in dictionary:
+                dictionary[i] = -1
+            else:
+                dictionary[i] -= 1
+        for v in dictionary.values():
+            if v:
+                return False
+        return True

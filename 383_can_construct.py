01e5ad757785a40d1ae11e8c3c3633942102f765
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-27 15:15:45 +0400

    Create 383_can_construct.py

---
+++ 383_can_construct.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        dictionary = {}
+        for i in magazine:
+            if i not in dictionary:
+                dictionary[i] = 1
+            else:
+                dictionary[i] += 1
+        for i in ransomNote:
+            if i not in dictionary:
+                dictionary[i] = -1
+            else:
+                dictionary[i] -= 1
+        for k, v in dictionary.items():
+            if v < 0:
+                return False
+        return True

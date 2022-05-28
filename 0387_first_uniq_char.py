0c88d6d761105a9351d654bad8f7e984808bee8c
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-28 11:06:43 +0400

    Update 0387_first_uniq_char.py

--- 0387_first_uniq_char.py
+++ 0387_first_uniq_char.py
@@ -29,3 +29,31 @@ class Solution1:
         for j in array:
             if j is not None: return j
         return -1
+
+# Alternative solution
+
+class Solution2:
+    def firstUniqChar(self, s: str) -> int:
+        hashset = set()
+        hashmap = dict()
+        for index, char in enumerate(s):
+            if char not in hashset:
+                hashset.add(char)
+                hashmap[char] = index
+            elif char in hashmap:
+                del(hashmap[char])
+        return min(hashmap.values()) if hashmap else -1
+    
+# Alternative solution
+
+class Solution3:
+    def firstUniqChar(self, s: str) -> int:
+        hashset = set()
+        hashmap = dict()
+        for index, char in enumerate(s):
+            if char not in hashset:
+                hashset.add(char)
+                hashmap[char] = index
+            elif char in hashmap:
+                del(hashmap[char])
+        return next(iter(hashmap.values())) if hashmap else -1

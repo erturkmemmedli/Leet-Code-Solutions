1cf9689ffbca79493ed127a306b8b93b85b400a0
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-28 21:28:01 +0400

    Update 0695_max_area_of_island.py

--- 0695_max_area_of_island.py
+++ 0695_max_area_of_island.py
@@ -22,3 +22,32 @@ class Solution:
                                 Q.append((r, c))
                 max_area = max(area, max_area)
         return max_area
+
+# Alternative solution
+
+class Solution1:
+    def __init__(self):
+        self.max_area = 0
+        self.area = 0
+        
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        visited = set()
+        m = len(grid)
+        n = len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if not grid[i][j] or (i, j) in visited:
+                    continue
+                self.area = 0
+                self.dfs(grid, i, j, m, n, self.max_area, self.area, visited)
+        return self.max_area
+                
+    def dfs(self, grid, row, col, m, n, max_area, area, visited):
+        if (row, col) not in visited:
+            visited.add((row, col))
+            self.area += 1
+            self.max_area = max(self.max_area, self.area)
+            for r, c in (row-1, col), (row+1, col), (row, col-1), (row, col+1):
+                if r >= 0 and c >= 0 and r < m and c < n and grid[r][c]:
+                    self.dfs(grid, r, c, m, n, self.max_area, self.area, visited)
+        return          

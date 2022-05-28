8abbb2e6c0ee04694f54fc56be84293404b357f4
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-28 18:54:42 +0400

    Update and rename 733_flood_fill.py to 0733_flood_fill.py

---
+++ 0733_flood_fill.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        m = len(image)
+        n = len(image[0])
+        oldColor = image[sr][sc]
+        image[sr][sc] = newColor
+        visited = set()
+        Q = deque([(sr, sc)])
+        while Q:
+            row, col = Q.popleft()
+            if (row, col) not in visited:
+                visited.add((row, col))
+                for r, c in (row-1, col), (row+1, col), (row, col-1), (row, col+1):
+                    if r >= 0 and c >= 0 and r < m and c < n:
+                        if image[r][c] == oldColor:
+                            Q.append((r, c))
+                            image[r][c] = newColor
+        return image
+
+# Alternative solution
+
+class Solution1:
+    def floodFill(self, image, sr: int, sc: int, newColor: int):
+        m = len(image)
+        n = len(image[0])
+        oldColor = image[sr][sc]
+        image[sr][sc] = newColor
+        visited = set()
+        self.dfs(image, sr, sc, m, n, oldColor, newColor, visited)
+        return image
+         
+    def dfs(self, image, row, col, m, n, old, new, visited):
+        if (row, col) not in visited:
+            visited.add((row, col))
+            for r, c in (row-1, col), (row+1, col), (row, col-1), (row, col+1):
+                if r >= 0 and c >= 0 and r < m and c < n:
+                    if image[r][c] == old:
+                        image[r][c] = new
+                        self.dfs(image, r, c, m, n, old, new, visited)
+        return

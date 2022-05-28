c7eacaf6c5ca3decf208f7996676fe06b208f433
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-28 14:45:01 +0400

    Update 0567_check_inclusion.py

--- 0567_check_inclusion.py
+++ 0567_check_inclusion.py
@@ -18,3 +18,23 @@ class Solution1:
             if not c1 - c2:
                 return True
         return False  
+
+# Alternative solution
+
+from collections import Counter
+
+class Solution2:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        target = Counter(s1)
+        window = Counter(s2[:len(s1)])
+        if target == window:
+                return True
+        for index, char in enumerate(s2[len(s1):], len(s1)):
+            temp = s2[index - len(s1)]
+            window[temp] -= 1
+            if not window[temp]: del(window[temp])
+            if char not in window: window[char] = 1
+            else: window[char] += 1
+            if target == window:
+                return True
+        return False

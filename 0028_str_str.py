ec748289d00d715a30a6335c052ec663e78da58c
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-28 10:39:16 +0400

    Update 0028_str_str.py

--- 0028_str_str.py
+++ 0028_str_str.py
@@ -6,3 +6,13 @@ class Solution:
             if haystack[i:i+n] == needle:
                 return i
         return -1
+
+# Alternative solution
+
+class Solution1:
+    def strStr(self, haystack: str, needle: str) -> int:
+        hash_val = hash(needle)
+        for i in range(len(haystack) - len(needle) + 1):
+            if hash_val == hash(haystack[i:i+len(needle)]):
+                return i
+        return -1

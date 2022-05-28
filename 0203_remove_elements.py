d3e2af1efec4c0ea7e12e76c7a2070212a5bf0b7
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-28 17:15:37 +0400

    Update 0203_remove_elements.py

--- 0203_remove_elements.py
+++ 0203_remove_elements.py
@@ -6,9 +6,7 @@
 
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        if head is None:
-            return
-        if head.val == val:
-            return self.removeElements(head.next, val)
+        if head is None: return
+        if head.val == val: return self.removeElements(head.next, val)
         head.next = self.removeElements(head.next, val)
         return head

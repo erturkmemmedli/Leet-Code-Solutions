4bf730330b4fe745bacb8dd7b51787499f6f7da9
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-29 20:29:32 +0400

    Create 0083_delete_duplicates.py

---
+++ 0083_delete_duplicates.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        output = head
+        node = head.next
+        while node:  
+            if head.val == node.val:
+                node = node.next
+            else:
+                head.next = node
+                head = node
+                node = head.next
+        head.next = None
+        return output

c4f03e1ab4462f11cd60fccbcfa6a6ea6c1a9930
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-29 23:27:37 +0400

    Update 0116_connect.py

--- 0116_connect.py
+++ 0116_connect.py
@@ -1,15 +1,17 @@
+"""
 # Definition for a Node.
-# class Node:
-#    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
-#        self.val = val
-#        self.left = left
-#        self.right = right
-#        self.next = next
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
 
 from collections import deque
 
 class Solution:
-    def connect(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
         if not root: return
         Q = deque([deque([root]), deque([])])
         while Q:

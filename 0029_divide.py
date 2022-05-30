2f3e0dcfd4a43870800a72999e218cbad0e397a1
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-30 13:18:49 +0400

    Update 0029_divide.py

--- 0029_divide.py
+++ 0029_divide.py
@@ -8,3 +8,20 @@ class Solution:
             return -(abs(a)//abs(b)) if abs(a)//abs(b) < 2**31 else -2**31
             
 # Alternative solution   
+
+class Solution1:
+    def divide(self, dividend: int, divisor: int) -> int:
+        is_positive = (dividend < 0 and divisor < 0) or (dividend > 0 and divisor > 0) 
+        dividend, divisor = abs(dividend), abs(divisor)
+        quotient = 0
+        while dividend >= divisor:
+            temporary = divisor
+            increment = 1
+            while dividend >= temporary:
+                dividend -= temporary
+                quotient += increment
+                temporary <<= 1
+                increment <<= 1
+        if not is_positive:
+            quotient *= -1
+        return min(max(-2**31, quotient), 2**31 - 1)

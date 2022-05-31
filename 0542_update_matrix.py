48710e95c7c74f2be71550859d33469fb760b3b1
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-05-31 16:25:19 +0400

    Update 0542_update_matrix.py

--- 0542_update_matrix.py
+++ 0542_update_matrix.py
@@ -20,3 +20,26 @@ class Solution:
                         visited.add((i, j))
                         Q.append((i, j))
         return mat
+
+# Alternative solution
+
+class Solution1:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m = len(mat)
+        n = len(mat[0])
+        for row in range(m):
+            for col in range(n):
+                if mat[row][col]:
+                    mat[row][col] = float('inf')
+                    if row and mat[row-1][col] < mat[row][col]:
+                        mat[row][col] = mat[row-1][col] + 1
+                    if col and mat[row][col-1] < mat[row][col]:
+                        mat[row][col] = mat[row][col-1] + 1
+        for row in range(m-1, -1, -1):
+            for col in range(n-1, -1, -1):
+                if mat[row][col]:
+                    if row < m - 1 and mat[row][col] > mat[row+1][col]:
+                        mat[row][col] = mat[row+1][col] + 1
+                    if col < n - 1 and mat[row][col] > mat[row][col+1]:
+                        mat[row][col] = mat[row][col+1] + 1
+        return mat

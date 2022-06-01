0a55a840628220bfb01cb07024d176527726c80d
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-01 11:58:57 +0400

    Update 0101_is_symmetric.py

--- 0101_is_symmetric.py
+++ 0101_is_symmetric.py
@@ -22,3 +22,34 @@ class Solution:
         arr.append(node.val)
         self.inorder(node.right, arr, multiplier * 10)
         return arr
+
+# Alternative solution
+
+class Solution1: 
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.dfs(root.left, root.right)
+        
+    def dfs(self, left, right):
+        if not left and not right: return True
+        if not left or not right: return False
+        if left.val == right.val:
+            outer = self.dfs(left.left, right.right)
+            inner = self.dfs(left.right, right.left)
+            if outer and inner: return True
+            else: return False
+           
+# Alternative solution
+
+class Solution2: 
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        stack = [[root.left, root.right]]
+        while stack:
+            left, right = stack.pop()
+            if not left and not right: continue
+            if not left or not right: return False
+            if left.val == right.val:
+                stack.append([left.left, right.right])
+                stack.append([left.right, right.left])
+            else:
+                return False
+        return True

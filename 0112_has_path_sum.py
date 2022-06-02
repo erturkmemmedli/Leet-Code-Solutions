88286631684c0e81abf4047805ca8e73799e1cae
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-02 12:36:55 +0400

    Update 0112_has_path_sum.py

--- 0112_has_path_sum.py
+++ 0112_has_path_sum.py
@@ -17,3 +17,12 @@ class Solution:
         if not root.left and not root.right: result.append(Sum + root.val)
         self.dfs(root.left, Sum + root.val, result)
         self.dfs(root.right, Sum + root.val, result)
+
+# Alternative solution
+
+class Solution1:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root: return False
+        if not root.left and not root.right and root.val == targetSum: return True
+        targetSum -= root.val
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)

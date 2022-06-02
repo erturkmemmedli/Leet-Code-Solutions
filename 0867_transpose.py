7281414a2289f8ef79ff5bb25c1c8454c0ec4b9a
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-02 14:49:28 +0400

    Update 0867_transpose.py

--- 0867_transpose.py
+++ 0867_transpose.py
@@ -1,3 +1,15 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
         return [list(map(lambda x: x[i], matrix)) for i in range(len(matrix[0]))]
+
+# Alternative solution
+
+class Solution1:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        m = len(matrix)
+        n = len(matrix[0])
+        transposed_matrix = [[0] * m for _ in range(n)]
+        for row in range(n):
+            for col in range(m):
+                transposed_matrix[row][col] = matrix[col][row]
+        return transposed_matrix

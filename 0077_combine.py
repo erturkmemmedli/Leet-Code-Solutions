7bf324120970c8a2eaca1df809a57aa5088137ac
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-02 10:59:53 +0400

    Update 0077_combine.py

--- 0077_combine.py
+++ 0077_combine.py
@@ -12,3 +12,37 @@ class Solution:
             return
         for i in range(len(nums)):
             self.recursion(nums[i+1:], k, result, temp + [nums[i]])
+
+# Alternative solition
+
+class Solution1:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        result = []
+        self.backtrack(n, k, result, 1, [])
+        return result
+    
+    def backtrack(self, n, k, result, nxt, temp):
+        if k == 0:
+            result.append(temp)
+            return
+        else:
+            for i in range(nxt, n + 1):
+                self.backtrack(n, k - 1, result, i + 1, temp + [i])
+                
+# Alternative solution
+
+class Solution2:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        result = []
+        self.backtrack(n, k, result, 1, [])
+        return result
+    
+    def backtrack(self, n, k, result, nxt, temp):
+        if k == 0:
+            result.append(temp.copy())
+            return
+        else:
+            for i in range(nxt, n + 1):
+                temp.append(i)
+                self.backtrack(n, k - 1, result, i + 1, temp)
+                temp.pop()

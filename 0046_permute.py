45639f32350120ad396d196547763f3309eaada3
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-02 11:17:40 +0400

    Update 0046_permute.py

--- 0046_permute.py
+++ 0046_permute.py
@@ -11,3 +11,37 @@ class Solution:
             return
         for i in range(len(nums)):
             self.DFS(nums[:i] + nums[i+1:], result, temp + [nums[i]])
+
+# Alternative solution
+
+class Solution1:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        self.backtrack(nums, result, [])
+        return result
+    
+    def backtrack(self, nums, result, temp):
+        if len(nums) == 0:
+            result.append(temp.copy())
+            return
+        else:
+            for i, n in enumerate(nums):
+                temp.append(n)
+                self.backtrack(nums[i+1:] + nums[:i], result, temp)
+                temp.pop()
+                
+# Alternative solution
+
+class Solution2:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        self.backtrack(nums, result, [])
+        return result
+    
+    def backtrack(self, nums, result, temp):
+        if len(nums) == 0:
+            result.append(temp)
+            return
+        else:
+            for i, n in enumerate(nums):
+                self.backtrack(nums[i+1:] + nums[:i], result, temp + [n])

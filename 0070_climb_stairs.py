1046576d2ce77cfaefc959af5d4b623a4edff29d
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-02 14:38:19 +0400

    Update 0070_climb_stairs.py

--- 0070_climb_stairs.py
+++ 0070_climb_stairs.py
@@ -5,3 +5,14 @@ class Solution:
         if n > 1: dp[2] = 2
         for i in range(3, n + 1): dp[i] = dp[i-1] + dp[i-2]
         return dp[-1]
+
+# Alternative solution
+
+class Solution1:
+    def climbStairs(self, n: int) -> int:
+        if n < 3: return n
+        prev = 1
+        answer = 2
+        for i in range(3, n + 1):
+            prev, answer = answer, answer + prev
+        return answer

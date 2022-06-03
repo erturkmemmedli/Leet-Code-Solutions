2d151fd04518d61c278d4045c4b6ef283957e5dc
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-03 15:51:33 +0400

    Update 0304_sum_region.py

--- 0304_sum_region.py
+++ 0304_sum_region.py
@@ -21,3 +21,28 @@ class NumMatrix:
             return self.H[(row2, col2)] - self.H[(row1-1, col2)]
         else:  
             return self.H[(row2, col2)] + self.H[(row1-1, col1-1)] - self.H[(row1-1, col2)] - self.H[(row2, col1-1)]
+
+# Alternative solution
+
+class NumMatrix1:
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.m = len(self.matrix)
+        self.n = len(self.matrix[0])
+        for row in range(1, self.m):
+            self.matrix[row][0] = self.matrix[row-1][0] + self.matrix[row][0]
+        for col in range(1, self.n):
+            self.matrix[0][col] = self.matrix[0][col-1] + self.matrix[0][col]
+        for row in range(1, self.m):
+            for col in range(1, self.n):
+                self.matrix[row][col] = self.matrix[row][col-1] + self.matrix[row-1][col] + self.matrix[row][col] - self.matrix[row-1][col-1]
+        
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        if row1 == 0 and col1 == 0:
+            return self.matrix[row2][col2]
+        elif row1 == 0:
+            return self.matrix[row2][col2] - self.matrix[row2][col1-1]
+        elif col1 == 0:
+            return self.matrix[row2][col2] - self.matrix[row1-1][col2]
+        else:  
+            return self.matrix[row2][col2] + self.matrix[row1-1][col1-1] - self.matrix[row1-1][col2] - self.matrix[row2][col1-1] 

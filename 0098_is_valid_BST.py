f8715d98a6d0b212532cb0117b52c12309c3a978
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-04 20:41:34 +0400

    Update 0098_is_valid_BST.py

--- 0098_is_valid_BST.py
+++ 0098_is_valid_BST.py
@@ -19,3 +19,22 @@ class Solution:
         if root.left: self.inorderTraversal(root.left, List)
         List.append(root.val)
         if root.right: self.inorderTraversal(root.right, List)
+
+# Alternative solution
+
+class Solution1:
+    def __init__(self):
+        self.flag = True
+        
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        inorder_list = []
+        self.inorderTraversal(root, inorder_list)
+        return self.flag
+        
+    def inorderTraversal(self, root, List):
+        if not root: return
+        if root.left: self.inorderTraversal(root.left, List)
+        List.append(root.val)
+        if len(List) > 1 and List[-1] <= List[-2]:
+            self.flag = False
+        if root.right: self.inorderTraversal(root.right, List)

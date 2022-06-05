baef47b0e5a6245ee604f47f6d2f83d9bf3cf48c
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-05 23:40:54 +0400

    Update 1512_num_identical_pairs.py

--- 1512_num_identical_pairs.py
+++ 1512_num_identical_pairs.py
@@ -6,3 +6,19 @@ class Solution:
                 if nums[i] == nums[j]:
                     count += 1
         return count
+
+# Alternative solution
+
+class Solution1:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        dictionary = {}
+        for num in nums:
+            if num not in dictionary:
+                dictionary[num] = 0
+            else:
+                dictionary[num] += 1
+        count = 0
+        for val in dictionary.values():
+            if val:
+                count += (val * (val + 1) // 2)
+        return count

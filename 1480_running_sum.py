c3ec6d59b2996c31787da92c9cc1c705f941a416
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-05 11:42:00 +0400

    Update 1480_running_sum.py

--- 1480_running_sum.py
+++ 1480_running_sum.py
@@ -4,3 +4,12 @@ class Solution:
             if i:
                 nums[i] += nums[i-1]
         return nums
+
+# Alternative solution
+
+class Solution1:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        result = [nums[0]]
+        for i in range(1, len(nums)):
+            result.append(nums[i] + result[i-1])
+        return result

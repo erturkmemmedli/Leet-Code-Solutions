166edc1ca56216e18e4476885cbab4a831e130d5
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-06 12:04:16 +0400

    Update 0104_max_depth.py

--- 0104_max_depth.py
+++ 0104_max_depth.py
@@ -28,7 +28,7 @@ class Solution:
 
 # Alternative solution
 
-class Solution2:
+class Solution1:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root: return 0
         return max(1 + self.maxDepth(root.left), 1 + self.maxDepth(root.right))

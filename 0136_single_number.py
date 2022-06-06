3be7c6376a890db0b7fb8fbcfc76dec69236e3ae
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-06 14:53:33 +0400

    Update 0136_single_number.py

--- 0136_single_number.py
+++ 0136_single_number.py
@@ -4,3 +4,15 @@ class Solution:
         for num in nums:
             result ^= num
         return result
+
+# Alternative solution
+
+class Solution1:
+    def singleNumber(self, nums: List[int]) -> int:
+        s = set()
+        for num in nums:
+            if num not in s:
+                s.add(num)
+            else:
+                s.discard(num)
+        return s.pop()

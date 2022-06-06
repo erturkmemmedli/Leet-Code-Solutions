2b4b0b6d628117c3dba0a7c2d9573db953e6ec16
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-06 20:39:26 +0400

    Update 0905_sort_array_by_parity.py

--- 0905_sort_array_by_parity.py
+++ 0905_sort_array_by_parity.py
@@ -7,3 +7,18 @@ class Solution:
                 pivot += 1
                 continue
         return nums
+    
+# Alternative solution
+
+class Solution1:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        pointer = 0
+        i = 0
+        while i < len(nums):
+            if nums[i] % 2 == 0:
+                nums[i], nums[pointer] = nums[pointer], nums[i]
+                i += 1
+                pointer += 1
+            elif nums[i] % 2 == 1:
+                i += 1
+        return nums

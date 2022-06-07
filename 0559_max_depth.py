19553fb6a3d298cac8e55d0e6091a119e7c9c69e
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-07 15:33:19 +0400

    Update 0559_max_depth.py

--- 0559_max_depth.py
+++ 0559_max_depth.py
@@ -13,3 +13,23 @@ class Solution:
         return max([1 + self.maxDepth(child) for child in root.children])
       
 # Alternative solution
+
+from collections import deque
+
+class Solution1:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root: return 0
+        if not root.children: return 1
+        depth = 1
+        Q = deque(root.children)
+        children = []
+        while Q:
+            child = Q.popleft()
+            if child.children:
+                children += child.children
+            if not Q:
+                depth += 1
+                if children:
+                    Q = deque(children)
+                children = []
+        return depth

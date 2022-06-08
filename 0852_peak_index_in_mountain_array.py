08da3996d7a92088b0b5ac881f440d3b4ad2c480
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-08 12:01:33 +0400

    Update 0852_peak_index_in_mountain_array.py

--- 0852_peak_index_in_mountain_array.py
+++ 0852_peak_index_in_mountain_array.py
@@ -3,3 +3,18 @@ class Solution:
         for i in range(1, len(arr)):
             if arr[i] < arr[i-1]:
                 return i-1
+
+# Alternative solution
+
+class Solution1:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        left = 0
+        right = len(arr) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if arr[mid] > arr[mid-1] and arr[mid] > arr[mid+1]:
+                return mid
+            elif arr[mid] < arr[mid-1]:
+                right = mid
+            elif arr[mid] > arr[mid-1]:
+                left = mid + 1

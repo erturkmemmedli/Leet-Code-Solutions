58af571c74a6d6bf8aeb63c0866da6ef1f2f6c1a
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-09 12:29:05 +0400

    Create 1304_sum_zero.py

---
+++ 1304_sum_zero.py
@@ -0,0 +1,4 @@
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        if n % 2 == 0: return list(range(-(n//2), 0)) + list(range(1, n//2 + 1))
+        else: return list(range(-(n//2), n//2 + 1))

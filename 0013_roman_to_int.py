038f292f6f95c16c381a65a1cb2470ead2ef86d0
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-10 15:33:48 +0400

    Update 0013_roman_to_int.py

--- 0013_roman_to_int.py
+++ 0013_roman_to_int.py
@@ -35,3 +35,16 @@ class Solution:
             if s[i] == 'I':
                 result += 1
         return result
+    
+# Alternative solution
+
+class Solution1:
+    def romanToInt(self, s: str) -> int:
+        d = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        integer = d[s[-1]]
+        for i in range(len(s)-2,-1,-1):
+            if d[s[i]] < d[s[i+1]]:
+                integer -= d[s[i]]
+            else:
+                integer += d[s[i]]
+        return integer

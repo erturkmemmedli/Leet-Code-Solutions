d40fd8baac21d00e96755d5f72e9ffcf99645be7
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-10 12:25:54 +0400

    Update 1051_height_checker.py

--- 1051_height_checker.py
+++ 1051_height_checker.py
@@ -6,3 +6,18 @@ class Solution:
             if expected[i] != height:
                 count += 1
         return count
+
+# Alternative solution
+
+class Solution1:
+    def heightChecker(self, heights: List[int]) -> int:
+        counting_array = [0] * 100
+        for h in heights:
+            counting_array[h-1] += 1
+        expected = []
+        for i, c in enumerate(counting_array):
+            if c: expected += [i+1] * c
+        count = 0
+        for he, ex in zip(heights, expected):
+            if he != ex: count += 1
+        return count

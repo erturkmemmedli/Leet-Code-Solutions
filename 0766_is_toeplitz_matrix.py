7cce809804934213e84e5b6c6bb42fe0797e9891
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-10 22:09:10 +0400

    Update 0766_is_toeplitz_matrix.py

--- 0766_is_toeplitz_matrix.py
+++ 0766_is_toeplitz_matrix.py
@@ -19,3 +19,15 @@ class Solution:
                 r += 1
                 c += 1
         return True
+
+# Alternative solution
+
+class Solution1:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+        for row in range(1, m):
+            for col in range(1, n):
+                if matrix[row][col] != matrix[row-1][col-1]:
+                    return False
+        return True

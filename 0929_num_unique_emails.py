3d28b2aebf623889c00b691ac240a86e88fa3a4e
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-10 21:21:33 +0400

    Update 0929_num_unique_emails.py

--- 0929_num_unique_emails.py
+++ 0929_num_unique_emails.py
@@ -16,3 +16,35 @@ class Solution:
                     temp += char
             address.add(temp)
         return len(address)
+
+# Alternative solution
+
+class Solution1:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        address = set()
+        for email in emails:
+            at = email.index('@')
+            try:
+                plus = email.index('+')
+            except:
+                plus = None
+            try:
+                period = email.index('.')
+            except:
+                period = None
+            while period != None and period < at:
+                if plus != None:
+                    if period < plus:
+                        plus -= 1
+                    else:
+                        break
+                email = email[:period] + email[period+1:]
+                at -= 1
+                try:
+                    period = email.index('.')
+                except:
+                    period = None
+            if plus != None:
+                email = email[:plus] + email[at:]
+            address.add(email)
+        return len(address)

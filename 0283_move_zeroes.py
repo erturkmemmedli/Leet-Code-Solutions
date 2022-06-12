fcdbbcca99b3b80495b139b1499ca7ccdda462c8
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-12 11:30:28 +0400

    Update 0283_move_zeroes.py

--- 0283_move_zeroes.py
+++ 0283_move_zeroes.py
@@ -7,3 +7,21 @@ class Solution:
             else:
                 nums[i], nums[pivot] = nums[pivot], nums[i]
                 pivot += 1
+
+# Alternative solution
+
+class Solution1:
+    def moveZeroes(self, nums: List[int]) -> None:
+        zero = -1
+        for i in range(len(nums)):
+            if not nums[i]:
+                if zero < 0:
+                    zero = i
+                else:
+                    continue
+            else:
+                if zero < 0:
+                    continue
+                else:
+                    nums[i], nums[zero] = nums[zero], nums[i]
+                    zero += 1

7b97f4320bbb0662d5a8c50bd35163c3b5c4a78b
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-12 15:18:57 +0400

    Update 1047_remove_duplicates.py

--- 1047_remove_duplicates.py
+++ 1047_remove_duplicates.py
@@ -11,3 +11,23 @@ class Solution:
                 i += 1
                 j += 1
         return s
+
+# Alternative solution
+
+class Solution1:
+    def removeDuplicates(self, s: str) -> str:
+        stack = ""
+        for char in s:
+            if stack and stack[-1] == char: stack = stack[:len(stack)-1]
+            else: stack += char
+        return stack
+    
+# Alternative solution
+
+class Solution2:
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+        for char in s:
+            if stack and stack[-1] == char: stack.pop()
+            else: stack.append(char)
+        return ''.join(stack)

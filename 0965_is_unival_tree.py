9477fa23baf8565f3341ad853abcf4992ee03536
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-12 13:26:43 +0400

    Update 0965_is_unival_tree.py

--- 0965_is_unival_tree.py
+++ 0965_is_unival_tree.py
@@ -21,3 +21,19 @@ class Solution:
                     self.flag = False
                 self.dfs(node.left, val)
                 self.dfs(node.right, val)
+
+# Alternative solution
+
+class Solution1:
+    def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+        val = root.val
+        result = self.dfs(root, val)
+        return result
+        
+    def dfs(self, node, val):
+        if not node:
+            return True
+        else:
+            if node.val != val:
+                return False
+            return self.dfs(node.left, val) and self.dfs(node.right, val)

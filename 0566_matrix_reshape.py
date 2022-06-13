e5567d8a069e28607c6aebecf28dc65d2c59e445
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-13 11:47:14 +0400

    Update 0566_matrix_reshape.py

--- 0566_matrix_reshape.py
+++ 0566_matrix_reshape.py
@@ -9,3 +9,23 @@ class Solution:
             for j in range(c):
                 result[i][j] = mat[(i * c + j) // n][(i * c + j) % n]
         return result
+
+# Alternative solution
+
+class Solution1:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        m = len(mat)
+        n = len(mat[0])
+        if m * n != r * c or (m == r and n == c): return mat
+        new_matrix = []
+        for row in mat:
+            new_matrix += row
+        result = []
+        temp = []
+        for elem in new_matrix:
+            if len(temp) < c:
+                temp.append(elem)
+            if len(temp) == c:
+                result.append(temp)
+                temp = []
+        return result

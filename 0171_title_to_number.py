e74e72cf439da1c328447e8891289d4b28bb1391
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-13 12:25:43 +0400

    Create 0171_title_to_number.py

---
+++ 0171_title_to_number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        result = 0
+        for i in range(len(columnTitle)):
+            result += (26 ** i) * (ord(columnTitle[-(i+1)]) - 64)
+        return result

6944d256ec2c5b4a2030c18f1bc4f1b28c99d5aa
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-15 17:12:29 +0400

    Update 0535_codec.py

--- 0535_codec.py
+++ 0535_codec.py
@@ -13,3 +13,33 @@ class Codec:
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
 # codec.decode(codec.encode(url))
+
+# Alternative solution
+
+import random
+import string
+
+class Codec:
+    def __init__(self):
+        self._encode = {}
+        self._decode = {}
+        self.characters = string.ascii_letters + string.digits
+
+    def encode(self, longUrl: str) -> str:
+        url = longUrl[8:]
+        hash_val = 0
+        for i, char in enumerate(url):
+            hash_val += i * ord(char) * 10000019
+        val = hash_val
+        self._decode[hash_val] = longUrl
+        tiny = ""
+        for _ in range(6):
+            tiny += self.characters[val % len(self.characters)]
+            val = val // len(self.characters)
+        tiny_url = 'http://tinyurl.com/' + tiny
+        self._encode[tiny] = hash_val
+        return tiny_url
+        
+    def decode(self, shortUrl: str) -> str:
+        tiny = shortUrl[-6:]
+        return self._decode[self._encode[tiny]]

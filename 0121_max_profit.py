0bb2a70e74310fdc02f79e25cb0c14b5512b10d8
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-17 15:06:34 +0400

    Update 0121_max_profit.py

--- 0121_max_profit.py
+++ 0121_max_profit.py
@@ -8,3 +8,15 @@ class Solution:
             else:
                 dp[i] = prices[i] - prices[minimum]
         return max(dp)
+
+# Alternative solution
+
+class Solution1:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = [0] * len(prices)
+        for i in range(1, len(prices)):
+            if dp[i-1] + prices[i] - prices[i-1] >= 0:
+                dp[i] = dp[i-1] + prices[i] - prices[i-1]
+            else:
+                dp[i] = 0
+        return max(dp)

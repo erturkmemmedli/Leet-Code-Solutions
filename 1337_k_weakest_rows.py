e02f30d8a89d94c532fca816733157b19e04221a
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-17 12:39:24 +0400

    Update 1337_k_weakest_rows.py

--- 1337_k_weakest_rows.py
+++ 1337_k_weakest_rows.py
@@ -3,3 +3,13 @@ class Solution:
         ones = [(row.count(1), i)  for i, row in enumerate(mat)]
         order = [i[1] for i in sorted(ones, key = lambda x: x[0])]
         return order[:k]
+
+# Alternative solution
+
+import heapq as H
+
+class Solution1:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        ones = [(row.count(1), i)  for i, row in enumerate(mat)]
+        H.heapify(ones)
+        return [H.heappop(ones)[1] for _ in range(k)]

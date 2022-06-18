30b35c432dfccd310d6e7116ff9a5b30d3fb180a
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-18 10:23:17 +0400

    Update 0387_first_uniq_char.py

--- 0387_first_uniq_char.py
+++ 0387_first_uniq_char.py
@@ -57,3 +57,14 @@ class Solution3:
             elif char in hashmap:
                 del(hashmap[char])
         return next(iter(hashmap.values())) if hashmap else -1
+
+# Alternative solution
+
+from collections import Counter
+
+class Solution4:
+    def firstUniqChar(self, s: str) -> int:
+        c = Counter(s)
+        for i, char in enumerate(s):
+            if c[char] == 1: return i
+        return -1

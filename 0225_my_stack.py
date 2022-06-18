c640717c2fbcd37e5782d19c00217e0a7153e76b
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-18 23:21:46 +0400

    Update 0225_my_stack.py

--- 0225_my_stack.py
+++ 0225_my_stack.py
@@ -22,3 +22,23 @@ class MyStack:
 # param_2 = obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.empty()
+
+# Alternative solution
+
+from collections import deque
+
+class MyStack1:
+    def __init__(self):
+        self.stack = deque()
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def empty(self) -> bool:
+        return not self.stack

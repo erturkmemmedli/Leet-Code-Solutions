0c9e196224bee2630ad19bd61ededef1028863ba
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-18 23:03:21 +0400

    Update 0232_my_queue.py

--- 0232_my_queue.py
+++ 0232_my_queue.py
@@ -24,3 +24,24 @@ class MyQueue:
 # param_2 = obj.pop()
 # param_3 = obj.peek()
 # param_4 = obj.empty()
+
+
+# Alternative solution
+
+class MyQueue1:
+    def __init__(self):
+        self.queue = []
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        popped = self.queue[0] 
+        self.queue = self.queue[1:]
+        return popped
+
+    def peek(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        return not len(self.queue)

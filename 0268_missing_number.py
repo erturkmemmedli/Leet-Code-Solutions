5fa0b5d8f0838fff0b8d3b6796c66832e6a32523
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-18 10:19:38 +0400

    Update 0268_missing_number.py

--- 0268_missing_number.py
+++ 0268_missing_number.py
@@ -5,3 +5,11 @@ class Solution:
         for i in nums:
             target -= i
         return target
+
+# Alternative solution
+
+class Solution1:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        target = n * (n+1) // 2
+        return target - sum(nums)

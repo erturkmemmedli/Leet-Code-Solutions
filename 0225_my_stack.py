2ffeb8ae4ee2b46b2486555bb265c8c57654b78a
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-18 23:39:00 +0400

    Update 0225_my_stack.py

--- 0225_my_stack.py
+++ 0225_my_stack.py
@@ -42,3 +42,25 @@ class MyStack1:
 
     def empty(self) -> bool:
         return not self.stack
+
+# Alternative solution
+
+from collections import deque
+
+class MyStack2:
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+        for _ in range(len(self.queue) - 1):
+            self.queue.append(self.queue.popleft())
+
+    def pop(self) -> int:
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        return not self.queue

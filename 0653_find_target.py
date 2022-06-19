29a3d09301afe1a489729b1fbc2371f42bd82ccf
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-19 15:55:04 +0400

    Update 0653_find_target.py

--- 0653_find_target.py
+++ 0653_find_target.py
@@ -17,3 +17,20 @@ class Solution:
         else:
             return True
         return self.findTarget(root.left, k) or self.findTarget(root.right, k)
+
+# Alternative solution
+
+class Solution1:
+    def __init__(self):
+        self.hashset = set()
+        self.flag = False
+        
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        if not self.flag:
+            if root.val in self.hashset:
+                self.flag = True
+            else:
+                self.hashset.add(k - root.val)
+                if root.left: self.findTarget(root.left, k)
+                if root.right: self.findTarget(root.right, k)        
+        return self.flag

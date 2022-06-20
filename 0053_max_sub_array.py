6e82676bbb42481dd575f1d0cffc3aefbb45faf1
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-20 17:53:28 +0400

    Update 0053_max_sub_array.py

--- 0053_max_sub_array.py
+++ 0053_max_sub_array.py
@@ -10,3 +10,23 @@ class Solution:
             else:
                 result = max(result, summ)
         return result
+
+# Alternative solution
+
+class Solution1:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maximum = nums[0]
+        temp = 0
+        for i in range(len(nums)):
+            if maximum <= 0 and nums[i] <= 0:
+                maximum = max(maximum, nums[i])
+                continue
+            if nums[i] > 0:
+                temp += nums[i]
+                maximum = max(maximum, temp)
+            if nums[i] < 0:
+                if temp + nums[i] > 0:
+                    temp += nums[i]
+                else:
+                    temp = 0
+        return maximum

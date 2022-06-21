7d7804bd1fcdc57cccac88348c9596a5de211b04
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-21 12:40:46 +0400

    Create 0404_sum_of_left_leaves.py

---
+++ 0404_sum_of_left_leaves.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def __init__(self):
+        self.sum = 0
+    
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        self.dfs(root)
+        return self.sum
+        
+    def dfs(self, root):
+        if not root: return
+        if root.left and not root.left.left and not root.left.right:
+            self.sum += root.left.val
+        self.sumOfLeftLeaves(root.left)
+        self.sumOfLeftLeaves(root.right)

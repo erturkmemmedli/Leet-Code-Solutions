6ffeafb2d98305452fdbdd557ca51beb66d4c7ca
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-22 12:54:55 +0400

    Update 0026_remove_duplicates.py

--- 0026_remove_duplicates.py
+++ 0026_remove_duplicates.py
@@ -13,3 +13,18 @@ class Solution:
             else:
                 j += 1
         return len(set(nums))
+
+# Alternative solution
+
+class Solution1:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        j = 1
+        while j < len(nums):
+            if nums[j] == nums[i]:
+                j += 1
+            else:
+                nums[j], nums[i+1] = nums[i+1], nums[j]
+                i += 1
+                j += 1
+        return i+1

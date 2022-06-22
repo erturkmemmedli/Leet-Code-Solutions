8a639ec427ea41fdac2859d1ae9f2a0930e71fa2
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-22 12:38:13 +0400

    Create 1009_bitwise_complement.py

---
+++ 1009_bitwise_complement.py
@@ -0,0 +1,12 @@
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        count = 0
+        temp = n
+        while temp:
+            count += 1
+            temp = temp >> 1
+        top = 1
+        for _ in  range(count):
+            top = top << 1
+        if top > 1: top -= 1
+        return top - n

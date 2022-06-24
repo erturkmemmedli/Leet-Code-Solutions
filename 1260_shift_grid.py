9c6cf8dac113379aee92c13b917303bf974c21da
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-24 14:45:43 +0400

    Update 1260_shift_grid.py

--- 1260_shift_grid.py
+++ 1260_shift_grid.py
@@ -1,16 +1,4 @@
 class Solution:
-    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
-        for _ in range(k):
-            for row in grid:
-                for i in range(len(row)-1,0,-1):
-                    row[i], row[i-1] = row[i-1], row[i]
-            for j in range(len(grid)-1,0,-1):
-                grid[j][0], grid[j-1][0] = grid[j-1][0], grid[j][0]
-        return grid
-      
-# Alternative solution
-
-class Solution1:
     def shiftGrid(self, grid, k: int):
         m = len(grid)
         n = len(grid[0])
@@ -18,7 +6,6 @@ class Solution1:
         k = k % l
         combined = [x for row in grid for x in row]
         combined = combined[l-k:] + combined[:l-k]
-        print(combined)
         result = []
         for i in range(0, m*n, n):
             result.append(combined[i:i+n])
@@ -26,9 +13,9 @@ class Solution1:
 
 # Alternative solution
 
-class Solution2:
+class Solution1:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
-        for _ in range(k // len(grid[0])):
+        for _ in range(k // len(grid[0]) % len(grid)):
             grid.insert(0, grid.pop())
         k = k % len(grid[0])
         for _ in range(k):

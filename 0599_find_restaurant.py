b2296a553d96c8e69e78f44bccc5cb2bbc1564f9
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-24 23:43:52 +0400

    Update 0599_find_restaurant.py

--- 0599_find_restaurant.py
+++ 0599_find_restaurant.py
@@ -12,3 +12,36 @@ class Solution:
         for k,v in d2.items():
             if v == m: result.append(k)
         return result
+
+# Alternative solution
+
+class Solution1:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        hashset = set(list1) & set(list2)
+        index = []
+        for item in hashset:
+            index.append((item, list1.index(item) + list2.index(item)))
+        index = sorted(index, key = lambda x: x[1])
+        i = 0
+        while i < len(index):
+            if index[i][1] == index[0][1]:
+                i += 1
+                continue
+            break
+        return [index[j][0] for j in range(i)]
+    
+# Alternative solution
+
+class Solution2:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        dictionary = {k:v for v, k in enumerate(list1)}
+        result = []
+        temp = float('inf')
+        for v, k in enumerate(list2):
+            minimum = dictionary.get(k, float('inf'))
+            if minimum < temp:
+                temp = minimum
+                result = [k]
+            elif minimum == temp:
+                result.append(v)
+        return result

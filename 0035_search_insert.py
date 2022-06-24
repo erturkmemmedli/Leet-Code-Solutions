c76a6421e700a90f4dc4dbea6d8a6ea1f1a7018e
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-24 15:40:01 +0400

    Update 0035_search_insert.py

--- 0035_search_insert.py
+++ 0035_search_insert.py
@@ -17,3 +17,38 @@ class Solution:
             return self.binary_search(nums, target, left, mid)
         if nums[mid] < target:
             return self.binary_search(nums, target, mid + 1, right)
+
+# Alternative solution
+
+class Solution1:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        mid = len(nums) // 2
+        if nums[mid] == target:
+            return mid
+        if len(nums) == 1:
+            if nums[mid] > target:
+                return mid
+            else:
+                return mid + 1
+        if nums[mid] > target:
+            return self.searchInsert(nums[:mid], target)
+        if nums[mid] < target:
+            return mid + self.searchInsert(nums[mid:], target)
+        
+# Alternative solution
+
+class Solution2:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if left == right:
+                if nums[mid] > target: return mid
+                else: return mid + 1
+            if nums[mid] > target:
+                right = mid
+            if nums[mid] < target:
+                left = mid + 1

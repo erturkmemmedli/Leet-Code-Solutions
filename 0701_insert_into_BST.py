e539701edf110c8132ea1628f9dfb3dccacab934
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-06-24 15:12:18 +0400

    Update 0701_insert_into_BST.py

--- 0701_insert_into_BST.py
+++ 0701_insert_into_BST.py
@@ -20,3 +20,15 @@ class Solution:
             else:
                 root.right = TreeNode(val)
             return root
+
+# Alternative solution
+
+class Solution1:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        return root

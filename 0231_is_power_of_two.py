9482653dbdcb52f2ef1313c01067b131a2cc17d5
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-07-02 12:10:05 +0400

    alternative solution

--- 0231_is_power_of_two.py
+++ 0231_is_power_of_two.py
@@ -3,3 +3,9 @@ class Solution:
         while n % 2 == 0 and n > 0:
             n = n >> 1
         return n == 1
+
+# Alternative solution
+
+class Solution1:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n and not n & (n-1)

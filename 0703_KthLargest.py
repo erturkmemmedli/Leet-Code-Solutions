eacbd1f41f731b35afab09dc68dda80efb3117d2
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-07-03 22:12:55 +0400

    Create 0703_KthLargest.py

---
+++ 0703_KthLargest.py
@@ -0,0 +1,57 @@
+import heapq
+
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.nums = nums
+        heapq.heapify(self.nums)
+        while len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.nums, val)
+        if len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+        return min(self.nums)
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+
+# Alterntive solution
+
+import heapq
+
+class KthLargest1:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.nums = nums
+        heapq.heapify(self.nums)
+        while len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.nums, val)
+        while len(self.nums) >= self.k:
+            pop = heapq.heappop(self.nums)
+        heapq.heappush(self.nums, pop)
+        return pop
+      
+# Alternative solution
+
+import heapq
+
+class KthLargest2:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.nums = nums
+        heapq.heapify(self.nums)
+        while len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+
+    def add(self, val: int) -> int:
+        if len(self.nums) < self.k:
+            heapq.heappush(self.nums, val)
+        elif val > self.nums[0]:
+            heapq.heapreplace(self.nums, val)
+        return self.nums[0]

cae58837f893a69ac88a2bf4a6495dfef588065e
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-07-04 12:40:34 +0400

    Update 0020_is_valid.py

--- 0020_is_valid.py
+++ 0020_is_valid.py
@@ -14,3 +14,19 @@ class Solution:
             if s in ['(', '{', '[']:
                 stack.append(s)
         return True if not stack else False
+
+# Alternative solution
+
+class Solution1:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for p in s:
+            if p in ['(', '[', '{']:
+                stack.append(p)
+            else:
+                if not stack:
+                    return False
+                pop = stack.pop()
+                if not (pop == '(' and p == ')') and not (pop == '[' and p == ']') and not (pop == '{' and p == '}'):
+                    return False
+        return True if not stack else False

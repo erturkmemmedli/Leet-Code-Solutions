c88c90910918e3b5f89664f1bb40fc72ca4adb9e
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-07-04 10:31:46 +0400

    Update 0860_lemonadeChange.py

--- 0860_lemonadeChange.py
+++ 0860_lemonadeChange.py
@@ -22,3 +22,29 @@ class Solution:
                     for _ in range(3):
                         stack_5.pop()
         return True
+
+# Alternative solution
+
+class Solution1:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        stack_5 = 0
+        stack_10 = 0
+        for bill in bills:
+            if bill == 5:
+                stack_5 += 1
+            if bill == 10:
+                if not stack_5:
+                    return False
+                stack_5 -= 1
+                stack_10 += 1
+            if bill == 20:
+                if stack_10:
+                    stack_10 -= 1
+                    if not stack_5:
+                        return False
+                    stack_5 -= 1
+                else:
+                    if len(stack_5) < 3:
+                        return False
+                    stack_5 -= 3
+        return True

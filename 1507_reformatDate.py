fce56ef266865dec1fad81eeb326771f09e90b32
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-07-04 12:47:45 +0400

    Create 1507_reformatDate.py

---
+++ 1507_reformatDate.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reformatDate(self, date: str) -> str:
+        month = {"Jan":'-01-', "Feb":'-02-', "Mar":'-03-',
+                 "Apr":'-04-', "May":'-05-', "Jun":'-06-',
+                 "Jul":'-07-', "Aug":'-08-', "Sep":'-09-',
+                 "Oct":'-10-', "Nov":'-11-', "Dec":'-12-'}
+        date = date.split()
+        if len(date[0]) == 4:
+            day = date[0][:2]
+        else:
+            day = "0" + date[0][0]
+        return date[2] + month[date[1]] + day

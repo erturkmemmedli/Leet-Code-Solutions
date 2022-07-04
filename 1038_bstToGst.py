eed9bc56de14a64dba6d3bc84e3381f0cb33f049
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-07-04 11:43:59 +0400

    Create 1038_bstToGst.py

---
+++ 1038_bstToGst.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def __init__(self):
+        self.sums = []
+    
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        self.calculate(root)
+        print(self.sums)
+        vals = [self.sums[-1]]
+        for i in range(1, len(self.sums)):
+            vals.append(self.sums[-1] - self.sums[i-1])
+        vals = vals[::-1]
+        self.inorder(root, vals)
+        return root
+        
+    def calculate(self, root):
+        if not root: return
+        self.calculate(root.left)
+        if not self.sums: self.sums.append(root.val)
+        else: self.sums.append(root.val + self.sums[-1])
+        self.calculate(root.right)
+        return root
+    
+    def inorder(self, root, vals):
+        if not root: return
+        self.inorder(root.left, vals)
+        root.val = vals.pop()
+        self.inorder(root.right, vals)
+        return root

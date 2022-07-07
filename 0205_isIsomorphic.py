a42de8ff7544a0f51da9ce89e09ce8a47447d128
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-07-07 10:58:56 +0400

    Create 0205_isIsomorphic.py

---
+++ 0205_isIsomorphic.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        hash_s = {}
+        hash_t = {}
+        for i in range(len(s)):
+            if s[i] not in hash_s:
+                hash_s[s[i]] = t[i]
+            else:
+                if hash_s[s[i]] != t[i]:
+                    return False
+            if t[i] not in hash_t:
+                hash_t[t[i]] = s[i]
+            else:
+                if hash_t[t[i]] != s[i]:
+                    return False
+        return True

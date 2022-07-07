28a96f68f316c0e358035689609681678ddcf4ee
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-07-07 12:31:27 +0400

    Update 1005_largestSumAfterKNegations.py

--- 1005_largestSumAfterKNegations.py
+++ 1005_largestSumAfterKNegations.py
@@ -21,3 +21,26 @@ class Solution:
         if k and k % 2 != 0:
             nums[-1] = -nums[-1]
         return sum(nums)                           
+
+# Alternative solution
+
+import heapq
+
+class Solution1:
+    def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+        while k > 0:
+            pop = heapq.heappop(nums)
+            if pop < 0:
+                heapq.heappush(nums, -pop)
+                k -= 1
+            elif pop == 0:
+                break
+            else:
+                if k % 2 == 0:
+                    heapq.heappush(nums, pop)
+                    break
+                else:
+                    heapq.heappush(nums, -pop)
+                    break
+        return sum(nums)

624eade2af628df8ee6e51d29604df7229ee1fde
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-07-23 22:57:50 +0400

    Update 0290_wordPattern.py

--- 0290_wordPattern.py
+++ 0290_wordPattern.py
@@ -14,3 +14,17 @@ class Solution:
             else:
                 if D2[S[i]] != char: return False
         return True
+
+# Alternative solution
+
+class Solution1:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        S = s.split()
+        if len(pattern) != len(S) or len(set(pattern)) != len(set(S)): return False
+        D = {}
+        for i, char in enumerate(pattern):
+            if char not in D:
+                D[char] = S[i]
+            else:
+                if D[char] != S[i]: return False
+        return True

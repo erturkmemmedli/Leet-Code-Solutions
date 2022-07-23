8f80f14bfd239511dee053d26cd787aca33f4bfe
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-07-23 15:04:57 +0400

    Create 0069_mySqrt.py

---
+++ 0069_mySqrt.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left = 0
+        right = x
+        while left <= right:
+            mid = (right + left) // 2
+            if mid ** 2 == x:
+                return mid
+            if mid ** 2 > x:
+                right = mid - 1
+            if mid ** 2 < x:
+                left = mid + 1
+        if left ** 2 > x: return left - 1
+        if right ** 2 < x: return right
+        return x

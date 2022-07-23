7e4276bc076d17a846f43e1ac8f2e285c3c16158
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-07-23 13:28:12 +0400

    Update 0022_generate_parenthesis.py

--- 0022_generate_parenthesis.py
+++ 0022_generate_parenthesis.py
@@ -5,3 +5,23 @@ class Solution:
             for j in range(i):
                 dp[i] += ['(' + x + ')' + y for x in dp[j] for y in dp[i - j - 1]]
         return dp[n]
+
+# Alternative solution
+
+class Solution1:
+    def generateParenthesis(self, n: int) -> List[str]:
+        output = []
+        path = ""
+        self.dfs(n, output, path)
+        return output
+        
+    def dfs(self, n, output, path):
+        if len(path) == 2 * n:
+            output.append(path)
+            return
+        for i in range(n):
+            if path.count('(') < n:
+                self.dfs(n, output, path + '(')
+            if path.count('(') > path.count(')'):
+                self.dfs(n, output, path + ')')
+            return

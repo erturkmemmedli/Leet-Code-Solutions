064978d3169e345253862cc527cefcf4455fbf0e
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-07-28 15:32:10 +0400

    Update 0189_rotate.py

--- 0189_rotate.py
+++ 0189_rotate.py
@@ -23,3 +23,20 @@ class Solution2:
             nums[i+k] = nums[i]
         for j in range(k):
             nums[j] = reserve[j]
+
+# Alternative solution
+
+class Solution3:
+    def rotate(self, nums: List[int], k: int) -> None:
+        l = len(nums)
+        k = k % l
+        if k == 0: return
+        self.reverse(nums, 0, l-1)
+        self.reverse(nums, 0, k-1)
+        self.reverse(nums, k, l-1)
+        
+    def reverse(self, nums, head, tail):
+        while head < tail:
+            nums[head], nums[tail] = nums[tail], nums[head]
+            head += 1
+            tail -= 1

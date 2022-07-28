db867b638e080036e8c0d626fbb630afcda65a8e
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-07-28 15:00:46 +0400

    Update 0189_rotate.py

--- 0189_rotate.py
+++ 0189_rotate.py
@@ -8,3 +8,18 @@ class Solution:
 class Solution1:
     def rotate(self, nums: List[int], k: int) -> None:
         nums[:] = nums[len(nums) - k % len(nums):] + nums[:len(nums) - k % len(nums)]
+
+# Alternative solution
+
+class Solution2:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if k >= len(nums): k = k % len(nums)
+        if k == 0: return
+        reserve = nums[-k:]
+        for i in range(len(nums)-k-1,-1,-1):
+            nums[i+k] = nums[i]
+        for j in range(k):
+            nums[j] = reserve[j]

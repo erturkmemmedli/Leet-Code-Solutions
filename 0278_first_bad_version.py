f521bcbfb9cbe0f765b70ac2ab855c4d3c08d153
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-07-28 16:25:12 +0400

    Update 0278_first_bad_version.py

--- 0278_first_bad_version.py
+++ 0278_first_bad_version.py
@@ -15,3 +15,20 @@ class Solution:
             return self.binary_search(mid + 1, new)
         if isBadVersion(mid):
             return self.binary_search(old, mid)
+
+# Alternative solution
+
+class Solution1:
+    def firstBadVersion(self, n: int) -> int:
+        return self.binary_search(1, n)
+        
+    def binary_search(self, l, r):
+        m = (l + r) // 2
+        if isBadVersion(m):
+            if m == l or r == l+1:
+                return m
+            return self.binary_search(l, m)
+        else:
+            if m == r or l == r-1:
+                return r
+            return self.binary_search(m, r)

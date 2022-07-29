6b409efbb8dd818c2227ed73913fba5e6b6eaedf
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-07-29 11:31:23 +0400

    Update 1128_numEquivDominoPairs.py

--- 1128_numEquivDominoPairs.py
+++ 1128_numEquivDominoPairs.py
@@ -14,3 +14,20 @@ class Solution:
             if value >= 2:
                 pairs += factorial(value) // (factorial(value-2) * 2)
         return pairs
+
+# Alternative solution
+
+from collections import defaultdict
+
+class Solution1:
+    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
+        D = defaultdict(int)
+        for domino in dominoes:
+            if domino[0] <= domino[1]:
+                D[str(domino[0]) + str(domino[1])] += 1
+            else:
+                D[str(domino[1]) + str(domino[0])] += 1
+        pairs = 0
+        for value in D.values():
+            pairs += value * (value-1) // 2
+        return pairs

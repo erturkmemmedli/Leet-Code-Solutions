144e3b01275db4f38efba3b49fd7cf41a7c28c02
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-01 10:27:54 +0400

    Update 0260_singleNumber.py

--- 0260_singleNumber.py
+++ 0260_singleNumber.py
@@ -4,3 +4,22 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
         C = Counter(nums)
         return [key for key, val in C.items() if val == 1]
+
+# Alternative solution
+
+class Solution1:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor = 0
+        for num in nums:
+            xor ^= num
+        step = 1
+        while xor & step == 0:
+            step <<= 1
+        first = 0
+        second = 0
+        for num in nums:
+            if num & step:
+                first ^= num
+            else:
+                second ^= num
+        return [first, second]

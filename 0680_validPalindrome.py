7f5ed5a1ab7c60e607538a8e8c578d669d9373e5
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-02 13:58:30 +0400

    Create 0680_validPalindrome.py

---
+++ 0680_validPalindrome.py
@@ -0,0 +1,24 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+            else:
+                break
+        part1 = s[i:j]
+        part2 = s[i+1:j+1]
+        return self.isPalindrome(part1) or self.isPalindrome(part2)
+        
+    def isPalindrome(self, s):
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+            else:
+                return False
+        return True

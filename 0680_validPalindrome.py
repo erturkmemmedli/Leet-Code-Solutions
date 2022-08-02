70986831deebac69aff25c892db31f32a1ac23e2
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-02 14:03:53 +0400

    Update 0680_validPalindrome.py

--- 0680_validPalindrome.py
+++ 0680_validPalindrome.py
@@ -22,3 +22,19 @@ class Solution:
             else:
                 return False
         return True
+
+# Alternative solution
+
+class Solution1:
+    def validPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+            else:
+                break
+        part1 = s[i:j]
+        part2 = s[i+1:j+1]
+        return part1 == part1[::-1] or part2 == part2[::-1]

a605ecf3131e3c6db35ce232363ef7b3cbd192b2
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-02 11:30:52 +0400

    Update 1232_checkStraightLine.py

--- 1232_checkStraightLine.py
+++ 1232_checkStraightLine.py
@@ -18,3 +18,14 @@ class Solution:
             if b/a != slope:
                 return False
         return True
+
+# Alternative solution
+
+class Solution1:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        x1, y1 = coordinates[0]
+        x2, y2 = coordinates[1]
+        for x, y in coordinates[2:]:
+            if (x - x2) * (y2 - y1) != (x2 - x1) * (y - y2):
+                return False
+        return True

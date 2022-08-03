b6ee650a5d56536b705bc4e8af41e9fc151fe6e9
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-03 15:35:43 +0400

    Update 0605_canPlaceFlowers.py

--- 0605_canPlaceFlowers.py
+++ 0605_canPlaceFlowers.py
@@ -27,3 +27,21 @@ class Solution:
                     else:
                         i += 1
         return count >= n
+
+# Alternative solution
+
+class Solution1:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        flowerbed.append(0)
+        count = 1
+        for flw in flowerbed:
+            if flw == 0:
+                count += 1
+            else:
+                count = 0
+            if count == 3:
+                n -= 1
+                count = 1
+            if n == 0:
+                return True
+        return False

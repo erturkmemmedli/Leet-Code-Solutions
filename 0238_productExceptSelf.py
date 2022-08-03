0aecb25dcd0b5638307312e921c52be028904290
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-03 12:41:19 +0400

    Update 0238_productExceptSelf.py

--- 0238_productExceptSelf.py
+++ 0238_productExceptSelf.py
@@ -13,3 +13,17 @@ class Solution:
             return [0 if num else product for num in nums]
         elif count == 0:
             return [product // nums[i] for i in range(len(nums))]
+           
+# Alternative solution
+
+class Solution1:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = 1
+        suffix = 1
+        result = [1] * len(nums)
+        for i in range(len(nums)):
+            result[i] *= prefix
+            prefix *= nums[i]
+            result[-1-i] *= suffix
+            suffix *= nums[-1-i]
+        return result

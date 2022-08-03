e615a1aea5201d702f104d4459bad5f182df8b3e
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-03 11:43:28 +0400

    Create 0633_judgeSquareSum.py

---
+++ 0633_judgeSquareSum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        a = int(c ** 0.5)
+        b = 0
+        while a >= b:
+            if a ** 2 + b ** 2 == c:
+                return True
+            elif a ** 2 + b ** 2 < c:
+                b += 1
+            elif a ** 2 + b ** 2 > c:
+                a -= 1
+        return False

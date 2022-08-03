a3001589174c6f91bffc33d13aa913c59970a301
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-03 15:48:13 +0400

    Update 0581_find_unsorted_subarray.py

--- 0581_find_unsorted_subarray.py
+++ 0581_find_unsorted_subarray.py
@@ -16,3 +16,25 @@ class Solution:
                     temp -= 1
                 output = i - problem + 1
         return output
+    
+# Alternative solution
+
+class Solution1:
+    def findUnsortedSubarray(self, nums: List[int]) -> int:
+        if len(nums) == 1: return 0
+        start, end, highest, stack = len(nums), len(nums) - 1, -float('inf'), []
+        for i, num in enumerate(nums):
+            if not stack:
+                stack.append(num)
+            else:
+                if num >= stack[-1]:
+                    stack.append(num)
+                    if num < highest:
+                        end = i
+                else:
+                    while stack and stack[-1] > num:
+                        highest = max(highest, stack.pop())
+                    stack.append(num)
+                    start = min(start, len(stack) - 1)
+                    end = i
+        return end - start + 1

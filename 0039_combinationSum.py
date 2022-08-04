0ee1bad2b750ad839c5be1fa54f4d09df53a5f92
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-04 17:15:08 +0400

    Update 0039_combinationSum.py

--- 0039_combinationSum.py
+++ 0039_combinationSum.py
@@ -16,3 +16,22 @@ class Solution:
             div = target // candidates[i]
             for j in range(1, div+1):
                 self.dfs(candidates[i+1:], target, output, path + [candidates[i]] * j, summ + candidates[i] * j)
+
+# Alternative solution
+
+class Solution1:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        output = set()
+        self.dfs(sorted(list(set(candidates)), reverse = True), target, output, [])
+        return output
+    
+    def dfs(self, candidates, target, output, path):
+        if target == 0:
+            output.add(tuple(path))
+            return
+        if target < 0:
+            return
+        for i in range(len(candidates)):
+            div = target // candidates[i]
+            for j in range(1, div+1):
+                self.dfs(candidates[i+1:], target - candidates[i] * j, output, path + [candidates[i]] * j)

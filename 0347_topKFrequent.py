72fa9827645afe0e04f5490b570cfbed07256062
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-04 12:08:10 +0400

    Update 0347_topKFrequent.py

--- 0347_topKFrequent.py
+++ 0347_topKFrequent.py
@@ -3,3 +3,20 @@ from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         return [key for key, val in Counter(nums).most_common(k)]
+
+# Alternative solution
+
+from heapq import heappop, heappush
+from collections import defaultdict
+
+class Solution1:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        D = defaultdict(int)
+        for num in nums:
+            D[num] += 1
+        heap = []
+        for key, val in D.items():
+            heappush(heap, (val, key))
+            if len(heap) > k:
+                heappop(heap)
+        return [b for a, b in heap]

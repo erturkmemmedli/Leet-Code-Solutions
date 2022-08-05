1861de21a843f4b19341d405ac99c2165664c37b
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-05 15:42:38 +0400

    Update 1314_matrixBlockSum.py

--- 1314_matrixBlockSum.py
+++ 1314_matrixBlockSum.py
@@ -18,3 +18,29 @@ class Solution:
         return answer
       
 # Alternative solution
+
+class Solution1:
+    def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
+        m = len(mat)
+        n = len(mat[0])
+        prefix = [[0] * n for _ in range(m)]
+        for i in range(m):
+            prefix[i][0] = mat[i][0]
+            for j in range(1, n):
+                prefix[i][j] = mat[i][j] + prefix[i][j-1]
+        for i in range(n):
+            for j in range(1, m):
+                prefix[j][i] = prefix[j][i] + prefix[j-1][i]
+        for i in range(m):
+            row_low, row_high = max(i-k, 0), min(i+k, m-1) 
+            for j in range(n):
+                col_low, col_high = max(j-k, 0), min(j+k, n-1)
+                value = prefix[row_high][col_high]
+                if row_low-1 >= 0:
+                    value -= prefix[row_low-1][col_high]
+                if col_low-1 >= 0:
+                    value -= prefix[row_high][col_low-1]
+                if row_low-1 >= 0 and col_low-1 >= 0:
+                    value += prefix[row_low-1][col_low-1]
+                mat[i][j] = value
+        return mat

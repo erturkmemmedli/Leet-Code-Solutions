c1c7274936357363a41ba61f0df4fc4c042ef191
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-09 16:30:00 +0400

    Update 0216_combination_sum_III.py

--- 0216_combination_sum_III.py
+++ 0216_combination_sum_III.py
@@ -11,3 +11,19 @@ class Solution:
             output.append(temp)
         for i in range(len(nums)):
             self.dfs(nums[i+1:], output, temp + [nums[i]], k - 1, n - nums[i])
+
+# Alternative solution
+
+class Solution1:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        output = []
+        self.dfs(k, n, output, 1, [])
+        return output
+    
+    def dfs(self, k, n, output, nxt, path):
+        if k == 0:
+            if n == 0:
+                output.append(path)
+            return
+        for i in range(nxt, 10):
+            self.dfs(k - 1, n - i, output, i + 1, path + [i])

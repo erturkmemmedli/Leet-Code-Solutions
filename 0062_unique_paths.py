e1a8b0ac225d6fa617110d13fb14f9bb45ce0603
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-09 17:16:20 +0400

    Update 0062_unique_paths.py

--- 0062_unique_paths.py
+++ 0062_unique_paths.py
@@ -5,3 +5,13 @@ class Solution:
             for j in range(1, n):
                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
         return dp[-1][-1]    
+
+# Alternative solution
+
+class Solution1:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1] * n
+        for i in range(m-1):
+            for j in range(1, n):
+                dp[j] = dp[j-1] + dp[j]
+        return dp[-1]

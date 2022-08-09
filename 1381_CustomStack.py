5b7cd79270815677271028dafe9504d4af0dd409
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-09 10:33:31 +0400

    Update 1381_CustomStack.py

--- 1381_CustomStack.py
+++ 1381_CustomStack.py
@@ -19,3 +19,27 @@ class CustomStack:
 # obj.push(x)
 # param_2 = obj.pop()
 # obj.increment(k,val)
+
+# Alternative solution
+
+class CustomStack1:
+    def __init__(self, maxSize: int):
+        self.stack = []
+        self.inc = []
+        self.maxSize = maxSize
+
+    def push(self, x: int) -> None:
+        if len(self.stack) < self.maxSize:
+            self.stack.append(x)
+            self.inc.append(0)
+
+    def pop(self) -> int:
+        if not self.stack:
+            return -1
+        if len(self.stack) > 1:
+            self.inc[-2] += self.inc[-1]
+        return self.stack.pop() + self.inc.pop()
+
+    def increment(self, k: int, val: int) -> None:
+        if self.stack:
+            self.inc[min(k, len(self.stack)) - 1] += val

ea2a077cb7826393005cbb230c6a733357b1a5ff
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-11 17:10:30 +0400

    Create 0215_findKthLargest.py

---
+++ 0215_findKthLargest.py
@@ -0,0 +1,56 @@
+from heapq import heapify, heappop, heappush
+
+class Solution:
+    def findKthLargest(self, nums: List[List[int]], k: int) -> int:
+        neg_heap = False
+        if len(nums) // 2 >= k:
+            nums[:] = [-i for i in nums]
+            neg_heap = True
+        else:
+            k = len(nums) - k + 1
+        heapify(nums)
+        for _ in range(k-1):
+            heappop(nums)
+        pop = heappop(nums)
+        return pop if not neg_heap else -pop
+
+# Alternative solution
+
+# This solution gives TLE (Time Limit Exceeded) error beacuse construction of BST is O(nlogn) at best and O(n^2) at worst.
+# And for worst case, i.e. for sorted or inversely sorted array, time complexity of the construction of BST will be O(n^2).
+# Although solution can be improved by balancing tree with AVL Tree or Red-Black Tree algorithms, it can also give O(nlogn) at best.
+# I just write it down here because maybe I can improve it via one of the abovementioned algorithms one day.
+
+class BST:
+    def __init__(self, val, left_count, right_count, left, right):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.left_count = 0
+        self.right_count = 0
+
+class Solution:
+    def findKthLargest(self, nums: List[List[int]], k: int) -> int:
+        tree = None
+        for num in nums:
+            tree = self.constructTree(tree, num)
+        return self.traverse(tree, k, len(nums))
+        
+    def constructTree(self, tree, num):
+        if not tree:
+            return BST(num, 0, 0, None, None)
+        elif num <= tree.val:
+            tree.left_count += 1
+            tree.left = self.constructTree(tree.left, num)
+        elif num > tree.val:
+            tree.right_count += 1
+            tree.right = self.constructTree(tree.right, num)
+        return tree
+    
+    def traverse(self, tree, k, n):
+        if tree.right_count == k-1:
+            return tree.val
+        elif tree.right_count > k-1:
+            return self.traverse(tree.right, k, tree.right_count)
+        else:
+            return self.traverse(tree.left, k-tree.right_count-1, tree.left_count)

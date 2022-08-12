ed40708bd1974928515182511bdecfdc3b207b7b
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-12 14:16:49 +0400

    Update 0841_canVisitAllRooms.py

--- 0841_canVisitAllRooms.py
+++ 0841_canVisitAllRooms.py
@@ -11,3 +11,18 @@ class Solution:
                 for key in rooms[room]:
                     Q.append(key)
         return len(visited) == len(rooms)
+
+# Alternative solution
+
+class Solution1:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        visited = set()
+        self.DFS(rooms, visited, 0)
+        return len(visited) == len(rooms)
+        
+    def DFS(self, rooms, visited, room):
+        if room in visited:
+            return
+        visited.add(room)
+        for key in rooms[room]:
+            self.DFS(rooms, visited, key)

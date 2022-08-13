3fc21302537b002879342efc5fda0f93aeb11343
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-13 10:51:47 +0400

    Update 0167_twoSum.py

--- 0167_twoSum.py
+++ 0167_twoSum.py
@@ -1,5 +1,18 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(numbers) - 1
+        while numbers[i] + numbers[j] != target:
+            if numbers[i] + numbers[j] > target:
+                j -= 1
+            if numbers[i] + numbers[j] < target:
+                i += 1
+        return [i+1, j+1]
+
+# Alternative solution
+    
+class Solution1:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
         hashmap = {}
         for i, number in enumerate(numbers):
             if number not in hashmap:

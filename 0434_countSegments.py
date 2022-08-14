1900e22f9e613fe1fd2b89343772f885f898f73f
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-14 13:41:16 +0400

    Create 0434_countSegments.py

---
+++ 0434_countSegments.py
@@ -0,0 +1,15 @@
+class Solution {
+    public int countSegments(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        String[] segments = s.split(" ");
+        int count = 0;
+        for (int i = 0; i < segments.length; i++) {
+            if (segments[i] != "") {
+                count++;
+            }
+        }
+        return count;
+    }
+}

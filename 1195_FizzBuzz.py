410c2d0273d300b947b2d0c9fcee9881721c4e54
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-17 16:55:00 +0400

    Update 1195_FizzBuzz.py

--- 1195_FizzBuzz.py
+++ 1195_FizzBuzz.py
@@ -54,3 +54,97 @@ class FizzBuzz:
         self.Buzz.release()
         self.FizzBuzz.release()
         return
+
+# Alternative solution
+
+from threading import Semaphore
+
+class FizzBuzz1:
+    def __init__(self, n: int):
+        self.n = n
+        self.Fizz = Semaphore(0)
+        self.Buzz = Semaphore(0)
+        self.FizzBuzz = Semaphore(0)
+        self.Number = Semaphore(1)
+        self.done = False
+        
+    # printFizz() outputs "fizz"
+    def fizz(self, printFizz: 'Callable[[], None]') -> None:
+        while True:
+            self.Fizz.acquire()
+            if self.done: return
+            printFizz()
+            self.Number.release()
+
+    # printBuzz() outputs "buzz"
+    def buzz(self, printBuzz: 'Callable[[], None]') -> None:
+        while True:
+            self.Buzz.acquire()
+            if  self.done: return
+            printBuzz()
+            self.Number.release()
+
+    # printFizzBuzz() outputs "fizzbuzz"
+    def fizzbuzz(self, printFizzBuzz: 'Callable[[], None]') -> None:
+        while True:
+            self.FizzBuzz.acquire()
+            if  self.done: return
+            printFizzBuzz()
+            self.Number.release()     
+
+    # printNumber(x) outputs "x", where x is an integer.
+    def number(self, printNumber: 'Callable[[int], None]') -> None:
+        for i in range(1, self.n + 1):
+            self.Number.acquire()
+            if i % 15 == 0:
+                self.FizzBuzz.release()
+            elif i % 5 == 0:
+                self.Buzz.release()
+            elif i % 3 == 0:
+                self.Fizz.release()
+            else:
+                printNumber(i)
+                self.Number.release()     
+        self.Number.acquire()
+        self.done = True
+        self.Fizz.release()
+        self.Buzz.release()
+        self.FizzBuzz.release()
+        return
+    
+# Alternative solution
+
+from threading import Barrier
+
+class FizzBuzz2:
+    def __init__(self, n: int):
+        self.n = n
+        self.barrier = Barrier(4)
+        
+    # printFizz() outputs "fizz"
+    def fizz(self, printFizz: 'Callable[[], None]') -> None:
+        for i in range(1, self.n + 1):
+            if i % 3 == 0 and i % 5 != 0:
+                printFizz()
+            self.barrier.wait()
+
+    # printBuzz() outputs "buzz"
+    def buzz(self, printBuzz: 'Callable[[], None]') -> None:
+        for i in range(1, self.n + 1):
+            if i % 3 != 0 and i % 5 == 0:
+                printBuzz()
+            self.barrier.wait()
+            
+    # printFizzBuzz() outputs "fizzbuzz"
+    def fizzbuzz(self, printFizzBuzz: 'Callable[[], None]') -> None:
+        for i in range(1, self.n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                printFizzBuzz()
+            self.barrier.wait()
+
+    # printNumber(x) outputs "x", where x is an integer.
+    def number(self, printNumber: 'Callable[[int], None]') -> None:
+        for i in range(1, self.n + 1):
+            if i % 3 != 0 and i % 5 != 0:
+                printNumber(i)
+            self.barrier.wait()

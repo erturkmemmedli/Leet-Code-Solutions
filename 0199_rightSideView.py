8a00d49672ebaf08a39f3953996e631942c51494
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-17 10:55:49 +0400

    Create 0199_rightSideView.py

---
+++ 0199_rightSideView.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root: return
+        output = []
+        Q = deque([[root]])
+        while Q:
+            level = Q.popleft()
+            output.append(level[-1].val)
+            next_level = []
+            for node in level:
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            if next_level:
+                Q.append(next_level)
+        return output

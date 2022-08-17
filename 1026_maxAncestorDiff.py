c9c0bec0c91997d51defb72dbb0c5dfce53d45b3
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-17 14:08:52 +0400

    Update 1026_maxAncestorDiff.py

--- 1026_maxAncestorDiff.py
+++ 1026_maxAncestorDiff.py
@@ -10,24 +10,21 @@ class Solution:
         self.maximum = 0
         
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
-        mxm = - float('inf')
-        mnm = float('inf')
-        self.traverse(root, mxm, mnm)
+        i = float('inf')
+        self.traverse(root, -i, i, -i, i)
         return self.maximum
         
-    def traverse(self, node, mxm, mnm):
+    def traverse(self, node, mxm, mnm, mxm_temp, mnm_temp):
         if not node:
             return
-        tmp = [mxm, mnm]
         if node.left:
             mxm = max(mxm, max(node.val, node.left.val))
             mnm = min(mnm, min(node.val, node.left.val))
             self.maximum = max(self.maximum, abs(mxm-mnm))
-            left = self.traverse(node.left, mxm, mnm)
+            left = self.traverse(node.left, mxm, mnm, mxm, mnm)
         if node.right:
-            mxm = max(tmp[0], max(node.val, node.right.val))
-            mnm = min(tmp[1], min(node.val, node.right.val))
+            mxm = max(mxm_temp, max(node.val, node.right.val))
+            mnm = min(mnm_temp, min(node.val, node.right.val))
             self.maximum = max(self.maximum, abs(mxm-mnm))
-            right = self.traverse(node.right, mxm, mnm)
+            right = self.traverse(node.right, mxm, mnm, mxm, mnm)
         return node
-        

f7de6b2dd1021ef7a18f3ac8b8ac6e0f4d8bc634
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-22 13:11:41 +0400

    Update 0024_swap_pairs.py

--- 0024_swap_pairs.py
+++ 0024_swap_pairs.py
@@ -13,3 +13,16 @@ class Solution:
         node.next = head
         node.next.next = self.swapPairs(head.next)
         return node
+
+# Alternative solution
+
+class Solution1:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        first = head
+        if not first: return
+        second = head.next
+        if not second: return first
+        node = second.next
+        second.next = first
+        first.next = self.swapPairs(node)
+        return second

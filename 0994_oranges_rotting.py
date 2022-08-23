dfe406d3dfffc911bc791849d964a8c5ef800951
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-23 12:18:08 +0400

    Update 0994_oranges_rotting.py

--- 0994_oranges_rotting.py
+++ 0994_oranges_rotting.py
@@ -20,3 +20,41 @@ class Solution:
                         distance[i][j] = min(distance[i][j], distance[row][col]+1)
         minute = max([distance[r][c] for r in range(m) for c in range(n)])
         return -1 if minute == float('inf') else minute
+
+# Alternative solution
+
+from collections import deque
+
+class Solution2:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        visited = set()
+        Queue = deque()
+        orangeCount = 0
+        minute = 0
+        rottenOranges = []
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 0:
+                    continue
+                if grid[i][j] == 1:
+                    orangeCount += 1
+                if grid[i][j] == 2:
+                    orangeCount += 1
+                    rottenOranges.append((i,j))
+        Queue.append(rottenOranges)
+        while Queue:
+            rotten = Queue.popleft()
+            new_rotten_oranges = []
+            for r, c in rotten:
+                if (r, c) not in visited:
+                    visited.add((r, c))
+                    for row, col in [r-1,c], [r+1,c], [r,c-1], [r,c+1]:
+                        if 0 <= row < m and 0 <= col < n and grid[row][col] == 1 and (row, col) not in visited:
+                            grid[row][col] = 2
+                            new_rotten_oranges.append((row,col))
+            if new_rotten_oranges:
+                Queue.append(new_rotten_oranges)
+                minute += 1
+        return minute if len(visited) == orangeCount else -1

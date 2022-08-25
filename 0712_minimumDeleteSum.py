b98b90f2e65e8fc54aa8c7b7e72c3c36397263ab
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-26 00:05:44 +0400

    Update 0712_minimumDeleteSum.py

--- 0712_minimumDeleteSum.py
+++ 0712_minimumDeleteSum.py
@@ -1,4 +1,19 @@
-# Alternative solution. Time Limit Exceeded (TLE) error.
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        m = len(s1)
+        n = len(s2)
+        dp = [[0] * (n+1) for _ in range(m+1)]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if s1[i-1] != s2[j-1]:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                else:
+                    dp[i][j] = dp[i-1][j-1] + ord(s1[i-1])
+        lcs_sum = dp[-1][-1]
+        total_sum = sum(ord(i) for i in s1 + s2)
+        return total_sum - 2 * lcs_sum
+
+# Alternative solution. Time Limit Exceeded (TLE) error. Backtracking part needs to be fixed.
 
 class Solution1:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:

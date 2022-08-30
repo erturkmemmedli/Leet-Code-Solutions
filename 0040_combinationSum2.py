1a1ea35f23a9bc6f0ed5ada8a4d89a3700284ba9
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-08-30 14:47:54 +0400

    Update 0040_combinationSum2.py

--- 0040_combinationSum2.py
+++ 0040_combinationSum2.py
@@ -17,3 +17,25 @@ class Solution:
         for i in range(len(candidates)):
             for j in range(1, dictionary[candidates[i]] + 1):
                 self.dfs(candidates[i+1:], target - candidates[i] * j, dictionary, output, path + [candidates[i]] * j)
+
+# Alternative solution
+
+from collections import Counter
+
+class Solution1:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        output = []
+        candidates.sort()
+        self.dfs(candidates, target, output, [])
+        return output
+        
+    def dfs(self, candidates, target, output, path):
+        if target == 0:
+            output.append(path)
+            return
+        if target < 0:
+            return
+        for i in range(len(candidates)):
+            if i > 0 and candidates[i] == candidates[i-1]:
+                continue
+            self.dfs(candidates[i+1:], target - candidates[i], output, path + [candidates[i]])

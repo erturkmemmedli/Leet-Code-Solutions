91bc2c38b282d36c01f32b0e862ee3310084ab7a
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-09-09 13:32:23 +0400

    Update 0103_zigzagLevelOrder.py

--- 0103_zigzagLevelOrder.py
+++ 0103_zigzagLevelOrder.py
@@ -12,6 +12,37 @@ class Solution:
         if not root:
             return
         queue = deque([[root]])
+        zigzag = [[root.val]]
+        reverse = 1
+        while queue:
+            level = queue.popleft()
+            next_level = []
+            next_zigzag = []
+            for node in level:
+                if node.left:
+                    next_level.append(node.left)
+                    next_zigzag.append(node.left.val)
+                if node.right:
+                    next_level.append(node.right)
+                    next_zigzag.append(node.right.val)
+            if next_level:
+                queue.append(next_level)
+                if reverse:
+                    zigzag.append(next_zigzag[::-1])
+                else:
+                    zigzag.append(next_zigzag)
+            reverse ^= 1
+        return zigzag
+   
+# Alternative solution
+
+from collections import deque
+
+class Solution1:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return
+        queue = deque([[root]])
         zigzag = [[root]]
         reverse = 1
         while queue:
@@ -34,7 +65,7 @@ class Solution:
     
 # Alternative solution
 
-class Solution1:
+class Solution2:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return

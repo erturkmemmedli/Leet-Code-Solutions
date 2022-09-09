22dfb0ee30690120317c6558344e79f84edabbc4
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-09-09 12:09:04 +0400

    Create 0901_StockSpanner.py

---
+++ 0901_StockSpanner.py
@@ -0,0 +1,20 @@
+class StockSpanner:
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price: int) -> int:
+        if not self.stack:
+            self.stack.append((price, 1))
+        elif price < self.stack[-1][0]:
+            self.stack.append((price, 1))
+        else:
+            span = 1
+            while self.stack and price >= self.stack[-1][0]:
+                span += self.stack[-1][1]
+                self.stack.pop()
+            self.stack.append((price, span))
+        return self.stack[-1][1]
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)

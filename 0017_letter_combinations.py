781e5cace49b775a8f26e69394751552481480ec
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-09-10 01:10:35 +0400

    Update 0017_letter_combinations.py

--- 0017_letter_combinations.py
+++ 0017_letter_combinations.py
@@ -23,3 +23,29 @@ class Solution:
             return
         for digit in self.hashmap[digits[0]]:
             self.dfs(self.hashmap, output, digits[1:], num + digit)
+
+# Alternative solution
+
+class Solution1:
+    def __init__(self):
+        self.hashmap = {'2': ['a', 'b', 'c'],
+                        '3': ['d', 'e', 'f'],
+                        '4': ['g', 'h', 'i'],
+                        '5': ['j', 'k', 'l'],
+                        '6': ['m', 'n', 'o'],
+                        '7': ['p', 'q', 'r', 's'],
+                        '8': ['t', 'u', 'v'],
+                        '9': ['w', 'x', 'y', 'z']}
+        
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: return
+        output = []
+        self.dfs(digits, output, "", 0)
+        return output
+        
+    def dfs(self, digits, output, path, i):
+        if len(digits) == len(path):
+            output.append(path)
+            return
+        for char in self.hashmap[digits[i]]:
+            self.dfs(digits, output, path + char, i + 1)

486b5ad6d8c9e030275fa561245a09583515cd6f
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-09-11 14:09:50 +0400

    Update 1338_minSetSize.py

--- 1338_minSetSize.py
+++ 1338_minSetSize.py
@@ -16,3 +16,21 @@ class Solution:
             limit += heappop(h)
             answer += 1
         return answer
+
+# Alternative solution
+
+from collections import Counter
+from math import ceil
+
+class Solution1:
+    def minSetSize(self, arr: List[int]) -> int:
+        n = len(arr)
+        limit = ceil(n/2)
+        counter = Counter(arr).most_common()
+        answer = 0
+        i = 0
+        while limit > 0:
+            limit -= counter[i][1]
+            i += 1
+            answer += 1
+        return answer

f4c2aa7e8ecfd806987b486a6286d675745768ba
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-09-11 16:32:53 +0400

    Update 0075_sortColors.py

--- 0075_sortColors.py
+++ 0075_sortColors.py
@@ -25,6 +25,27 @@ class Solution:
             
 # Alternative solution
 
-class Solution:
+class Solution1:
     def sortColors(self, nums: List[int]) -> None:
         nums[:] = [0] * nums.count(0) + [1] * nums.count(1) + [2] * nums.count(2)
+
+# Alternative solution
+        
+class Solution2:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zero = 0
+        one = 0
+        for i, num in enumerate(nums):
+            if num == 2:
+                continue
+            if num == 1:
+                nums[i], nums[one] = nums[one], nums[i]
+                one += 1
+            if num == 0:
+                nums[i], nums[one] = nums[one], nums[i]
+                nums[one], nums[zero] = nums[zero], nums[one]
+                zero += 1
+                one += 1

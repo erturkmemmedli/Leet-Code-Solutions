e035f3dbe60fe8adf967be065459a3ca0f9ff153
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-09-17 22:35:40 +0400

    Update 0047_permute_unique.py

--- 0047_permute_unique.py
+++ 0047_permute_unique.py
@@ -9,3 +9,20 @@ class Solution:
             output.add(tuple(path))
         for i in range(len(nums)):
             self.dfs(nums[i+1:] + nums[:i], output, path + [nums[i]])
+
+# Alternative solution
+
+class Solution1:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        output = []
+        nums.sort()
+        self.dfs(nums, output, [])
+        return output
+    
+    def dfs(self, nums, output, path):
+        if not nums:
+            output.append(path)
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            self.dfs(nums[:i] + nums[i+1:], output, path + [nums[i]])

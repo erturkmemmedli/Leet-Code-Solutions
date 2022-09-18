3031da89f4f1750b86361545460fd061fd0f071d
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-09-18 13:28:52 +0400

    Update 0106_buildTree.py

--- 0106_buildTree.py
+++ 0106_buildTree.py
@@ -15,3 +15,22 @@ class Solution:
         root.left = self.buildTree(inorder[:index], postorder[:index])
         root.right = self.buildTree(inorder[index+1:], postorder[index:-1])
         return root
+
+# Alternative solution
+
+class Solution1:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        inorder_map = {val: i for i, val in enumerate(inorder)}
+        self.index = len(postorder)-1
+        return self.recursion(inorder_map, postorder, 0, len(postorder)-1)
+        
+    def recursion(self, inorder_map, postorder, left, right):
+        if left > right:
+            return
+        val = postorder[self.index]
+        self.index -= 1
+        root = TreeNode(val)
+        mid = inorder_map[val]
+        root.right = self.recursion(inorder_map, postorder, mid+1, right)
+        root.left = self.recursion(inorder_map, postorder, left, mid-1)
+        return root

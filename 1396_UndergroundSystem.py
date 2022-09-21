1c0dfd6ec8dc599e1e995a49b139ba9aa41af49c
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-09-21 17:30:19 +0400

    Update and rename 1396_underground_system.py to 1396_UndergroundSystem.py

--- 1396_UndergroundSystem.py
+++ 1396_UndergroundSystem.py
@@ -24,3 +24,22 @@ class UndergroundSystem(object):
 # obj.checkIn(id,stationName,t)
 # obj.checkOut(id,stationName,t)
 # param_3 = obj.getAverageTime(startStation,endStation)
+
+# Alternative solution
+
+from collections import defaultdict
+
+class UndergroundSystem1:
+    def __init__(self):
+        self.customerRecordBook = {}
+        self.flightTimeBook = defaultdict(list)
+
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
+        self.customerRecordBook[id] = (stationName, t)
+
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        place, time = self.customerRecordBook[id]
+        self.flightTimeBook[(place, stationName)].append((t - time))
+
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        result = self.flightTimeBook[(startStation, endStation)]

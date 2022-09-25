9241eb2a2f935ce22fa1e18ca5cd876eed9d5fc0
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-09-25 22:43:46 +0400

    Update 0309_maxProfit.py

--- 0309_maxProfit.py
+++ 0309_maxProfit.py
@@ -4,7 +4,8 @@ class Solution:
         k (number of transactions) is infinity here
         we have additional constraint such as cooldown
         T[i][k][0] = max(T[i-1][k][0], T[i-1][k][1] + prices[i])
-        T[i][k][1] = max(T[i-1][k][1], T[i-2][k][0] - prices[i])
+        T[i][k][1] = max(T[i-1][k][1], T[i-2][k-1][0] - prices[i]) = max(T[i-1][k][1], T[i-2][k][0] - prices[i])
+        since k - 1 = k for k = infinity
         '''
         T_i_k_0_pre = 0
         T_i_k_0 = 0

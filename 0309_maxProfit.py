b7a5292c557ecce196695d5b1fa5e85e992a4cb5
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-09-25 22:40:48 +0400

    Create 0309_maxProfit.py

---
+++ 0309_maxProfit.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        '''
+        k (number of transactions) is infinity here
+        we have additional constraint such as cooldown
+        T[i][k][0] = max(T[i-1][k][0], T[i-1][k][1] + prices[i])
+        T[i][k][1] = max(T[i-1][k][1], T[i-2][k][0] - prices[i])
+        '''
+        T_i_k_0_pre = 0
+        T_i_k_0 = 0
+        T_i_k_1 = -float('inf')
+        for price in prices:
+            T_i_k_0_old = T_i_k_0
+            T_i_k_0 = max(T_i_k_0, T_i_k_1 + price)
+            T_i_k_1 = max(T_i_k_1, T_i_k_0_pre - price)
+            T_i_k_0_pre = T_i_k_0_old
+        return T_i_k_0

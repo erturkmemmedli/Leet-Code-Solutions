7b3b9eeb7d3ddcbcb4e6b3d2acec7d08b9cb6755
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-09-25 17:03:02 +0400

    Create 0153_findMin.py

---
+++ 0153_findMin.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        return self.binary_search(nums, 0, len(nums) - 1)
+        
+    def binary_search(self, nums, left, right):
+        mid = (left + right) // 2
+        val = nums[mid]
+        if left == right:
+            return val
+        if val >= nums[0] and nums[0] > nums[-1]:
+            return self.binary_search(nums, mid + 1, right)
+        else:
+            return self.binary_search(nums, left, mid)

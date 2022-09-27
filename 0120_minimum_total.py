14f989a2f8cce446cc1ed1196017083ac15c303a
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-09-27 10:40:53 +0400

    Update 0120_minimum_total.py

--- 0120_minimum_total.py
+++ 0120_minimum_total.py
@@ -6,3 +6,15 @@ class Solution:
             for j in range(len(triangle[i])):
                 dp[i][j + 1] = triangle[i][j] + min(dp[i-1][j], dp[i-1][j + 1])
         return min(dp[-1])
+
+# Alterntative solution
+
+class Solution1:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        base = triangle.pop()
+        while triangle:
+            new_base = triangle.pop()
+            for i in range(len(new_base)):
+                new_base[i] += min(base[i], base[i+1])
+            base = new_base
+        return base[0]

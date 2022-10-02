2ea5a6f57fc5f9ac076b3d5fda91faffa9f85255
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-02 18:32:48 +0400

    Update 1094_carPooling.py

--- 1094_carPooling.py
+++ 1094_carPooling.py
@@ -9,3 +9,21 @@ class Solution:
             locations[i] += locations[i-1]
             if locations[i] > capacity: return False
         return True
+
+# Alternative solution
+
+from heapq import heappush, heappop
+
+class Solution1:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        trips.sort(key = lambda x: x[1])
+        heap = []
+        current = 0
+        for num, fr, to in trips:
+            while heap and heap[0][0] <= fr:
+                pop = heappop(heap)
+                current -= pop[1]
+            current += num
+            if current > capacity: return False
+            heappush(heap, (to, num))
+        return True

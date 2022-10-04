a0d10c0dac5ca6d756938b2880a6f7c4cfd51709
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-04 12:21:00 +0400

    Update 0148_sortList.py

--- 0148_sortList.py
+++ 0148_sortList.py
@@ -9,7 +9,7 @@ class Solution:
         temp, length = head, 0
         while temp:
             temp, length = temp.next, length + 1
-        if length  <= 1:
+        if length <= 1:
             return head
         mid = length // 2
         prev, temp, length = None, head, mid
@@ -19,6 +19,48 @@ class Solution:
             prev.next = None
         A = self.sortList(head)
         B = self.sortList(temp)
+        return self.merge(A, B)
+
+    def merge(self, A, B):
+        if A.val < B.val:
+            head = A
+            A = A.next
+        else:
+            head = B
+            B = B.next
+        temp = head
+        while A and B:
+            if A.val < B.val:
+                temp.next = A
+                A = A.next
+            else:
+                temp.next = B
+                B = B.next
+            temp = temp.next
+        if A:
+            temp.next = A
+        if B:
+            temp.next = B
+        return head
+
+# Alternative solution
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution1:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        slow, fast = head, head
+        while fast and fast.next:
+            prev, slow, fast = slow, slow.next, fast.next.next
+        prev.next = None
+        A = self.sortList(head)
+        B = self.sortList(slow)
         return self.merge(A, B)
 
     def merge(self, A, B):

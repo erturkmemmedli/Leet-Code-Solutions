f18c76c2229a612c00dcbb9ffbbc7ea04f10b4d4
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-04 21:46:11 +0400

    Update 0437_pathSum.py

--- 0437_pathSum.py
+++ 0437_pathSum.py
@@ -26,3 +26,29 @@ class Solution:
         for i in range(len(self.path) - 1):
             self.path[i] += node.val
         self.path.pop()
+
+# Alternative solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution1:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        if not root: return 0
+        self.result = 0
+        self.dfs(root, targetSum, 0, collections.defaultdict(int, {0:1}))
+        return self.result
+
+    def dfs(self, node, targetSum, currentSum, memo):
+        if not node: return
+        currentSum += node.val
+        previousSum = currentSum - targetSum
+        self.result += memo[previousSum]
+        memo[currentSum] += 1
+        self.dfs(node.left, targetSum, currentSum, memo)
+        self.dfs(node.right, targetSum, currentSum, memo)
+        memo[currentSum] -= 1

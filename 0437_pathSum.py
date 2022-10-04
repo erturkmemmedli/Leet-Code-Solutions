fb21b17e9cd7a4c2c7104a369949959a61b4a135
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-04 21:46:34 +0400

    Update 0437_pathSum.py

--- 0437_pathSum.py
+++ 0437_pathSum.py
@@ -29,13 +29,6 @@ class Solution:
 
 # Alternative solution
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 class Solution1:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         if not root: return 0

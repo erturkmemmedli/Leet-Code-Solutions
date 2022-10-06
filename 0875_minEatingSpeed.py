02e2d74665551fd0b885cf3a973cf04447799c53
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-06 16:36:03 +0400

    Update 0875_minEatingSpeed.py

--- 0875_minEatingSpeed.py
+++ 0875_minEatingSpeed.py
@@ -17,3 +17,21 @@ class Solution:
                 remind = min(remind, mid)
                 right = mid - 1
         return remind if remind != math.inf else left
+
+# Alternative solution
+
+class Solution1:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        if h == len(piles):
+            return max(piles)
+        left = 1
+        right = max(piles)
+        while left < right:
+            mid = (left + right) // 2
+            s = sum([math.ceil(pile / mid) for pile in piles])
+            print(mid, s)
+            if s > h:
+                left = mid + 1
+            else:
+                right = mid
+        return left

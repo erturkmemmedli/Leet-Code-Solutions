552e55e8d034d302c24565a3aebd62a5292785ae
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-06 09:19:23 +0400

    Update 0611_triangleNumber.py

--- 0611_triangleNumber.py
+++ 0611_triangleNumber.py
@@ -1,4 +1,17 @@
-
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        output = 0
+        for i in range(2, len(nums)):
+            left = 0
+            right = i - 1
+            while left < right:
+                if nums[left] + nums[right] > nums[i]:
+                    output += right - left
+                    right -= 1
+                else:
+                    left += 1
+        return output
 
 # Alternative solution (Brute Force which gives TLE error)
 

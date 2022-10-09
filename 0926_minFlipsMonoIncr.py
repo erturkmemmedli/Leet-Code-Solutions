c0ffb742cc4afb498ed2ba245fde7329cd03d4b1
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-09 18:16:53 +0400

    Create 0926_minFlipsMonoIncr.py

---
+++ 0926_minFlipsMonoIncr.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minFlipsMonoIncr(self, s: str) -> int:
+        zeros = 0
+        ones = 0
+        flip = 0
+        if s[0] == '0': zeros += 1
+        else: ones += 1
+        for i in range(1, len(s)):
+            if s[i] == '0':
+                if i > 0 and zeros < ones:
+                    flip = min(flip + 1, ones)
+                zeros += 1
+            if s[i] == '1':
+                zeros = 0
+                ones += 1
+        return flip

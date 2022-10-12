ae088cc574521337ef009de228c491bc129b6c43
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-12 12:35:33 +0400

    Update 0720_longestWord.py

--- 0720_longestWord.py
+++ 0720_longestWord.py
@@ -17,3 +17,39 @@ class Solution:
                 else:
                     out = word
         return out if out != '~' else ""
+
+# Alternative solution
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+    
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        node = self.root
+        flag = True
+        for i, char in enumerate(word):
+            if char not in node.children:
+                if i < len(word) - 1:
+                    flag = False
+                    break
+                else:
+                    node.children[char] = TrieNode()
+            node = node.children[char]
+        return flag
+
+class Solution1:
+    def longestWord(self, words: List[str]) -> str:
+        longest = ""
+        words.sort(key = len)
+        trie = Trie()
+        for word in words:
+            if trie.insert(word):
+                if len(word) == len(longest):
+                    longest = min(longest, word)
+                else:
+                    longest = max(longest, word, key = len)
+        return longest

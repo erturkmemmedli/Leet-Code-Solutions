2e5bf0840d692fcb95c3c02d281dbd5c84655a1f
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-13 10:41:13 +0400

    Update 0210_findOrder.py

--- 0210_findOrder.py
+++ 0210_findOrder.py
@@ -33,4 +33,35 @@ class Solution:
     def topologicalSort(self):
         order = sorted(self.postorder, key = lambda x: x[1], reverse = True)
         order = [index for index, node in order]
+        
+# Alternative solution
+
+class Solution1:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # construct the graph inversely
+        self.graph = [[] for _ in range(numCourses)]
+        for a, b in prerequisites:
+            self.graph[a].append(b)
+        # cycle detection and topological sort
+        self.cycleFound = 0
+        self.visited = [0] * numCourses
+        self.postorder = []
+        for i in range(numCourses):
+            if self.cycleFound == 1:
+                return []
+            if self.visited[i] == 0:
+                self.cycleDetection(i)
+        return self.postorder
+
+    def cycleDetection(self, node):
+        if self.cycleFound == 1:
+            return
+        if self.visited[node] == 1:
+            self.cycleFound = 1
+        if self.visited[node] == 0:
+            self.visited[node] = 1
+            for neighbor in self.graph[node]:
+                self.cycleDetection(neighbor)
+            self.visited[node] = 2
+            self.postorder.append(node)
         return order

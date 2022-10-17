2ff3053c1437eab593892eb220f214f6829f3a1a
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-17 12:29:07 +0400

    Create 0450_deleteNode.py

---
+++ 0450_deleteNode.py
@@ -0,0 +1,82 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        nodeToDelete, parent = self.binarySearch(root, None, key)
+        if nodeToDelete is None:
+            return root
+        if not nodeToDelete.left and not nodeToDelete.right:
+            if not parent:
+                return None
+            else:
+                if nodeToDelete == parent.left:
+                    parent.left = None
+                if nodeToDelete == parent.right:
+                    parent.right = None
+            return root
+        elif not nodeToDelete.left:
+            if not parent:
+                return root.right
+            else:
+                if nodeToDelete == parent.left:
+                    parent.left = nodeToDelete.right
+                if nodeToDelete == parent.right:
+                    parent.right = nodeToDelete.right
+            return root
+        elif not nodeToDelete.right:
+            if not parent:
+                return root.left
+            else:
+                if nodeToDelete == parent.left:
+                    parent.left = nodeToDelete.left
+                if nodeToDelete == parent.right:
+                    parent.right = nodeToDelete.left
+            return root
+        else:
+            nodeToReplace, father = self.rightDescendant(nodeToDelete.right, nodeToDelete)
+            if not parent:
+                if nodeToReplace == nodeToDelete.right:
+                    nodeToReplace.left = nodeToDelete.left
+                    return nodeToReplace
+                else:
+                    father.left = nodeToReplace.right
+                    nodeToReplace.left = nodeToDelete.left
+                    nodeToReplace.right = nodeToDelete.right
+                    return nodeToReplace
+            else:
+                if nodeToReplace == nodeToDelete.right:
+                    nodeToReplace.left = nodeToDelete.left
+                    if nodeToDelete == parent.left:
+                        parent.left = nodeToReplace
+                    if nodeToDelete == parent.right:
+                        parent.right = nodeToReplace
+                    return root
+                else:
+                    father.left = nodeToReplace.right
+                    nodeToReplace.left = nodeToDelete.left
+                    nodeToReplace.right = nodeToDelete.right
+                    if nodeToDelete == parent.left:
+                        parent.left = nodeToReplace
+                    if nodeToDelete == parent.right:
+                        parent.right = nodeToReplace
+                    return root
+
+    def binarySearch(self, node, parent, key):
+        if not node:
+            return node, parent
+        if key > node.val:
+            return self.binarySearch(node.right, node, key)
+        elif key < node.val:
+            return self.binarySearch(node.left, node, key)
+        else:
+            return node, parent
+
+    def rightDescendant(self, node, parent):
+        if node.left:
+            return self.rightDescendant(node.left, node)
+        return node, parent

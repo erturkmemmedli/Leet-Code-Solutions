5d4314ef3310cda89da2fd2deb710bebd08b9d5e
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-18 12:20:55 +0400

    Update 0785_is_bipartite.py

--- 0785_is_bipartite.py
+++ 0785_is_bipartite.py
@@ -21,3 +21,33 @@ class Solution:
                             if (color[vertex] == 'W' and color[path] == 'W') or (color[vertex] == 'B' and color[path] == 'B'):
                                 return False
         return True
+
+# Alternative solution
+
+class Solution1:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        self.visited = set()
+        self.color = [0] * len(graph) # all nodes are colorless at start
+        for i in range(len(graph)):
+            if i not in self.visited:
+                self.color[i] = 1 # color initializes as 1 - Red
+                if not self.bfs(graph, i):
+                    return False
+        return True
+            
+    def bfs(self, graph, i):
+        queue = collections.deque([i])
+        while queue:
+            node = queue.popleft()
+            if node not in self.visited:
+                self.visited.add(node)
+                for neighbor in graph[node]:
+                    if self.color[node] == self.color[neighbor]: # same neighboring colors means not bipartite
+                        return False
+                    if neighbor not in self.visited:
+                        if self.color[node] == 1: # if node is 1 - Red, neigbor is 2 - Black
+                            self.color[neighbor] = 2
+                        if self.color[node] == 2:
+                            self.color[neighbor] = 1 # if node is 2 - Black, neigbor is 1 - Red
+                        queue.append(neighbor)
+        return True

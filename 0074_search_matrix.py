b05079d61c7a22e8b2c90132319354e51beff44a
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-19 23:34:54 +0400

    Update 0074_search_matrix.py

--- 0074_search_matrix.py
+++ 0074_search_matrix.py
@@ -33,3 +33,13 @@ class Solution:
             if target < matrix[index][mid]:
                 right = mid - 1
         return False
+
+# Alternative solution
+
+class Solution1:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        firstIndices = [i[0] for i in matrix]
+        row = bisect.bisect_left(firstIndices, target) - 1
+        if row < 0: return True if matrix[row + 1][0] == target else False
+        column = bisect.bisect_left(matrix[row], target)
+        return matrix[row][column] == target

4dc35eb585daa520b190fe5b56e1e004ba60ed1d
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-21 15:44:12 +0400

    Update and rename 00873_lenLongestFibSubseq.py to 0873_lenLongestFibSubseq.py

--- 0873_lenLongestFibSubseq.py
+++ 0873_lenLongestFibSubseq.py
@@ -17,3 +17,15 @@ class Solution:
                         temp += 1
                 length = max(length, temp)
         return length
+
+# Alternative solution
+
+class Solution1:
+    def lenLongestFibSubseq(self, arr: List[int]) -> int:
+        hashset = set(arr)
+        hashmap = collections.defaultdict(int)
+        for i in range(2, len(arr)):
+            for j in range(1,i):
+                if arr[i] - arr[j] < arr[j] and arr[i] - arr[j] in hashset:
+                    hashmap[(arr[i], arr[j])] = hashmap.get((arr[j], arr[i] - arr[j]), 2) + 1
+        return max(hashmap.values() or [0])

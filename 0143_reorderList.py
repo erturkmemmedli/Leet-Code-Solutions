ba87e1294a21fca1683351ed1aeb7386fa3ddad5
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-23 16:57:29 +0400

    Update 0143_reorderList.py

--- 0143_reorderList.py
+++ 0143_reorderList.py
@@ -47,3 +47,28 @@ class Solution:
             return
         node1.next = self.recursiveBuild(node2, node1.next)
         return node1
+
+# Alternative solution
+
+class Solution1:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+        temp, slow.next = slow.next, None
+        secondHead = self.reverseList(temp)
+        return self.recursiveBuild(head, secondHead)
+
+    def reverseList(self, node):
+        prev, curr = None, node
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next
+        return prev
+
+    def recursiveBuild(self, node1, node2):
+        if not node1 and not node2: return
+        node1.next = self.recursiveBuild(node2, node1.next)
+        return node1

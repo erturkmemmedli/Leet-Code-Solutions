64500597355664491dffe6715be9a1489d4a8a14
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-23 16:33:28 +0400

    Create 0143_reorderList.py

---
+++ 0143_reorderList.py
@@ -0,0 +1,49 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head and not head.next:
+            return head
+        # find length of linked list
+        n = 0
+        temp = head
+        while temp:
+            n += 1
+            temp = temp.next
+        # find mid of linked list
+        mid = (n + 1) // 2
+        n = 0
+        prev = None
+        temp = head
+        while n < mid:
+            n += 1
+            prev = temp
+            temp = temp.next
+        # seperate into two parts
+        prev.next = None
+        secondHead = self.reverseList(temp)
+        # merge these parts
+        return self.recursiveBuild(head, secondHead)
+
+    def reverseList(self, node):
+        prev = None
+        curr = node
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
+
+    def recursiveBuild(self, node1, node2):
+        if not node1 and not node2:
+            return
+        node1.next = self.recursiveBuild(node2, node1.next)
+        return node1

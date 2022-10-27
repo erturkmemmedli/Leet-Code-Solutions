68a4e29e1e67f2b5194be78dc47cc9dcb972b8a1
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-27 23:56:51 +0400

    Update 0093_restoreIpAddresses.py

--- 0093_restoreIpAddresses.py
+++ 0093_restoreIpAddresses.py
@@ -58,3 +58,33 @@ class Solution1:
                     return
                 else:
                     self.dfs([], path + [string])
+
+# Alternative solution
+
+class Solution2:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        if len(s) < 4 or len(s) > 12:
+            return
+        self.output = set()
+        self.dfs(s, "", 0)
+        return self.output
+
+    def dfs(self, string, path, step):
+        print(string, path)
+        if path and path[-1] != '.':
+            self.output.add(path)
+            return
+        for _ in range(4):
+            if step < 3:
+                if string and string[0] != '0':
+                    self.dfs(string[1:], path + string[:1] + '.', step + 1)
+                    self.dfs(string[2:], path + string[:2] + '.', step + 1)
+                    if int(string[:3]) <= 255:
+                        self.dfs(string[3:], path + string[:3] + '.', step + 1)
+                else:
+                    self.dfs(string[1:], path + string[:1] + '.', step + 1)
+            else:
+                if len(string) == 0 or len(string) > 3 or (len(string) > 1 and string[0] == '0') or int(string[:3]) > 255:
+                    return
+                else:
+                    self.dfs("", path + string, step + 1)

05e9a831fc964603d89282d6e12ec596ae4fc5f1
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-27 14:09:49 +0400

    Create 1081_smallestSubsequence.py

---
+++ 1081_smallestSubsequence.py
@@ -0,0 +1,17 @@
+from collections import defaultdict, deque
+
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        indexMap = defaultdict(deque)
+        for i, char in enumerate(s):
+            indexMap[char].append(i)
+        characters = []
+        for i, char in enumerate(s):
+            if char in characters:
+                indexMap[char].popleft()
+                continue
+            while characters and char < characters[-1] and indexMap[characters[-1]]:
+                characters.pop()
+            characters.append(char)
+            indexMap[char].popleft()
+        return "".join(characters)

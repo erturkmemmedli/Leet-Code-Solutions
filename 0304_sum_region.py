9e6301dc0b4d6d29aac0a134fd7f2c9afe448ae4
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-29 00:54:08 +0400

    Update 0304_sum_region.py

--- 0304_sum_region.py
+++ 0304_sum_region.py
@@ -22,6 +22,10 @@ class NumMatrix:
         else:  
             return self.H[(row2, col2)] + self.H[(row1-1, col1-1)] - self.H[(row1-1, col2)] - self.H[(row2, col1-1)]
 
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
+
 # Alternative solution
 
 class NumMatrix1:
@@ -46,3 +50,27 @@ class NumMatrix1:
             return self.matrix[row2][col2] - self.matrix[row1-1][col2]
         else:  
             return self.matrix[row2][col2] + self.matrix[row1-1][col1-1] - self.matrix[row1-1][col2] - self.matrix[row2][col1-1] 
+
+# Alternative solution
+
+class NumMatrix2:
+    def __init__(self, matrix: List[List[int]]):
+        m = len(matrix)
+        n = len(matrix[0])
+        self.M = matrix
+        for i in range(m):
+            for j in range(1, n):
+                self.M[i][j] += self.M[i][j-1]
+        for i in range(1, m):
+            for j in range(n):
+                self.M[i][j] += self.M[i-1][j]
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        result = self.M[row2][col2]
+        if row1 > 0 and col1 > 0:
+            result += self.M[row1-1][col1-1]
+        if col1 > 0:
+            result -= self.M[row2][col1-1]
+        if row1 > 0:
+            result -= self.M[row1-1][col2]
+        return result

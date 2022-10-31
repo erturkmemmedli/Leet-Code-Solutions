91ca0f91f6f6cf84900a22c437322a3d7f2073f5
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-31 12:12:07 +0400

    Update 0055_canJump.py

--- 0055_canJump.py
+++ 0055_canJump.py
@@ -1,6 +1,6 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        mxm_dist = mxm_dist=0
+        mxm_dist = 0
         for i in range(len(nums) - 1):
             mxm_dist = max(mxm_dist - 1, nums[i])
             if mxm_dist == 0:

044a6011270c93260628b8fe8fffb4a78fd192a8
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-31 11:38:47 +0400

    Update 0055_canJump.py

--- 0055_canJump.py
+++ 0055_canJump.py
@@ -6,3 +6,17 @@ class Solution:
             if mxm_dist == 0:
                 return False
         return True
+
+# Alternative solution (which gives TLE error)
+
+class Solution1:
+    def canJump(self, nums: List[int]) -> bool:
+        bfsQueue = collections.deque([(0, nums[0])])
+        while bfsQueue:
+            index, distance = bfsQueue.popleft()
+            if index == len(nums) - 1:
+                return True
+            for i in range(distance, 0, -1):
+                if index + i == len(nums) - 1 or (index + i < len(nums) and nums[index + i] != 0):
+                    bfsQueue.append((index + i, nums[index + i]))
+        return False

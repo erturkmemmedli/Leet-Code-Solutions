c8030123700637c3642f65784de4e6fb27939917
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-11-02 14:05:24 +0400

    Update 1042_gardenNoAdj.py

--- 1042_gardenNoAdj.py
+++ 1042_gardenNoAdj.py
@@ -16,3 +16,28 @@ class Solution:
                     flowers[i] = j
                     break
         return flowers
+
+# Alternative solution
+
+class Solution1:
+    def gardenNoAdj(self, n: int, paths: List[List[int]]) -> List[int]:
+        graph = [[] for _ in range(n)]
+        for a, b in paths:
+            graph[a - 1].append(b - 1)
+            graph[b - 1].append(a - 1)
+        flowers = [0] * n
+        mapping = {i : {1, 2, 3, 4} for i in range(n)}
+        for i in range(n):
+            if flowers[i] == 0:
+                queue = collections.deque([(i, mapping[i])])
+                while queue:
+                    node, choice = queue.popleft()
+                    if flowers[node]:
+                        continue
+                    select = choice.pop()
+                    flowers[node] = select
+                    for neighbor in graph[node]:
+                        if flowers[neighbor] == 0:
+                            mapping[neighbor] &= ({1, 2, 3, 4} - {select})
+                            queue.append((neighbor, mapping[neighbor]))
+        return flowers

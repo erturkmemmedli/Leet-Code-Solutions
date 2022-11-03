0a8c950b0c7509c83539de4a096385184db88522
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-11-03 10:06:16 +0400

    Update 0368_largestDivisibleSubset.py

--- 0368_largestDivisibleSubset.py
+++ 0368_largestDivisibleSubset.py
@@ -1,6 +1,29 @@
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        nums.sort()
+        dp = [[1, i] for i in range(n)]
+        maximum = 0
+        maximumIndex = 0
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[j] % nums[i] == 0:
+                    if dp[j][0] < dp[i][0] + 1:
+                        dp[j][0] = dp[i][0] + 1 
+                        dp[j][1] = i
+                    if maximum < dp[j][0]:
+                        maximum = dp[j][0]
+                        maximumIndex = j
+        result = collections.deque()
+        while dp[maximumIndex][0] > 1:
+            result.appendleft(nums[maximumIndex])
+            maximumIndex = dp[maximumIndex][1]
+        result.appendleft(nums[maximumIndex])
+        return result
+
 # Alternatice solution (which gives TLE error)
 
-class Solution:
+class Solution1:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         nums.sort()
         self.output = []

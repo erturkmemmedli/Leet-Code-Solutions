3ea388e8048f8c023eaa6ba335b3703f7451a461
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-11-04 14:08:11 +0400

    Update 0732_MyCalendarThree.py

--- 0732_MyCalendarThree.py
+++ 0732_MyCalendarThree.py
@@ -40,3 +40,30 @@ class MyCalendarThree1:
             self.updateSegmentTreeWithLazyPropagation(start, end, left, mid, 2 * index)
             self.updateSegmentTreeWithLazyPropagation(start, end, mid, right, 2 * index + 1)
             self.segment[index] = self.lazy[index] + max(self.segment[2 * index], self.segment[2 * index + 1])
+
+# Alternative solution
+
+from sortedcontainers import SortedList
+
+class MyCalendarThree2:
+    def __init__(self):
+        self.list = SortedList([[0, 0]])
+        self.result = 0
+
+    def book(self, startTime: int, endTime: int) -> int:
+        self.construct(startTime)
+        self.construct(endTime)
+        for interval in self.list.irange([startTime, 0], [endTime, 0], inclusive = (True, False)):
+            interval[1] += 1
+            self.result = max(self.result, interval[1])
+        return self.result
+
+    def construct(self, new):
+        index = bisect.bisect_left(self.list, [new, 0])
+        if index < len(self.list) and self.list[index][0] == new:
+            return
+        self.list.add([new, self.list[index - 1][1]])
+
+# Your MyCalendarThree object will be instantiated and called as such:
+# obj = MyCalendarThree()
+# param_1 = obj.book(startTime,endTime)

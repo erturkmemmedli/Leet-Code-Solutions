5325549acf4ea7cc50baa09c3f1fa45f85638a84
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-11-10 17:50:05 +0400

    Create 0081_search.py

---
+++ 0081_search.py
@@ -0,0 +1,21 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return True
+            while left < mid and nums[mid] == nums[left]:
+                left += 1
+            if nums[mid] >= nums[left]:
+                if nums[mid] > target >= nums[left]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return False

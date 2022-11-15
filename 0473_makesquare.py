68d5b9a293c5a8d3f7bafb49df4f229370741dc7
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-11-15 13:53:13 +0400

    Update 0473_makesquare.py

--- 0473_makesquare.py
+++ 0473_makesquare.py
@@ -6,6 +6,38 @@ class Solution:
         side = perimeter // 4
         matchsticks.sort(reverse = True)
         if matchsticks[0] > side: return False
+
+        @lru_cache
+        def dfs(target, s1, s2, s3, s4, index):
+            if index == len(matchsticks):
+                if s1 == s2 == s3 == s4:
+                    return True
+            if s1 + matchsticks[index] <= target:
+                if dfs(target, s1 + matchsticks[index], s2, s3, s4, index + 1):
+                    return True
+            if s2 + matchsticks[index] <= target:
+                if dfs(target, s1, s2 + matchsticks[index], s3, s4, index + 1):
+                    return True
+            if s3 + matchsticks[index] <= target:
+                if dfs(target, s1, s2, s3 + matchsticks[index], s4, index + 1):
+                    return True
+            if s4 + matchsticks[index] <= target:
+                if dfs(target, s1, s2, s3, s4 + matchsticks[index], index + 1):
+                    return True
+            return False
+
+        return dfs(side, 0, 0, 0, 0, 0)
+
+# Alternative solution (which does not include cache so gives TLE error)
+
+class Solution1:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        if len(matchsticks) < 4: return False
+        perimeter = sum(matchsticks)
+        if perimeter % 4 != 0: return False
+        side = perimeter // 4
+        matchsticks.sort(reverse = True)
+        if matchsticks[0] > side: return False
         square = [0] * 4
         return self.dfs(matchsticks, side, square, 0)
     

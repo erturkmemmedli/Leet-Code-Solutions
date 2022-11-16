7f43a2ed26d41b52375d0d7a18fa3bda96eb11e8
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-11-16 16:58:39 +0400

    Update 0849_maxDistToClosest.py

--- 0849_maxDistToClosest.py
+++ 0849_maxDistToClosest.py
@@ -15,3 +15,18 @@ class Solution:
             if idx == len(seats) - 1:
                 distance = max(distance, idx - i)
         return distance
+
+# Alternative solution
+
+class Solution1:
+    def maxDistToClosest(self, seats: List[int]) -> int:
+        occupiedSeats = []
+        for i, seat in enumerate(seats):
+            if seat == 1:
+                occupiedSeats.append(i)
+        maxDistance = occupiedSeats[0]
+        for i in range(len(occupiedSeats) - 1):
+            distance = occupiedSeats[i+1] - occupiedSeats[i]
+            maxDistance = max(maxDistance, distance // 2)
+        maxDistance = max(maxDistance, len(seats) - occupiedSeats[-1] - 1)
+        return maxDistance

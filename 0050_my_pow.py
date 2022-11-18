3f24e601b9b406ece669fb263ef7b0e08740ceb4
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-11-18 16:25:04 +0400

    Update 0050_my_pow.py

--- 0050_my_pow.py
+++ 0050_my_pow.py
@@ -10,3 +10,9 @@ class Solution1:
         if n < 0: return 1 / self.myPow(x, -n)
         if n % 2: return x * self.myPow(x, n - 1)
         else: return self.myPow(x * x, n // 2)
+
+# Alternative solution
+
+class Solution2:
+    def myPow(self, x: float, n: int) -> float:
+        return pow(x, n)

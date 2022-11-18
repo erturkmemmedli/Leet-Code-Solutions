d37d7ea98cefa7056fe4d3e377109f69c5176c0f
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-11-18 16:17:32 +0400

    Create 0649_predictPartyVictory.py

---
+++ 0649_predictPartyVictory.py
@@ -0,0 +1,54 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        while senate:
+            c = collections.Counter(senate)
+            if len(c) == 1:
+                return "Radiant" if "R" in c else "Dire"
+            senate, radiant, dire = self.vote(senate)
+            if radiant:
+                if radiant >= c["D"]:
+                    return "Radiant"
+                else:
+                    temp = []
+                    for party in senate:
+                        if radiant and party == "D":
+                            radiant -= 1
+                        else:
+                            temp.append(party)
+                    senate = temp
+            if dire:
+                if dire >= c["R"]:
+                    return "Dire"
+                else:
+                    temp = []
+                    for party in senate:
+                        if dire and party == "R":
+                            dire -= 1
+                        else:
+                            temp.append(party)
+                    senate = temp
+
+    def vote(self, senate):
+        def helper(candidate, first, second):
+            if first > 0:
+                first -= 1
+            else:
+                stack.append(candidate)
+                second += 1
+            return first, second
+        stack = []
+        radiant = 0
+        dire = 0
+        for party in senate:
+            if not stack or party == stack[-1]:
+                if party == "R":
+                    radiant += 1
+                if party == "D":
+                    dire += 1
+                stack.append(party)
+            else:
+                if party == "R":
+                    dire, radiant = helper(party, dire, radiant)
+                if party == "D":
+                    radiant, dire = helper(party, radiant, dire)
+        return stack, radiant, dire

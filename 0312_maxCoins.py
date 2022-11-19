846d9f232a5bdc54578c5f6f9930f2c9fd51eb79
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-11-19 13:23:03 +0400

    Create 0312_maxCoins.py

---
+++ 0312_maxCoins.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        nums = [1] + nums + [1]
+        n = len(nums)
+        dp = [[0] * n for _ in range(n)]
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 2, n):
+                dp[i][j] = max(nums[i] * nums[j] * nums[k] + dp[i][k] + dp[k][j] for k in range(i + 1, j))
+        return dp[0][-1]

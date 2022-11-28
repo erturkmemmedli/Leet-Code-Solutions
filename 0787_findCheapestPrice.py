08524b154fb2bbe795975564c784a96425f89bcb
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-11-28 11:58:28 +0400

    Update 0787_findCheapestPrice.py

--- 0787_findCheapestPrice.py
+++ 0787_findCheapestPrice.py
@@ -14,3 +14,23 @@ class Solution:
                     heapq.heappush(heap, (price + cost[node][stop], child, stop + 1))
         result = min(cost[dst])
         return result if result != inf else -1
+
+# Alternative solution
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        graph = collections.defaultdict(list)
+        for fr, to, pr in flights:
+            graph[fr].append((to, pr))
+        cost = [[inf] * (k + 2) for _ in range(n)]
+        cost[src][0] = 0
+        heap = [(0, src, 0)]
+        while heap:
+            price, node, stop = heapq.heappop(heap)
+            if node == dst:
+                return price
+            for child, price in graph[node]:
+                if stop + 1 <= k + 1 and cost[child][stop + 1] > price + cost[node][stop]:
+                    cost[child][stop + 1] = price + cost[node][stop]
+                    heapq.heappush(heap, (price + cost[node][stop], child, stop + 1))
+        return -1

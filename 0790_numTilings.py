d7ee3924946ebfbb20a5b5d9ecf6f80e5d29ce92
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-11-29 11:15:37 +0400

    Create 0790_numTilings.py

---
+++ 0790_numTilings.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numTilings(self, n: int) -> int:
+        dp = collections.deque([[1,1],[2,2]])
+        if n < 3: return dp[n-1][0]
+        for _ in range(n-2):
+            first = dp[0][0] + dp[1][0] + 2 * dp[0][1]
+            second = dp[1][0] + dp[1][1]
+            dp.popleft()
+            dp.append([first, second])
+        return dp[-1][0] % (10**9 + 7)

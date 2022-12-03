8590a17de4c8202b071b87a865f174439bf8768b
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-12-03 21:04:11 +0400

    Update 0451_frequencySort.py

--- 0451_frequencySort.py
+++ 0451_frequencySort.py
@@ -20,3 +20,9 @@ class Solution1:
         for k, v in C.most_common():
             output += k * v
         return output
+
+# Alternative solution
+
+class Solution2:
+    def frequencySort(self, s: str) -> str:
+        return "".join([key * val for key, val in collections.Counter(s).most_common()])

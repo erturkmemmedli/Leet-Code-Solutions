bd33b012a3c38d1fe8f9e65f3f61f2f0c3211e3a
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-12-06 14:03:05 +0400

    Update 0128_longestConsecutive.py

--- 0128_longestConsecutive.py
+++ 0128_longestConsecutive.py
@@ -28,3 +28,33 @@ class Solution:
                 else:
                     self.parent[db] = da
                     self.rank[da] += 1
+
+# Alternative solution
+
+class Solution1:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        self.parent = {num : num for num in nums}
+        self.rank = {num: 1 for num in nums}
+        for num in nums:
+            self.union(num, num - 1)
+            self.union(num, num + 1)
+        for num in nums:
+            self.find(num)
+        return max(self.rank.values(), default = 0)
+    
+    def find(self, x):
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, a, b):
+        if a in self.parent and b in self.parent:
+            da, db = self.find(a), self.find(b)
+            if da == db:
+                return
+            if self.rank[da] < self.rank[db]:
+                self.parent[da] = db
+                self.rank[db] += self.rank[da]
+            else:
+                self.parent[db] = da
+                self.rank[da] += self.rank[db]

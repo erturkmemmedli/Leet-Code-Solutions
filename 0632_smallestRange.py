c2390a1fc83e143679d8ee1be0d815a3d930915a
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-12-07 15:40:05 +0400

    Update 0632_smallestRange.py

--- 0632_smallestRange.py
+++ 0632_smallestRange.py
@@ -17,3 +17,23 @@ class Solution:
                     minimumDistance = maxWindow - minWindow
                     minimumDistancePair = [minWindow, maxWindow]
         return minimumDistancePair
+
+# Alternative solution
+
+class Solution1:
+    def smallestRange(self, nums: List[List[int]]) -> List[int]:      
+        numsMerged = []
+        for i, group in enumerate(nums):
+            heapq.heappush(numsMerged, (group[0], i, 0))
+        maxValue = max(group[0] for group in nums)
+        left, right = 0, float('inf')
+        while numsMerged:
+            minValue, listIndex, numIndex = heapq.heappop(numsMerged)
+            if maxValue - minValue < right - left:
+                left, right = minValue, maxValue
+            if numIndex + 1 == len(nums[listIndex]):
+                return [left, right]
+            candidate = nums[listIndex][numIndex + 1]
+            maxValue = max(maxValue, candidate)
+            heapq.heappush(numsMerged, (candidate, listIndex, numIndex + 1))
+        

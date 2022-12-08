027295fce72d4f2dad6c6bdc700f4805e5cdee6b
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-12-08 16:43:40 +0400

    Update 0842_splitIntoFibonacci.py

--- 0842_splitIntoFibonacci.py
+++ 0842_splitIntoFibonacci.py
@@ -7,21 +7,20 @@ class Solution:
     def backtrack(self, num):
         if not num and len(self.result) > 2 and self.result[-1] < 2**31:
             return True
-        if len(self.result) > 2 and len(str(self.result[-1])) > len(num):
-            return False
         if len(self.result) < 2:
             for i in range(1, len(num)//2+1):
-                if i == 1 or (i > 1 and num[0] != '0'):
-                    self.result.append(int(num[:i]))
+                nextFibo = int(num[:i])
+                if i == 1 or (i > 1 and num[0] != '0') and nextFibo < 2**31:
+                    self.result.append(nextFibo)
                     if self.backtrack(num[i:]):
                         return True
                     else:
                         self.result.pop()
         else:
             for i in range(len(str(self.result[-1])), len(num)+1):
-                nextFibonacciNumber = int(num[:i])
-                if (i == 1 or (i > 1 and num[0] != '0')) and nextFibonacciNumber == self.result[-1] + self.result[-2]:
-                    self.result.append(nextFibonacciNumber)
+                nextFibo = int(num[:i])
+                if (i == 1 or (i > 1 and num[0] != '0')) and nextFibo == self.result[-1] + self.result[-2] and nextFibo < 2**31:
+                    self.result.append(nextFibo)
                     if self.backtrack(num[i:]):
                         return True
                     else:

7b5eddf7ef93e659eb96627eb540918039b29cf1
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-12-14 11:13:44 +0400

    Update 1461_hasAllCodes.py

--- 1461_hasAllCodes.py
+++ 1461_hasAllCodes.py
@@ -7,3 +7,12 @@ class Solution:
         for i in range(bin_count):
             if i not in hashset: return False
         return True
+
+# Alternative solution
+
+class Solution1:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        hashset = set()
+        for i in range(0, len(s)-k+1):
+            hashset.add(s[i:i+k])
+        return len(hashset) == 2 ** k

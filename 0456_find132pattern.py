297b5c97b17e783534d0bd121558a023c44fd72c
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-12-19 16:15:41 +0400

    Update 0456_find132pattern.py

--- 0456_find132pattern.py
+++ 0456_find132pattern.py
@@ -9,3 +9,29 @@ class Solution(object):
                 k = stack.pop()
             stack.append(nums[i])
         return False
+
+# Alternative solution
+
+class Solution1:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack = []
+        mini, maxi = None, None
+        for num in nums:
+            if mini == None or (maxi == None and num <= mini):
+                mini = num
+            elif (maxi == None and num > mini) or num > maxi:
+                maxi = num
+                while stack:
+                    if num <= stack[-1][0]:
+                        break
+                    elif stack[-1][0] < num < stack[-1][1]:
+                        return True
+                    else:
+                        stack.pop()
+            elif mini < num < maxi:
+                return True
+            elif num < mini:
+                stack.append([mini, maxi])
+                mini = num
+                maxi = None
+        return False

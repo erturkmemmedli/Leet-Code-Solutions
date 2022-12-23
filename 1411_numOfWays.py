5bd9a7e3a1b0fdbe498e5cb43af99fb24a27616d
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-12-23 12:35:51 +0400

    Update 1411_numOfWays.py

--- 1411_numOfWays.py
+++ 1411_numOfWays.py
@@ -1,11 +1,10 @@
 class Solution:
     def numOfWays(self, n: int) -> int:
-        sameAtEdges, allDifferent = 24, 30
-        if n == 1: return 12
-        if n == 2: return sameAtEdges + allDifferent
-        for _ in range(n-2):
-           small = sameAtEdges // 2 + allDifferent * 2 // 5
-           big = sameAtEdges // 2 + allDifferent * 3 // 5
+        sameAtEdges = allDifferent = small = big = 6
+        if n == 1: return sameAtEdges + allDifferent
+        for _ in range(n - 1):
            sameAtEdges = small * 4
            allDifferent = big * 5
+           small = sameAtEdges // 2 + allDifferent * 2 // 5
+           big = sameAtEdges // 2 + allDifferent * 3 // 5
         return (sameAtEdges + allDifferent) % 1000000007

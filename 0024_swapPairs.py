e38cdbe76e574591ecb5fd94b817870f75f5cc45
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-12-28 11:24:38 +0400

    Update 0024_swapPairs.py

--- 0024_swapPairs.py
+++ 0024_swapPairs.py
@@ -26,26 +26,3 @@ class Solution1:
         second.next = first
         first.next = self.swapPairs(node)
         return second
-
-# Alternative solution
-
-class Solution2:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        heap = []
-        for i, llist in enumerate(lists):
-            if llist:
-                heapq.heappush(heap, (llist.val, i))
-        dummy = ListNode()
-        temp = dummy
-        while heap:
-            val, idx = heapq.heappop(heap)
-            llist = lists[idx]
-            node = llist
-            llist = llist.next
-            node.next = None
-            temp.next = node
-            temp = temp.next
-            if llist: 
-                lists[idx] = llist
-                heapq.heappush(heap, (llist.val, idx))
-        return dummy.next

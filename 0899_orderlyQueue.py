6f259c20af2bbe9b9a7afd1a2427c9e0b0e09992
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-01 23:27:14 +0400

    Update 0899_orderlyQueue.py

--- 0899_orderlyQueue.py
+++ 0899_orderlyQueue.py
@@ -22,7 +22,6 @@ class Solution:
                     size = count
                     idx = curr
                 elif count == size:
-                    print(idx, curr)
                     k = 0
                     while s[(idx + size + k) % len(s)] == s[(curr + size + k) % len(s)]:
                         k += 1
@@ -31,5 +30,30 @@ class Solution:
             else:
                 count = 0
                 head = True
+        s = s[idx:] + s[:idx]
+        return s
+
+# Alternative solution
+
+class Solution1:
+    def orderlyQueue(self, s: str, k: int) -> str:
+        if k != 1: return "".join(sorted(s))
+        mini, curr, idx, count, size, head = '~', -1, -1, 0, 0, True
+        for i, char in enumerate(s):
+            if char < mini:
+                mini, idx, curr, count, size, head = char, i, i, 1, 1, False
+            elif char == mini:
+                count += 1
+                if head:
+                    curr, head = i, False
+                    if size != 1: continue
+                if count > size:
+                    size, idx = count, curr
+                elif count == size:
+                    k = 0
+                    while s[(idx + size + k) % len(s)] == s[(curr + size + k) % len(s)]: k += 1
+                    if s[(idx + size + k) % len(s)] > s[(curr + size + k) % len(s)]: idx = curr
+            else:
+                count, head = 0, True
         s = s[idx:] + s[:idx]
         return s

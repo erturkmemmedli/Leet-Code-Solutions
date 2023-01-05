8ccf4dde219ee265ff3cb428c1ef45f3dbae2caf
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-05 11:28:29 +0400

    Create 1513_numSub.py

---
+++ 1513_numSub.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numSub(self, s: str) -> int:
+        contiguousOnes = []
+        count = 0
+        for char in s:
+            if char == '1':
+                count += 1
+            else:
+                if count: contiguousOnes.append(count)
+                count = 0
+        if count: contiguousOnes.append(count)
+        result = 0
+        for num in contiguousOnes:
+            result += num * (num + 1) // 2 % 1_000_000_007
+        return result

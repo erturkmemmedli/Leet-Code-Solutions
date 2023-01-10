348717ea1ccc00eaf0249e42b88bd747bb000f55
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-11 00:16:49 +0400

    Update 0076_minWindow.py

--- 0076_minWindow.py
+++ 0076_minWindow.py
@@ -25,3 +25,33 @@ class Solution:
                         del sMap[s[left]]
                 left += 1
         return substring
+
+# Alternative solution
+
+class Solution1:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s): return ""
+        frequency = {}
+        for char in t:
+            if char not in frequency:
+                frequency[char] = 0
+            frequency[char] += 1
+        left = 0
+        startIndex = 0
+        matched = 0
+        minLength = float("inf")
+        for right, char in enumerate(s):
+            if char in frequency:
+                frequency[char] -= 1
+                if frequency[char] == 0:
+                    matched += 1
+                while matched == len(frequency):
+                    if minLength > right - left + 1:
+                        minLength = right - left + 1
+                        startIndex = left
+                    if s[left] in frequency:
+                        if frequency[s[left]] == 0:
+                            matched -= 1
+                        frequency[s[left]] += 1
+                    left += 1
+        return s[startIndex : startIndex + (minLength if minLength != float("inf") else 0)]

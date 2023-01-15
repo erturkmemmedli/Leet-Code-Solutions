20ed77bcf2063ab0014b6f3d070aec1f458fe101
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-15 18:56:22 +0400

    Update 1004_longestOnes.py

--- 1004_longestOnes.py
+++ 1004_longestOnes.py
@@ -59,3 +59,21 @@ class Solution1:
         if mxm > sum(arr):
             mxm = sum(arr)
         return mxm
+
+# Alternative solution
+
+class Solution:
+    def longestOnes(self, nums, k):
+        left = 0
+        zeroCount = 0
+        maxLength = 0
+        for right, num in enumerate(nums):
+            if num == 1:
+                maxLength = max(maxLength, right - left + 1)
+            else:
+                zeroCount += 1
+                while zeroCount > k:
+                    if nums[left] == 0:
+                        zeroCount -= 1
+                    left += 1
+        return maxLength

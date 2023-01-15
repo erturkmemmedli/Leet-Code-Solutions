50a0340c36d261ce548a339a094e79488e459876
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-15 20:09:12 +0400

    Update 0021_mergeTwoLists.py

--- 0021_mergeTwoLists.py
+++ 0021_mergeTwoLists.py
@@ -29,3 +29,22 @@ class Solution1:
         else:
             list2.next = self.mergeTwoLists(list1, list2.next)
             return list2
+
+# Alternative solution
+
+class Solution:
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode()
+        current = dummy
+        temp1 = list1
+        temp2 = list2
+        while temp1 and temp2:
+            if temp1.val <= temp2.val:
+                current.next = temp1
+                temp1 = temp1.next
+            else:
+                current.next = temp2
+                temp2 = temp2.next
+            current = current.next
+        current.next = temp1 if temp1 else temp2
+        return dummy.next

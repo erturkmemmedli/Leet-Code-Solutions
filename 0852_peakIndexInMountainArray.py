491928d30a14fad299205a9e44a920b25050fcce
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-15 19:53:17 +0400

    Update 0852_peakIndexInMountainArray.py

--- 0852_peakIndexInMountainArray.py
+++ 0852_peakIndexInMountainArray.py
@@ -18,3 +18,17 @@ class Solution1:
                 right = mid
             elif arr[mid] > arr[mid-1]:
                 left = mid + 1
+
+# Alternative solution
+
+class Solution:
+    def peakIndexInMountainArray(self, arr):
+        left, right = 0, len(arr) - 1
+        while left + 2 <= right:
+            mid = left + (right - left) // 2
+            if arr[mid - 1] < arr[mid] > arr[mid + 1]:
+                return mid
+            elif arr[mid + 1] > arr[mid]:
+                left = mid
+            else:
+                right = mid

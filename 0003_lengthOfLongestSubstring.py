766947dee042f82e33bb28a8964836bdf1faaf4b
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-15 18:54:16 +0400

    Update 0003_lengthOfLongestSubstring.py

--- 0003_lengthOfLongestSubstring.py
+++ 0003_lengthOfLongestSubstring.py
@@ -43,3 +43,24 @@ class Solution2:
                 start = max(start, hashmap[char][-2] + 1)
             longest = max(longest, i - start + 1)
         return longest
+
+# Alternative solution
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        hashmap = {}
+        left = 0
+        length = 0
+        for char in s:
+            if char not in hashmap:
+                hashmap[char] = 1
+                length = max(length, len(hashmap))
+            else:
+                hashmap[char] += 1
+                while hashmap[char] != 1:
+                    hashmap[s[left]] -= 1
+                    if hashmap[s[left]] == 0:
+                        del hashmap[s[left]]
+                    left += 1
+        return length

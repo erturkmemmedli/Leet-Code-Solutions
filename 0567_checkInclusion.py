d0a472dc89d631a7ec1e031b12a9b85221f5dd93
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-15 18:47:57 +0400

    Update 0567_checkInclusion.py

--- 0567_checkInclusion.py
+++ 0567_checkInclusion.py
@@ -61,3 +61,30 @@ class Solution3:
                 countWindow[char] += 1
                 if countWindow == target: return True
         return False
+
+# Alternative solution
+
+class Solution:
+    def checkInclusion(self, s1, s2):
+        if len(s1) > len(s2):
+            return False
+        k = len(s1)
+        map1 = {}
+        for char in s1:
+            if char not in map1:
+                map1[char] = 1
+            else:
+                map1[char] += 1
+        map2 = {}
+        for i, char in enumerate(s2):
+            if char not in map2:
+                map2[char] = 1
+            else:
+                map2[char] += 1
+            if i >= k:
+                map2[s2[i - k]] -= 1
+                if not map2[s2[i - k]]:
+                    del map2[s2[i - k]]
+            if map1 == map2:
+                return True
+        return False

5b42b71ee30858e9aab8058a332296a1621374e6
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-15 19:36:52 +0400

    Update 0540_singleNonDuplicate.py

--- 0540_singleNonDuplicate.py
+++ 0540_singleNonDuplicate.py
@@ -13,3 +13,26 @@ class Solution:
                 return self.singleNonDuplicate(nums[mid+2:])
             else:
                 return self.singleNonDuplicate(nums[:mid-1])
+
+# Alternative solution
+
+class Solution:
+    def singleNonDuplicate(self, nums):
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if (right - left + 1) % 4 == 1:
+                if nums[mid] == nums[mid - 1]:
+                    right = mid
+                elif nums[mid] == nums[mid + 1]:
+                    left = mid
+                else:
+                    return nums[mid]
+            else:
+                if nums[mid] == nums[mid - 1]:
+                    left = mid + 1
+                elif nums[mid] == nums[mid + 1]:
+                    right = mid - 1
+                else:
+                    return nums[mid]
+        return nums[left]

f85b83010ea5646075f89b9d67bf9a4f692605c8
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-15 19:18:01 +0400

    Update 0704_search.py

--- 0704_search.py
+++ 0704_search.py
@@ -14,3 +14,18 @@ class Solution:
             return self.binary_search(nums, target, left, mid - 1)
         if nums[mid] < target:
             return self.binary_search(nums, target, mid + 1, right)
+
+# Alternative solution
+
+class Solution:
+    def search(self, nums, target):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid  = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return -1

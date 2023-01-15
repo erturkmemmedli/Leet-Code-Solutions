2821f8bd5ce4c389aceea60d27a3d29c5948c840
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-15 18:23:40 +0400

    Update 0033_search.py

--- 0033_search.py
+++ 0033_search.py
@@ -52,3 +52,24 @@ class Solution1:
                 else:
                     right = mid - 1
         return -1
+
+# Alternative solution
+
+class Solution2:
+    def search(self, nums, target):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                if nums[mid] >= nums[left] or nums[left] > target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                if nums[mid] <= nums[right] or nums[right] < target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return -1

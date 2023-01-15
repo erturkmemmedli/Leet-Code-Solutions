661156cb391b1bdb58058d28be2f2bf93fb8013e
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-15 18:51:55 +0400

    Update 0485_findMaxConsecutiveOnes.py

--- 0485_findMaxConsecutiveOnes.py
+++ 0485_findMaxConsecutiveOnes.py
@@ -9,3 +9,16 @@ class Solution:
             else:
                 count = 0
         return result
+
+# Alternative solution
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums):
+        left = -1
+        maxOnes = 0
+        for right, num in enumerate(nums):
+            if num == 1:
+                maxOnes = max(maxOnes, right - left)
+            else:
+                left = right
+        return maxOnes

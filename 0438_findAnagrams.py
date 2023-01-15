bbfddb7c0f7483992b04f5771831a921b347f62f
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-15 18:46:18 +0400

    Update 0438_findAnagrams.py

--- 0438_findAnagrams.py
+++ 0438_findAnagrams.py
@@ -12,3 +12,40 @@ class Solution:
             if source == target:
                 result.append(i - len(p) + 1)
         return result
+
+# Alternative solution
+
+class Solution:
+    def findAnagrams(self, s, p):
+        if len(s) < len(p):
+            return []
+        k = len(p)
+        anagrams = []
+        frequency = {}
+        headWindow = 0
+        for char in p:
+            if char not in frequency:
+                frequency[char] = 1
+            else:
+                frequency[char] += 1
+        for i, char in enumerate(s):
+            if char in frequency:
+                frequency[char] -= 1
+                if not frequency[char]:
+                    del frequency[char]
+            else:
+                frequency[char] = -1
+            if i == k - 1:
+                if not frequency:
+                    anagrams.append(0)
+            elif i >= k:
+                if s[headWindow] not in frequency:
+                    frequency[s[headWindow]] = 1
+                else:
+                    frequency[s[headWindow]] += 1
+                    if not frequency[s[headWindow]]:
+                        del frequency[s[headWindow]]
+                if not frequency:
+                    anagrams.append(headWindow + 1)
+                headWindow += 1
+        return anagrams

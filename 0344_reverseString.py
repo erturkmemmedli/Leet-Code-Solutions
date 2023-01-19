99bf879fdfc0b4164e95ee93a25c3e5f25dd48fd
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-19 16:45:02 +0400

    Update 0344_reverseString.py

--- 0344_reverseString.py
+++ 0344_reverseString.py
@@ -2,3 +2,15 @@ class Solution:
     def reverseString(self, s: List[str]) -> None:
         for i in range(len(s)//2):
             s[i], s[len(s) - 1 - i] = s[len(s) - 1 - i], s[i]
+
+# Alternative solution
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left, right = left + 1, right - 1

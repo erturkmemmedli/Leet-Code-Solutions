dc8af6ae4a1832d204d70d5634e3770ffa8e736b
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-19 15:34:55 +0400

    Update 0125_isPalindrome.py

--- 0125_isPalindrome.py
+++ 0125_isPalindrome.py
@@ -9,3 +9,19 @@ class Solution:
             if new[i] != new[len(new) - 1 - i]:
                 return False
         return True
+
+# Alternative solution
+
+class Solution:
+    def isPalindrome(self, s):
+        left, right = 0, len(s) - 1
+        while left < right:
+            while not (s[left].isalpha() or s[left].isdigit()) and left < right:
+                left += 1
+            while not (s[right].isalpha() or s[right].isdigit()) and left < right:
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True

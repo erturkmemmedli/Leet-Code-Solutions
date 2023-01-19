a6fc2f64017a504a0ba779e83c7e928791514f78
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-19 16:03:10 +0400

    Update 0026_removeDuplicates.py

--- 0026_removeDuplicates.py
+++ 0026_removeDuplicates.py
@@ -28,3 +28,15 @@ class Solution1:
                 i += 1
                 j += 1
         return i+1
+
+# Alternative solution
+
+class Solution:
+    def removeDuplicates(self, nums):
+        i, j = 0, 1
+        while j < len(nums):
+            if nums[i] != nums[j]:
+                nums[i + 1], nums[j] = nums[j], nums[i + 1]
+                i += 1
+            j += 1
+        return i + 1

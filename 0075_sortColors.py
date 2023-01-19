76695fa058fb43e35a16b3fa94ca1125b4fd061a
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-20 00:36:42 +0400

    Update 0075_sortColors.py

--- 0075_sortColors.py
+++ 0075_sortColors.py
@@ -49,3 +49,24 @@ class Solution2:
                 nums[one], nums[zero] = nums[zero], nums[one]
                 zero += 1
                 one += 1
+
+# Alternative solution
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i, j = 0, 0
+        while j < len(nums):
+            if nums[j] == 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+            j += 1
+        j = i
+        while j < len(nums):
+            if nums[j] == 1:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+            j += 1
+        return nums

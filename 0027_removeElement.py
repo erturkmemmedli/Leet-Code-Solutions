50e58f4c49dafea3b4e014134b7be35ee47d3056
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-19 16:40:57 +0400

    Update 0027_removeElement.py

--- 0027_removeElement.py
+++ 0027_removeElement.py
@@ -30,3 +30,15 @@ class Solution1:
             else:
                 j -= 1
         return len(nums) - count   
+
+# Alternative solution
+
+class Solution:
+    def removeElement(self, nums, val):
+        i, j = 0, 0
+        while j < len(nums):
+            if nums[j] != val:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+            j += 1
+        return i

9e5f66c17bc5f9a628420404950bedfa0f78877e
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-24 16:49:13 +0400

    Create 0181_EmployeesEarningMoreThanTheirManagers.sql

---
+++ 0181_EmployeesEarningMoreThanTheirManagers.sql
@@ -0,0 +1,12 @@
+/* SQL Schema:
+Create table If Not Exists Employee (id int, name varchar(255), salary int, managerId int)
+Truncate table Employee
+insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3')
+insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4')
+insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', 'None')
+insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', 'None')
+*/
+
+# Write your MySQL query statement below
+
+select name as Employee from Employee e where salary > (select salary from Employee where e.managerId = id)

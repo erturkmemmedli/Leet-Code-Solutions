a0540ee6ff6194a282656d2b30485e09a3476960
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-24 16:01:25 +0400

    Create 0175_CombineTwoTables.sql

---
+++ 0175_CombineTwoTables.sql
@@ -0,0 +1,17 @@
+/* SQL Schema:
+Create table If Not Exists Person (personId int, firstName varchar(255), lastName varchar(255))
+Create table If Not Exists Address (addressId int, personId int, city varchar(255), state varchar(255))
+Truncate table Person
+insert into Person (personId, lastName, firstName) values ("1", "Wang", "Allen")
+insert into Person (personId, lastName, firstName) values ("2", "Alice", "Bob")
+Truncate table Address
+insert into Address (addressId, personId, city, state) values ("1", "2", "New York City", "New York")
+insert into Address (addressId, personId, city, state) values ("2", "3", "Leetcode", "California")
+*/
+
+# Write your MySQL query statement below
+
+select p.firstName, p.lastName, a.city, a.state
+    from Person as p
+    left join Address as a
+    on p.personId = a.personId

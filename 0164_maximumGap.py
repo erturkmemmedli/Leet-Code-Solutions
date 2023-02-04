ddd5f55a1b5be58bebd6b66a654f9e67a9b54ec2
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-04 11:57:48 +0400

    Update 0164_maximumGap.py

--- 0164_maximumGap.py
+++ 0164_maximumGap.py
@@ -12,7 +12,7 @@ class Solution:
             index = n - 2 if num == high else (num - low) * (n - 1) // (high - low)
             bucket[index].append(num)
         candidates = [[min(bucket[i]), max(bucket[i])] for i in range(n - 1) if bucket[i]]
-        maxDiff = 0
+        maxDiff = abs(candidates[0][1] - candidates[0][0])
         for i in range(1, len(candidates)):
             maxDiff = max(maxDiff, candidates[i][0] - candidates[i - 1][1])
         return maxDiff

da6bc1be3553921b9776421a5d457e641ba9cf71
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-04 11:04:25 +0400

    Create 0164_maximumGap.java

---
+++ 0164_maximumGap.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        if (nums.length < 2) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int maxDiff = 0;
+        for (int i = 1; i < nums.length; i++) {
+            maxDiff = Math.max(maxDiff, nums[i] - nums[i - 1]);
+        }
+        return maxDiff;
+    }
+}

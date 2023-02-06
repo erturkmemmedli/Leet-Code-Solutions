69b7fa9461d39947144346d1f3be6d8b4c255e09
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-06 16:34:27 +0400

    Update 0442_findDuplicates.py

--- 0442_findDuplicates.py
+++ 0442_findDuplicates.py
@@ -10,3 +10,20 @@ class Solution:
             if nums[i] != i + 1:
                 result.append(nums[i])
         return result
+
+# Alternative solution
+
+class Solution:
+    def findDuplicates(self, nums):
+        i = 0
+        while i < len(nums):
+            j = nums[i] - 1
+            if nums[i] != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i += 1
+        duplicates = []
+        for i, num in enumerate(nums):
+            if num != i + 1:
+                duplicates.append(num)
+        return duplicates

afa9784ff701f6f5693242b8b916327a62925d2a
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-06 16:26:05 +0400

    Update 0268_missingNumber.py

--- 0268_missingNumber.py
+++ 0268_missingNumber.py
@@ -13,3 +13,19 @@ class Solution1:
         n = len(nums)
         target = n * (n+1) // 2
         return target - sum(nums)
+
+# Alternative solution
+
+class Solution:
+    def missingNumber(self, nums):
+        nums.append(-1)
+        i = 0
+        while i < len(nums):
+            j = nums[i]
+            if nums[i] != nums[j] and nums[i] != -1:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i += 1
+        for i, num in enumerate(nums):
+            if num == -1:
+                return i

254cead9441ab6c07dac9b6e39dedddcf45f1ddd
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-06 17:27:31 +0400

    Update 0041_firstMissingPositive.py

--- 0041_firstMissingPositive.py
+++ 0041_firstMissingPositive.py
@@ -16,3 +16,19 @@ class Solution:
             return 1
         else: 
             return pop + 1
+
+# Alternative solution
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        i = 0
+        while i < len(nums):
+            j = nums[i] - 1
+            if 0 <= j < len(nums) - 1 and nums[i] != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i += 1
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                return i + 1
+        return len(nums) + 1

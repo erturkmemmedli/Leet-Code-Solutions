116782e7bac7880f8eda283a6593e17bfe0f0061
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-08 12:08:28 +0400

    Update 0973_kClosest.py

--- 0973_kClosest.py
+++ 0973_kClosest.py
@@ -10,3 +10,24 @@ class Solution:
         for _ in range(k):
             output.append(points[heappop(heap)[1]])
         return output
+
+# Alternative solution
+
+import heapq
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        heap = []
+        for i in range(k):
+            distance = self.squaredEuclideanDistance(points[i])
+            heapq.heappush(heap, (-distance, points[i]))
+        for i in range(k, len(points)):
+            distance = self.squaredEuclideanDistance(points[i])
+            if distance < -heap[0][0]:
+                heapq.heappop(heap)
+                heapq.heappush(heap, (-distance, points[i]))
+        return [pts for _, pts in heap]
+            
+    def squaredEuclideanDistance(self, point):
+        x, y = point
+        return x ** 2 + y ** 2

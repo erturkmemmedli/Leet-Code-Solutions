1c377d3a884df064ac8083b561cb1487fc9beb5c
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-08 11:45:30 +0400

    Update 0347_topKFrequent.py

--- 0347_topKFrequent.py
+++ 0347_topKFrequent.py
@@ -20,3 +20,21 @@ class Solution1:
             if len(heap) > k:
                 heappop(heap)
         return [b for a, b in heap]
+
+# Alternative solution
+
+from heapq import heappop, heappush
+from collections import Counter
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = Counter(nums)
+        heap = []
+        for key, val in counter.items():
+            if len(heap) < k:
+                heappush(heap, (val, key))
+            else:
+                if val > heap[0][0]:
+                    heappop(heap)
+                    heappush(heap, (val, key))
+        return [k for _, k in heap]

f4b7ffb3237db0f34cb07772c420b0c0993d253f
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-09 11:16:19 +0400

    Update 0986_intervalIntersection.py

--- 0986_intervalIntersection.py
+++ 0986_intervalIntersection.py
@@ -16,3 +16,25 @@ class Solution:
             else:
                 j += 1
         return answer
+
+# Alternative solution
+
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        resultList, i, j = [], 0, 0
+        while i < len(firstList) and j < len(secondList):
+            if firstList[i][1] < secondList[j][0]:
+                i += 1
+            elif secondList[j][1] < firstList[i][0]:
+                j += 1
+            else:
+                start = max(firstList[i][0], secondList[j][0])
+                end = min(firstList[i][1], secondList[j][1])
+                resultList.append([start, end])
+                if firstList[i][1] < secondList[j][1]:
+                    secondList[j][0] = firstList[i][1]
+                    i += 1
+                else:
+                    firstList[i][0] = secondList[j][1]
+                    j += 1
+        return resultList

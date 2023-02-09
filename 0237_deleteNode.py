614ff647febd53440391f8db25fd138094a93775
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-09 17:25:01 +0400

    Update 0237_deleteNode.py

--- 0237_deleteNode.py
+++ 0237_deleteNode.py
@@ -8,3 +8,26 @@ class Solution:
     def deleteNode(self, node):
         node.val = node.next.val
         node.next = node.next.next
+
+# Alternative solution
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        prev = node
+        while node:
+            if not node.next:
+                prev.next = None
+                break
+            node.val = node.next.val
+            prev = node
+            node = node.next

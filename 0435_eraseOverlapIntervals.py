9102a11788e0dfd71fbf4b65795305b63190a3f8
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-09 11:30:46 +0400

    Update 0435_eraseOverlapIntervals.py

--- 0435_eraseOverlapIntervals.py
+++ 0435_eraseOverlapIntervals.py
@@ -17,3 +17,20 @@ class Solution:
                 start = left
                 end = right
         return deleted
+
+# Alternative solution
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key = lambda x: [x[0], x[1]])
+        remove = 0
+        last = intervals[0]
+        for current in intervals[1:]:
+            if current[1] < last[1]:
+                remove += 1
+                last = current
+            elif current[0] < last[1]:
+                remove += 1
+            else:
+                last = current
+        return remove

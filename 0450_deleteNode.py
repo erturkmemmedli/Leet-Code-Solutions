8e81406cdc084fdd989ae7b7fb7c478242c21b9c
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-09 11:00:33 +0400

    Update 0450_deleteNode.py

--- 0450_deleteNode.py
+++ 0450_deleteNode.py
@@ -80,3 +80,26 @@ class Solution:
         if node.left:
             return self.rightDescendant(node.left, node)
         return node, parent
+
+# Alternative solution
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return
+        if root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            else:
+                node = root.right
+                while node.left:
+                    node = node.left
+                root.val = node.val
+                root.right = self.deleteNode(root.right, root.val)
+        return root

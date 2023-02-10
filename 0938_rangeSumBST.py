57ee67b8d0c00fc969a83ee1edd1b546260c6250
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-10 16:22:43 +0400

    Update 0938_rangeSumBST.py

--- 0938_rangeSumBST.py
+++ 0938_rangeSumBST.py
@@ -21,3 +21,34 @@ class Solution:
         if root.right and root.val < high:
             self.dfs(root.right, low, high)
         return
+
+# Alternative solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        self.rangeSum = 0
+        self.dfs(root, low, high)
+        return self.rangeSum
+        
+    def dfs(self, node, low, high):
+        if not node:
+            return
+        if node.val <= low:
+            if node.val == low:
+                self.rangeSum += node.val
+            self.dfs(node.right, low, high)
+        elif node.val >= high:
+            if node.val == high:
+                self.rangeSum += node.val
+            self.dfs(node.left, low, high)
+        else:
+            self.rangeSum += node.val
+            self.dfs(node.left, low, high)
+            self.dfs(node.right, low, high)

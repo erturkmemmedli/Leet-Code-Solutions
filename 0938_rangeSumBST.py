f475fc7d46806823823cfc32bcc237c0d6678951
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-10 16:23:42 +0400

    Update 0938_rangeSumBST.py

--- 0938_rangeSumBST.py
+++ 0938_rangeSumBST.py
@@ -24,13 +24,6 @@ class Solution:
 
 # Alternative solution
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         self.rangeSum = 0

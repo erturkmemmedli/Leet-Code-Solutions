565d1c08b518488c5748dbca1fa7abf6952344f3
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-10 10:29:45 +0400

    Update 0142_detectCycle.py

--- 0142_detectCycle.py
+++ 0142_detectCycle.py
@@ -32,3 +32,42 @@ class Solution1:
             slow = slow.next
             fast = fast.next
         return slow
+
+# Alternative solution
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Mathematical proof
+
+        # o -> o -> o -> o -> o -> o -> o -> o -> o
+        #           ^                             |
+        #           |_____________________________|
+        #
+        # <___ x ___><___ k ___><_____ y - k _____>
+        #
+        # assume length of linked list = x + y
+        # cycle starts at position x
+        # hare and tortoise meet at position x + k
+        #
+        # length of path of tortoise = x + k
+        # length of path of hare = x + y + k
+        #
+        # we know that hare is 2x faster than tortoise
+        # 2 * x + 2 * k = x + y + k   =>   x = y - k
+        #
+        # So, now if we move with the same speed,
+        # distance from 0 to x (which is x) = distance from x + k to k (which is y - k)
+
+        hare = tortoise = head
+        cycyleStartPoint, hareAndTortoiseMeetingPoint = head, None
+        while hare and hare.next:
+            hare = hare.next.next
+            tortoise = tortoise.next
+            if hare == tortoise:
+                hareAndTortoiseMeetingPoint = hare
+                break
+        if not hareAndTortoiseMeetingPoint: return
+        while cycyleStartPoint != hareAndTortoiseMeetingPoint:
+            cycyleStartPoint = cycyleStartPoint.next
+            hareAndTortoiseMeetingPoint = hareAndTortoiseMeetingPoint.next
+        return cycyleStartPoint

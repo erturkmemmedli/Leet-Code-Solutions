d3a6b118bd8662beddc5177e66b4078470c0935e
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-10 10:45:37 +0400

    Update 0202_isHappy.py

--- 0202_isHappy.py
+++ 0202_isHappy.py
@@ -9,3 +9,25 @@ class Solution:
                 x += int(i) ** 2
             n = x
         return n == 1
+
+# Alternative solution
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        fast = slow = n
+        while fast != 1:
+            fast = self.digitSquareSum(self.digitSquareSum(fast))
+            if fast == 1:
+                break
+            slow = self.digitSquareSum(slow)
+            if fast == slow:
+                return False
+        return True
+            
+    def digitSquareSum(self, n):
+        res = 0
+        while n:
+            x = n % 10
+            res += x ** 2
+            n //= 10
+        return res

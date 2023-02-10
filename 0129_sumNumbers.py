a4cffa6133df788f1d1dd659b3694628609de6d4
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-10 22:21:06 +0400

    Update 0129_sumNumbers.py

--- 0129_sumNumbers.py
+++ 0129_sumNumbers.py
@@ -21,3 +21,20 @@ class Solution:
             self.dfs(node.left, value * 10 + node.left.val)
         if node.right:
             self.dfs(node.right, value * 10 + node.right.val)    
+
+# Alternative solution
+
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        self.sum = 0
+        self.dfs(root, 0)
+        return self.sum
+
+    def dfs(self, node, pathSum):
+        if not node:
+            return 0
+        pathSum += node.val
+        if not node.left and not node.right:
+            self.sum += pathSum
+        self.dfs(node.left, 10 * pathSum)
+        self.dfs(node.right, 10 * pathSum)

74896293ab65a8680a8cd3188b99526d9e92d3f1
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-11 19:45:52 +0400

    Update 0102_levelOrder.py

--- 0102_levelOrder.py
+++ 0102_levelOrder.py
@@ -52,13 +52,6 @@ class Solution1:
 
 # Alternative solution
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:

6dd91a73774a2b6577403ef988a27520b141daf7
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-11 19:36:29 +0400

    Update 0102_levelOrder.py

--- 0102_levelOrder.py
+++ 0102_levelOrder.py
@@ -49,3 +49,31 @@ class Solution1:
             if next_level:
                 Q.append(next_level)
         return output
+
+# Alternative solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        queue = deque([root])
+        levelOrder = []
+        while queue:
+            levelSize = len(queue)
+            level = []
+            for _ in range(levelSize):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            levelOrder.append(level)
+        return levelOrder

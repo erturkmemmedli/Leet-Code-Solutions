ed487628a3353e29f9bc25e76baaf3c077a85173
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-11 21:02:47 +0400

    Update 0429_levelOrder.py

--- 0429_levelOrder.py
+++ 0429_levelOrder.py
@@ -26,3 +26,22 @@ class Solution:
             if nodes:
                 Q.append(nodes)
         return output
+
+# Alternative solution
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root:
+            return []
+        queue = deque([root])
+        levelOrder = []
+        while queue:
+            levelSize = len(queue)
+            level = []
+            for _ in range(levelSize):
+                currentNode = queue.popleft()
+                level.append(currentNode.val)
+                for node in currentNode.children:
+                    queue.append(node)
+            levelOrder.append(level)
+        return levelOrder

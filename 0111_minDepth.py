1dba83aae4133d231cb6f832d59c53603e374ab4
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-11 19:55:58 +0400

    Update 0111_minDepth.py

--- 0111_minDepth.py
+++ 0111_minDepth.py
@@ -23,3 +23,24 @@ class Solution:
             self.dfs(node.left, height+1)
         if node.right:
             self.dfs(node.right, height+1)
+
+# Alternative solution
+
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        queue = deque([root])
+        depth = 0
+        while queue:
+            levelSize = len(queue)
+            depth += 1
+            for _ in range(levelSize):
+                node = queue.popleft()
+                if not node.left and not node.right:
+                    return depth
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return depth

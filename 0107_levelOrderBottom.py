a6d2f564215515dafff5a435462d85d15f30cf7e
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-11 19:46:34 +0400

    Update 0107_levelOrderBottom.py

--- 0107_levelOrderBottom.py
+++ 0107_levelOrderBottom.py
@@ -25,3 +25,24 @@ class Solution:
             if next_level:
                 Q.append(next_level)
         return output
+
+# Alternative solution
+
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        queue = deque([root])
+        levelOrder = deque()
+        while queue:
+            levelSize = len(queue)
+            level = []
+            for _ in range(levelSize):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            levelOrder.appendleft(level)
+        return levelOrder

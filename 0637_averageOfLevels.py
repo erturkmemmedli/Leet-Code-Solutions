4c44d16e9d8c256cd21e6526e3a6a8cdac2b5f6b
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-11 19:10:58 +0400

    Update 0637_averageOfLevels.py

--- 0637_averageOfLevels.py
+++ 0637_averageOfLevels.py
@@ -64,3 +64,31 @@ class Solution2:
                     Q.append(node.right)
             result.append(summ/n)
         return result
+
+# Alternative solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        queue = deque([root])
+        averages = []
+        while queue:
+            levelSize = len(queue)
+            levelSum = 0
+            for _ in range(levelSize):
+                node = queue.popleft()
+                levelSum += node.val
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            averages.append(levelSum / levelSize)
+        return averages

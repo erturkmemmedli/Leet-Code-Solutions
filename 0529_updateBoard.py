63b32630d8be98d60b03b9a8fd8140cd77609259
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-12 20:43:33 +0400

    Update 0529_updateBoard.py

--- 0529_updateBoard.py
+++ 0529_updateBoard.py
@@ -33,3 +33,30 @@ class Solution:
                         else:
                             board[i][j] = str(count)
         return board # if the clicked cell is blank, all adjacent blank cells and cells with digits showing count of mine(s) around the cell are revealed.
+
+# Alternative solution
+
+class Solution:
+    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        m, n = len(board), len(board[0])
+        self.direction = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+        row, col = click
+        self.dfs(board, m, n, row, col)
+        return board
+
+    def dfs(self, board, m, n, row, col):
+        if m > row >= 0 <= col < n:
+            if board[row][col] == 'M':
+                board[row][col] = 'X'
+                return
+            elif board[row][col] == 'E':
+                numMines = 0
+                for r, c in self.direction:
+                    if m > row + r >= 0 <= col + c < n and board[row + r][col + c] == 'M':
+                        numMines += 1
+                if numMines:
+                    board[row][col] = str(numMines)
+                else:
+                    board[row][col] = 'B'
+                    for r, c in self.direction:
+                        self.dfs(board, m, n, row + r, col + c)

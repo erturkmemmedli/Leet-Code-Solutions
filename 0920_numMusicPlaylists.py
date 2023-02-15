52e4b02983644de386bb242546ab47229f83c0c1
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-15 15:04:35 +0400

    Update 0920_numMusicPlaylists.py

--- 0920_numMusicPlaylists.py
+++ 0920_numMusicPlaylists.py
@@ -4,7 +4,7 @@ class Solution:
         dp = [[0 for _ in range(n+1)] for _ in range(goal+1)]
         dp[1][1] = 1
         for i in range(2, goal+1):
-            for j in range(min(n+1, i+1)):
+            for j in range(1, min(n+1, i+1)):
                 dp[i][j] = (dp[i][j] + dp[i-1][j-1] * j) % mod
                 if j > k:
                     dp[i][j] = (dp[i][j] + dp[i-1][j] * (j-k)) % mod

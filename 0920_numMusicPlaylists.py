2d4018125323e2adac7180ac63983be83d372231
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-15 14:55:36 +0400

    Create 0920_numMusicPlaylists.py

---
+++ 0920_numMusicPlaylists.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
+        mod = 10 ** 9 + 7
+        dp = [[0 for _ in range(n+1)] for _ in range(goal+1)]
+        dp[1][1] = 1
+        for i in range(2, goal+1):
+            for j in range(min(n+1, i+1)):
+                dp[i][j] = (dp[i][j] + dp[i-1][j-1] * j) % mod
+                if j > k:
+                    dp[i][j] = (dp[i][j] + dp[i-1][j] * (j-k)) % mod
+        return dp[-1][-1]

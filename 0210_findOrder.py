651d898b6f84ed7af3d62cb8a29f008c7d2ca6b1
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-20 10:50:52 +0400

    Update 0210_findOrder.py

--- 0210_findOrder.py
+++ 0210_findOrder.py
@@ -65,3 +65,35 @@ class Solution1:
             self.visited[node] = 2
             self.postorder.append(node)
         return order
+
+# Alternative solution
+
+from collections import deque
+
+class Solution:
+    def findOrder(self, numCourses, prerequisites):
+        graph = {i: [] for i in range(numCourses)}
+        in_degree = {i: 0 for i in range(numCourses)}
+        
+        for course, preq_course in prerequisites:
+            graph[preq_course].append(course)
+            in_degree[course] += 1
+            
+        queue = deque()
+        
+        for course, preq_count in in_degree.items():
+            if preq_count == 0:
+                queue.append(course)
+                
+        topologically_sorted_courses = []
+        
+        while queue:
+            node = queue.popleft()
+            topologically_sorted_courses.append(node)
+            
+            for neighbor in graph[node]:
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
+                    queue.append(neighbor)
+                    
+        return topologically_sorted_courses if len(topologically_sorted_courses) == numCourses else []

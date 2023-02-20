ed9f4e6495cb7ff7ffa477311a7a21a28c202b0c
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-20 16:18:25 +0400

    Update 0310_findMinHeightTrees.py

--- 0310_findMinHeightTrees.py
+++ 0310_findMinHeightTrees.py
@@ -21,3 +21,44 @@ class Solution:
             if newLevel:
                 queue.append(newLevel)
         return queue[0]
+
+# Alternative solution
+
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        if n <= 2:
+            return list(range(n))
+
+        graph = {i: [] for i in range(n)}
+        in_degree = {i: 0 for i in range(n)}
+
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+            in_degree[a] += 1
+            in_degree[b] += 1
+        
+        queue = deque([key for key, val in in_degree.items() if val == 1])
+
+        MHT = []
+
+        while queue:
+            level_length = len(queue)
+            
+            for _ in range(level_length):
+                node = queue.popleft()
+                in_degree[node] -= 1
+
+                for neighbor in graph[node]:
+                    if in_degree[neighbor] == 0:
+                        continue
+
+                    in_degree[neighbor] -= 1
+
+                    if in_degree[neighbor] == 1:
+                        queue.append(neighbor)
+
+            if queue:
+                MHT = list(queue)
+            
+        return MHT

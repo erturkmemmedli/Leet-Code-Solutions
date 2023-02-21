d9b059b4aaf09e0950c02fd0e62a90457ee58b57
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-21 09:49:54 +0400

    Create 2192_getAncestors.py

---
+++ 2192_getAncestors.py
@@ -0,0 +1,30 @@
+class Solution:
+    def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        graph = {i:[] for i in range(n)}
+        in_degree = {i:0 for i in range(n)}
+
+        for start, end in edges:
+            graph[start].append(end)
+            in_degree[end] += 1
+
+        queue = deque([node for node, degree in in_degree.items() if not degree])
+        answer = [set() for _ in range(n)]
+
+        print(graph)
+        print(in_degree)
+
+        while queue:
+            node = queue.popleft()
+            
+            for neighbor in graph[node]:
+                answer[neighbor].add(node)
+                
+                for element in answer[node]:
+                    answer[neighbor].add(element)
+
+                in_degree[neighbor] -= 1
+
+                if in_degree[neighbor] == 0:
+                    queue.append(neighbor)
+
+        return [sorted(list(ans)) for ans in answer]

82d79111b3bc8f3efc688b349168ac7b55cd6d38
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-23 23:23:22 +0400

    Update 0212_findWords.py

--- 0212_findWords.py
+++ 0212_findWords.py
@@ -6,6 +6,68 @@ class TrieNode:
 class Trie:
     def __init__(self):
         self.root = TrieNode()
+        self.wordAwaiting = 0
+
+    def insert(self, word):
+        node = self.root
+
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        
+        self.wordAwaiting += 1
+        node.isEnd = True
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        m, n = len(board), len(board[0])
+        trie = Trie()
+        node = trie.root
+        result = []
+        
+        for word in words:
+            trie.insert(word)
+
+        for i in range(m):
+            for j in range(n):
+                char = board[i][j]
+                if char in node.children:
+                    self.dfs(trie, node.children[char], board, i, j, m, n, char, result)
+
+        return result
+            
+    def dfs(self, trie, node, board, r, c, m, n, path, result):
+        if not node or trie.wordAwaiting == 0:
+            return
+
+        if node.isEnd:
+            result.append(path)
+            node.isEnd = False
+            trie.wordAwaiting -= 1
+
+        temp = board[r][c]
+        board[r][c] = "#"
+
+        for row, col in [r-1, c], [r+1, c], [r, c-1], [r, c+1]:
+            if m > row >= 0 <= col < n:
+                char = board[row][col]
+                if char in node.children:
+                    self.dfs(trie, node.children[char], board, row, col, m, n, path + char, result)
+
+        board[r][c] = temp
+
+
+# Alternative solution (which gives TLE error)
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+        
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
 
     def insert(self, word):
         node = self.root

28ceebdb0cb70983f858333231f50dfb93aaa44a
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-23 15:04:37 +0400

    Update 1268_suggestedProducts.py

--- 1268_suggestedProducts.py
+++ 1268_suggestedProducts.py
@@ -47,3 +47,54 @@ class Solution:
                 break
         output += [[]] * (len(searchWord) - i - 1)
         return output
+
+# Alternative solution
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.suggestions = []
+        
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def get_root(self):
+        return self.root
+
+    def insert(self, word):
+        node = self.root
+
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+
+            node = node.children[char]
+
+            if len(node.suggestions) < 3:
+                node.suggestions.append(word)
+
+    def suggest(self, node, char):
+        if node and char in node.children:
+            return node.children[char], node.children[char].suggestions 
+        return None, []
+
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        products.sort()
+        trie = Trie()
+
+        for product in products:
+            trie.insert(product)
+
+        output = []
+        root = trie.get_root()
+
+        for char in searchWord:
+            if not root:
+                output.append([])
+            else:
+                root, suggestion = trie.suggest(root, char)
+                output.append(suggestion)
+
+        return output

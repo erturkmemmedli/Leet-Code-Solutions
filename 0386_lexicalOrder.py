207d71d55a4db1ab6e0d75306a893ba68ff73ead
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-23 15:35:39 +0400

    Update 0386_lexicalOrder.py

--- 0386_lexicalOrder.py
+++ 0386_lexicalOrder.py
@@ -54,3 +54,48 @@ class Solution1:
             result.append(new_start)
             result += self.trie_dfs(new_start, n)
         return result
+
+# Alternative solution
+
+class TrieNode:
+    def __init__(self):
+        self.children = [[] for _ in range(10)]
+    
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def get_root(self):
+        return self.root
+
+    def insert(self, number):
+        node = self.root
+
+        for num in number:
+            if node.children[int(num)] == []:
+                node.children[int(num)] = TrieNode()
+
+            node = node.children[int(num)]
+
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        trie = Trie()
+
+        for i in range(1, n + 1):
+            trie.insert(str(i))
+
+        root = trie.get_root()
+        current = 0
+        self.output = []
+
+        self.dfs(root, current)
+
+        return self.output
+        
+    def dfs(self, node, current):
+        for i in range(10):
+            if node.children[i]:
+                self.output.append(current + i)
+                self.dfs(node.children[i], 10 * (current + i))
+            elif i != 0:
+                return

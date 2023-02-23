89478c494a113f3e90df9f5547ea71f1018327c3
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-23 12:20:15 +0400

    Update 0014_longestCommonPrefix.py

--- 0014_longestCommonPrefix.py
+++ 0014_longestCommonPrefix.py
@@ -10,3 +10,45 @@ class Solution:
                     break
             output = output[:j]
         return output
+
+# Alternative solution
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for i, char in enumerate(word):
+            if node.isEnd:
+                return word[:i]
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.isEnd = True
+
+    def prefix(self, word, answer):
+        node = self.root
+        for i, char in enumerate(word):
+            if node.isEnd:
+                return word[:i]
+            if char not in node.children:
+                node.isEnd = True
+                return word[:i]
+            node = node.children[char]
+        node.isEnd = True
+        return word 
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        trie = Trie()
+        trie.insert(strs[0])
+        answer = strs[0]
+        for i in range(1, len(strs)):
+            answer = trie.prefix(strs[i], answer)
+        return answer

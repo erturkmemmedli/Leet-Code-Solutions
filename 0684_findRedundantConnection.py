59e30da53c94ee9a2b74dbf214b877376a6be1e0
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-24 17:03:30 +0400

    Update 0684_findRedundantConnection.py

--- 0684_findRedundantConnection.py
+++ 0684_findRedundantConnection.py
@@ -105,3 +105,34 @@ class Solution3:
             self.parent[root_b] = root_a
             self.rank[root_a] += self.rank[root_b]
             return True
+
+# Alternative solution
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        
+    def find(self, a):
+        while a != self.parent[a]:
+            a = self.parent[a]
+
+        return a
+
+    def union(self, a, b):
+        root_a = self.find(a)
+        root_b = self.find(b)
+
+        if root_a == root_b:
+            return False
+
+        self.parent[root_b] = root_a
+
+        return True
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        uf = UnionFind(len(edges))
+
+        for a, b in edges:
+            if not uf.union(a - 1, b - 1):
+                return [a, b]

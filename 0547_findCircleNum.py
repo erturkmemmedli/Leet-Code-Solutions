c38a7463b1d8081b1eeec6b45a6bcdb1159cf8f8
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-24 17:48:39 +0400

    Update 0547_findCircleNum.py

--- 0547_findCircleNum.py
+++ 0547_findCircleNum.py
@@ -17,3 +17,37 @@ class Solution:
                 visited.add(j)
                 temp.append(j)
                 self.dfs(isConnected, n, j, visited, temp)
+                
+# Alternative solution
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+
+    def find(self, a):
+        while a != self.parent[a]:
+            a = self.parent[a]
+        
+        return a
+
+    def union(self, a, b):
+        root_a = self.find(a)
+        root_b = self.find(b)
+
+        if root_a != root_b:
+            self.parent[root_b] = root_a
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        uf = UnionFind(n)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if isConnected[i][j]:
+                    uf.union(i, j)
+        
+        for i in range(n):
+            uf.parent[i] = uf.find(uf.parent[i])
+
+        return len(set(uf.parent))

dc8885b5f9f3d0317b87a5fc55ac8fa580892582
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-26 11:36:43 +0400

    Update 0295_MedianFinder.py

--- 0295_MedianFinder.py
+++ 0295_MedianFinder.py
@@ -37,3 +37,26 @@ class MedianFinder1:
         if len(self.maxHeap) > len(self.minHeap):
             return -self.maxHeap[0]
         return (self.minHeap[0] - self.maxHeap[0]) / 2
+
+# Alternative solution
+
+class MedianFinder:
+    def __init__(self):
+        self.minHeap = []
+        self.maxHeap = []
+
+    def addNum(self, num: int) -> None:
+        if not self.maxHeap or -self.maxHeap[0] > num:
+            heapq.heappush(self.maxHeap, -num)
+        else:
+            heapq.heappush(self.minHeap, num)
+
+        if len(self.maxHeap) > len(self.minHeap) + 1:
+            heapq.heappush(self.minHeap, -heapq.heappop(self.maxHeap))
+        elif len(self.minHeap) > len(self.maxHeap):
+            heapq.heappush(self.maxHeap, -heapq.heappop(self.minHeap))
+
+    def findMedian(self) -> float:
+        if len(self.minHeap) == len(self.maxHeap):
+            return (self.minHeap[0] - self.maxHeap[0]) / 2
+        return -self.maxHeap[0]

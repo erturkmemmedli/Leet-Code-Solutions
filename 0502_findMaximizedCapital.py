bbbbf7a2598d61c8a58dc3c0dc23822a7c706808
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-27 17:33:53 +0400

    Update 0502_findMaximizedCapital.py

--- 0502_findMaximizedCapital.py
+++ 0502_findMaximizedCapital.py
@@ -12,3 +12,30 @@ class Solution:
                 break
             w -= heappop(heap)
         return w
+
+# Alternative solution
+
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        usableHeap = []
+        reserveHeap = []
+
+        for i in range(len(profits)):
+            if capital[i] <= w:
+                heappush(usableHeap, (-profits[i], capital[i]))
+            else:
+                heappush(reserveHeap, (capital[i], profits[i]))
+    
+        while k:
+            if not usableHeap:
+                break
+                
+            profit, capital = heappop(usableHeap)
+            w -= profit
+            k -= 1
+
+            while reserveHeap and reserveHeap[0][0] <= w:
+                capital, profit = heappop(reserveHeap)
+                heappush(usableHeap, (-profit, capital))
+
+        return w

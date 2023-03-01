46ac90879e56b5f0cc723118172df0d5047aba7d
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-03-01 11:27:34 +0400

    Update 0090_subsetsWithDup.py

--- 0090_subsetsWithDup.py
+++ 0090_subsetsWithDup.py
@@ -25,3 +25,19 @@ class Solution:
             self.result.append(currState)
         for i in range(start, len(nums)):
             self.backtrack(nums, currState + [nums[i]], i + 1)
+
+# Alternative solution
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        self.result = []
+        self.backtrack(nums, [], 0)
+        return self.result
+
+    def backtrack(self, nums, currState, start):
+        self.result.append(currState)
+        for i in range(start, len(nums)):
+            if i > start and nums[i] == nums[i - 1]:
+                continue
+            self.backtrack(nums, currState + [nums[i]], i + 1)

a8a33f8cacdc25185667724716605954df40bf5b
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-03-01 11:19:22 +0400

    Update 0090_subsetsWithDup.py

--- 0090_subsetsWithDup.py
+++ 0090_subsetsWithDup.py
@@ -10,3 +10,18 @@ class Solution:
         output.add(tuple(path))
         for i in range(len(nums)):
             self.dfs(nums[i+1:], output, path + [nums[i]])
+
+# Alternative solution
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        self.result = []
+        self.backtrack(nums, [], 0)
+        return self.result
+
+    def backtrack(self, nums, currState, start):
+        if currState not in self.result:
+            self.result.append(currState)
+        for i in range(start, len(nums)):
+            self.backtrack(nums, currState + [nums[i]], i + 1)

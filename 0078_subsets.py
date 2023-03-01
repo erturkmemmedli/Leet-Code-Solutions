73d325e171ceeab26e1be44660f2cf31174d5623
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-03-01 11:01:21 +0400

    Update 0078_subsets.py

--- 0078_subsets.py
+++ 0078_subsets.py
@@ -10,3 +10,18 @@ class Solution:
         for i in range(len(nums)):
             self.dfs(nums[i+1:], output, path + [nums[i]])
         return
+
+# Alternative solution
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        self.backtrack(result, nums, [], 0)
+        return result
+
+    def backtrack(self, result, nums, currState, start):
+        result.append(currState[:])
+        for i in range(start, len(nums)):
+            currState.append(nums[i])
+            self.backtrack(result, nums, currState, i + 1)
+            currState.pop()

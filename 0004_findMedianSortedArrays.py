206c0a9b8aa55973317161892a48397aa27363b0
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-03-02 17:09:55 +0400

    Update 0004_findMedianSortedArrays.py

--- 0004_findMedianSortedArrays.py
+++ 0004_findMedianSortedArrays.py
@@ -39,3 +39,30 @@ class Solution1(object):
             return merged[len(merged) // 2]
         else:
             return (merged[len(merged) // 2 - 1] + merged[len(merged) // 2]) / 2
+
+# Alternative solution
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        total_length = len(nums1) + len(nums2)
+        k = total_length // 2
+
+        if not nums1:
+            return nums2[k] if total_length & 1 else (nums2[k-1] + nums2[k]) / 2
+        if not nums2:
+            return nums1[k] if total_length & 1 else (nums1[k-1] + nums1[k]) / 2
+
+        heap = []
+        heappush(heap, (nums1[0], 1, 0))
+        heappush(heap, (nums2[0], 2, 0))
+
+        while k:
+            val, arr_no, idx = heappop(heap)
+            k -= 1
+
+            if arr_no == 1 and idx + 1 < len(nums1):
+                heappush(heap, (nums1[idx + 1], arr_no, idx + 1))
+            if arr_no == 2 and idx + 1 < len(nums2):
+                heappush(heap, (nums2[idx + 1], arr_no, idx + 1))
+        
+        return heap[0][0] if total_length & 1 else (val + heap[0][0]) / 2

d83b72b2ae330f298e50a61ff8336d3a23aabed7
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-03-02 14:37:33 +0400

    Update 0857_mincostToHireWorkers.py

--- 0857_mincostToHireWorkers.py
+++ 0857_mincostToHireWorkers.py
@@ -10,3 +10,29 @@ class Solution:
             if len(heap) == k:
                 minMoney = min(minMoney, ratio * sumHeap)
         return minMoney
+
+# Altrnative solution (which gives TLE error)
+
+class Solution:
+    def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
+        result = float("inf")
+        heap = []
+        factor = sorted([wage[i]/quality[i] for i in range(len(wage))])
+        j = 0
+
+        while j < len(factor):
+            for i, (q, w) in enumerate(sorted(zip(quality, wage))):
+                if q * factor[j] < w:
+                    continue
+
+                heappush(heap, q * factor[j])
+                
+                if len(heap) == k:
+                    result = min(result, sum(heap))
+                    heap = []
+                    break
+
+            heap = []
+            j += 1
+
+        return result

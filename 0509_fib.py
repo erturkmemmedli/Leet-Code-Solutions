9738206382ac0495ba0ec3411a2d0c56f30bf278
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-03-03 09:40:47 +0400

    Update 0509_fib.py

--- 0509_fib.py
+++ 0509_fib.py
@@ -6,3 +6,20 @@ class Solution:
         for _ in range(n-1):
             curr, prev = curr + prev, curr
         return curr
+
+# Alternative solution
+
+class Solution:
+    def fib(self, n: int) -> int:
+        self.memo = {}
+        return self.dp(n)
+
+    def dp(self, n):
+        if n <= 1:
+            return n
+        
+        if n in self.memo:
+            return self.memo[n]
+
+        self.memo[n] = self.dp(n-1) + self.dp(n-2)
+        return self.memo[n]

60676a423ca1c70a68409d0323ccf7f1a21924b3
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-03-04 11:49:35 +0400

    Update 0126_findLadders.py

--- 0126_findLadders.py
+++ 0126_findLadders.py
@@ -1,3 +1,118 @@
+class Solution:
+    def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
+        graph = set(wordList)
+        if endWord not in graph:return []
+        currLevel = {beginWord}
+        self.parents = {}
+
+        while currLevel:
+            graph -= currLevel
+            nextLevel = set()
+
+            for word in currLevel:
+                for i in range(len(word)):
+                    for char in string.ascii_lowercase:
+                        newWord = word[:i] + char + word[i+1:]
+                        if newWord in graph:
+                            nextLevel.add(newWord)
+                            if newWord not in self.parents:
+                                self.parents[newWord] = []
+                            self.parents[newWord].append(word)
+
+            if endWord in nextLevel:
+                break
+            currLevel = nextLevel
+
+        self.ladders = []
+        self.dfs(beginWord, endWord, [])
+        return self.ladders
+
+    def dfs(self, begin, end, path):
+        if begin == end:
+            path.append(begin)
+            self.ladders.append(path[::-1])
+            return
+
+        if end not in self.parents:
+            return
+
+        for word in self.parents[end]:
+            self.dfs(begin, word, path + [end])
+
+# Alternative solution (which gives TLE error)
+
+class Solution:
+    def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
+        self.graph = {}
+        self.graph[beginWord] = []
+
+        for word in wordList:
+            if self.isNode(beginWord, word):
+                self.graph[beginWord].append(word)
+            
+        for i in range(len(wordList)):
+            for j in range(i + 1, len(wordList)):
+                if self.isNode(wordList[i], wordList[j]):
+                    if wordList[i] not in self.graph:
+                        self.graph[wordList[i]] = []
+                    self.graph[wordList[i]].append(wordList[j])
+                    if wordList[j] not in self.graph:
+                        self.graph[wordList[j]] = []
+                    self.graph[wordList[j]].append(wordList[i])
+
+        self.parent = {}
+        length = self.bfs(beginWord, endWord)
+        if not length: return []
+
+        self.ladders = []
+        self.dfs(length, [], beginWord, endWord)
+        return self.ladders
+
+    def isNode(self, word1, word2):
+        difference = 0
+
+        for i in range(len(word1)):
+            if word1[i] != word2[i]:
+                difference += 1
+            if difference > 1:
+                return False
+
+        return difference == 1
+
+    def bfs(self, beginWord, endWord):
+        heap = [(1, beginWord)]
+        visited = set()
+        length = None
+
+        while heap:
+            distance, node = heappop(heap)
+            visited.add(node)
+
+            if length and length < distance:
+                break
+
+            if node == endWord:
+                length = distance
+
+            for neighbor in self.graph[node]:
+                if neighbor not in visited:
+                    heappush(heap, (distance + 1, neighbor))
+                    if neighbor not in self.parent:
+                        self.parent[neighbor] = set()
+                    self.parent[neighbor].add(node)
+
+        return length
+
+    def dfs(self, length, path, beginWord, endWord):
+        if beginWord == endWord:
+            if len(path) == length - 1:
+                path.append(endWord)
+                self.ladders.append(path[::-1])
+            return
+
+        for node in self.parent[endWord]:
+            self.dfs(length, path + [endWord], beginWord, node)
+
 # Alternative solution (which gives TLE error)
 
 class Solution:

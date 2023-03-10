cc6c0da6a1f4e89573b1dead89c7e6ca9b0cc910
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-03-10 15:54:11 +0400

    Update 0064_minPathSum.py

--- 0064_minPathSum.py
+++ 0064_minPathSum.py
@@ -12,3 +12,21 @@ class Solution:
             for j in range(1, n):
                 dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
         return dp[-1][-1]
+
+# Alternative solution
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+
+        for i in range(1, m):
+            grid[i][0] += grid[i-1][0]
+
+        for j in range(1, n):
+            grid[0][j] += grid[0][j-1]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+                
+        return grid[-1][-1]

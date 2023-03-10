54d701f21c738341aa1c46d37e6a143b72f3482c
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-03-10 16:02:50 +0400

    Create 1277_countSquares.py

--- 1277_countSquares.py
+++ 1277_countSquares.py
@@ -36,3 +36,16 @@ class Solution1:
                         dp[i][j] = 1
                 answer += dp[i][j]
         return answer
+
+# Alternative solution
+
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        m, n = len(matrix), len(matrix[0])
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] and matrix[i][j] <= matrix[i][j-1] and matrix[i][j] <= matrix[i-1][j] and matrix[i][j] <= matrix[i-1][j-1]:
+                    matrix[i][j] = min(matrix[i][j-1], matrix[i-1][j], matrix[i-1][j-1]) + 1
+        
+        return sum(matrix[i][j] for i in range(m) for j in range(n))

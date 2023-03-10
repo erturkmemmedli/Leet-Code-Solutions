4cda80f22a4d52149cf484f3d2a0875a118923fd
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-03-10 15:43:16 +0400

    Update 0119_getRow.py

--- 0119_getRow.py
+++ 0119_getRow.py
@@ -10,3 +10,16 @@ class Solution:
             dp = [1] + temp + [1]
             temp = []
         return dp
+
+# Alternative solution
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        pascal = [1]
+        for i in range(rowIndex):
+            dp = [1]
+            for i in range(len(pascal) - 1):
+                dp.append(pascal[i] + pascal[i + 1])
+            dp.append(1)
+            pascal = dp[:]
+        return pascal

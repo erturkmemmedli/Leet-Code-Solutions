d565ce55c25ed2a52b65eb27122e2e6bb3c0084b
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-03-10 16:45:12 +0400

    Update 0894_allPossibleFBT.py

--- 0894_allPossibleFBT.py
+++ 0894_allPossibleFBT.py
@@ -24,13 +24,6 @@ class Solution:
 
 # Alternative solution
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 class Solution:
     def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
         self.memo = {0 : [], 1 : [TreeNode()]}

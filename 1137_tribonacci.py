d3d52c52a945d464c4d81a9919303057e14efb14
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-03-10 15:47:29 +0400

    Update 1137_tribonacci.py

--- 1137_tribonacci.py
+++ 1137_tribonacci.py
@@ -5,3 +5,25 @@ class Solution:
         for _ in range(n-2):
             first, second, third = second, third, first + second + third
         return third
+
+# Alternative solution
+
+class Solution:
+    def __init__(self):
+        self.memo = {}
+
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        
+        if n == 1:
+            return 1
+
+        if n == 2:
+            return 1
+
+        if n in self.memo:
+            return self.memo[n]
+
+        self.memo[n] = self.tribonacci(n - 3) + self.tribonacci(n - 2) + self.tribonacci(n - 1)
+        return self.memo[n]

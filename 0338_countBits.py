eeceeec2d4a197634a31ba499e8066316616570f
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-03-10 11:56:11 +0400

    Update 0338_countBits.py

--- 0338_countBits.py
+++ 0338_countBits.py
@@ -32,3 +32,21 @@ class Solution2:
             else:
                 dp[i] = dp[i-1] + 1
         return dp
+
+# Alternative solution
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        output = [0]
+        power = 0
+
+        while n >= len(output) * 2:
+            for i in range(len(output)):
+                output.append(output[i] + 1)
+
+        index = n - len(output) + 1
+
+        for i in range(index):
+            output.append(output[i] + 1)
+
+        return output

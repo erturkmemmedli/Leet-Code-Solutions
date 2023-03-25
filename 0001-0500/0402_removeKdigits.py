fb06a70e7313401ba78aa43de2b13c654bcae39d
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-03-25 12:42:47 +0400

    Update 0402_removeKdigits.py

--- 0001-0500/0402_removeKdigits.py
+++ 0001-0500/0402_removeKdigits.py
@@ -23,3 +23,47 @@ class Solution:
                 stack = []
         result = "".join(stack).lstrip("0")
         return result if result else "0"
+
+# Alternative solution
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stack = []
+
+        i = 0
+        while i < len(num):
+            while stack and stack[-1] > num[i]:
+                stack.pop()
+                k -= 1
+                if k == 0:
+                    break 
+            stack.append(num[i])
+            i += 1
+            if k == 0:
+                break
+
+        while k:
+            stack.pop()
+            k -= 1
+
+        j = 0     
+        while j < len(stack):
+            if stack[j] == '0':
+                j += 1
+            else:
+                break
+
+        result = "".join(stack[j:])
+        
+        if result:
+            result += num[i:]
+            return result
+
+        while i < len(num):
+            if num[i] == '0':
+                i += 1
+            else:
+                break
+            
+        result = num[i:]
+        return result if result else "0"

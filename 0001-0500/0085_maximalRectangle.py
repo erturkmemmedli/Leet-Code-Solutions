6b8585528fd2637000d2973b4bfad7db67988614
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-03-25 16:57:49 +0400

    Update 0085_maximalRectangle.py

--- 0001-0500/0085_maximalRectangle.py
+++ 0001-0500/0085_maximalRectangle.py
@@ -14,3 +14,50 @@ class Solution:
                     maxArea = max(maxArea, height * width)
                 stack.append(k)
         return maxArea
+
+# Alternative solution
+
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        m, n = len(matrix), len(matrix[0])
+        heights = [0] * n
+        max_area = 0
+
+        for i in range(m):
+            for j in range(n):
+                heights[j] = int(matrix[i][j]) * heights[j] + int(matrix[i][j])
+            
+            max_area = max(max_area, self.largestRectangleArea(heights))
+
+        return max_area
+
+    def largestRectangleArea(self, heights):
+        stack = []
+        max_area = 0
+        i = 0
+
+        while i < len(heights):
+            max_area = max(max_area, heights[i])
+            width = 1
+
+            while stack and stack[-1][0] >= heights[i]:
+                h, w = stack.pop()
+                
+                if stack and stack[-1][0] >= heights[i]:
+                    stack[-1] = [min(stack[-1][0], h), stack[-1][1] + w]
+                    max_area = max(max_area, stack[-1][0] * stack[-1][1])
+                    
+                else:
+                    width += w
+                    max_area = max(max_area, min(h, heights[i]) * width)
+                    break
+        
+            stack.append([heights[i], width])
+            i += 1
+
+        while len(stack) > 1:
+            h, w = stack.pop()
+            stack[-1] = [stack[-1][0], stack[-1][1] + w]
+            max_area = max(max_area, stack[-1][0] * stack[-1][1])
+            
+        return max_area

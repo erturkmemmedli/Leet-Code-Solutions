9b957d944996545fcb00b8536d532d97f49d60e6
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-05-17 22:48:37 +0400

    Update 2130_pairSum.py

--- 2001-2500/2130_pairSum.py
+++ 2001-2500/2130_pairSum.py
@@ -3,6 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
         l = []
@@ -17,3 +18,36 @@ class Solution:
             i += 1
             j -= 1
         return res
+
+# Alternative solution
+
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        fast, slow = head, head
+
+        while fast:
+            slow = slow.next
+            fast = fast.next.next
+
+        first = head
+        second = self.reverseList(slow)
+        maxPairSum = 0
+
+        while second:
+            maxPairSum = max(maxPairSum, first.val + second.val)
+            first = first.next
+            second = second.next
+
+        return maxPairSum
+
+    def reverseList(self, head):
+        prev = None
+        curr = head
+        
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        
+        return prev

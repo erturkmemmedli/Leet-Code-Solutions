20fee6e3a4d4cecff4accedffba95fbbc949b9c5
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-06-03 14:46:05 +0400

    Update 1392_longestPrefix.py

--- 1001-1500/1392_longestPrefix.py
+++ 1001-1500/1392_longestPrefix.py
@@ -4,3 +4,16 @@ class Solution:
             if s[:-i] == s[i:]:
                 return s[i:]
         return ""
+
+# Alternative solution
+
+class Solution:
+    def longestPrefix(self, s: str) -> str:
+        res, l, r, mod = 0, 0, 0, 10**9 + 7
+
+        for i in range(len(s) - 1):
+            l = (l * 128 + ord(s[i])) % mod
+            r = (r + pow(128, i, mod) * ord(s[~i])) % mod
+            if l == r: res = i + 1
+
+        return s[:res]

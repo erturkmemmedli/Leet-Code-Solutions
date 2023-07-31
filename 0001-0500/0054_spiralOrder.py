6ad9fb0b69172c235fd1130ebba603200551821e
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-07-31 17:51:33 +0400

    Update 0054_spiralOrder.py

--- 0001-0500/0054_spiralOrder.py
+++ 0001-0500/0054_spiralOrder.py
@@ -14,3 +14,56 @@ class Solution:
                 x += dx
                 y += dy
         return spiral
+
+# Alternative solution
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m, n = len(matrix), len(matrix[0])
+        left_edge, right_edge, top_edge, bottom_edge = 0, n - 1, 0, m - 1
+        right, left, down, up = True, False, False, False
+        output = [matrix[0][0]]
+        row, col = 0, 0
+
+        while right or left or down or up:
+            top_edge += 1
+            while right:
+                if col + 1 <= right_edge:
+                    col += 1
+                    output.append(matrix[row][col])
+                else:
+                    right = False
+                    if row + 1 <= bottom_edge:
+                        down = True
+                        
+            right_edge -= 1
+            while down:
+                if row + 1 <= bottom_edge:
+                    row += 1
+                    output.append(matrix[row][col])
+                else:
+                    down = False
+                    if col - 1 >= left_edge:
+                        left = True         
+
+            bottom_edge -= 1
+            while left:
+                if col - 1 >= left_edge:
+                    col -= 1
+                    output.append(matrix[row][col])
+                else:
+                    left = False
+                    if row - 1 >= top_edge:
+                        up = True
+                        
+            left_edge += 1
+            while up:
+                if row - 1 >= top_edge:
+                    row -= 1
+                    output.append(matrix[row][col])
+                else:
+                    up = False
+                    if col + 1 <= right_edge:
+                        right = True     
+
+        return output

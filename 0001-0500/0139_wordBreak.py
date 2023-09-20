71dcddae3b6a0eba26c3a4ff05a349d9c3714afa
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-09-20 14:00:29 +0400

    Update 0139_wordBreak.py

--- 0001-0500/0139_wordBreak.py
+++ 0001-0500/0139_wordBreak.py
@@ -47,3 +47,20 @@ class Solution:
                 visited.add(string)
                 trie.search(queue, visited, string)
         return False
+
+# Alternative solution
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [0] * (len(s) + 1)
+        dp[-1] = 1
+
+        for i in range(len(s) - 1, -1, -1):
+            for word in wordDict:
+                if i + len(word) < len(dp) and s[i:i+len(word)] == word:
+                    dp[i] = dp[i + len(word)]
+                
+                if dp[i]:
+                    break
+                
+        return dp[0] == 1

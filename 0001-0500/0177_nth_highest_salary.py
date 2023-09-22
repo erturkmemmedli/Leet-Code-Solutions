7232a0eeb67f238cd04795b7c7ca58bc15772c76
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-09-22 17:47:14 +0400

    Update 0177_nth_highest_salary.py

--- 0001-0500/0177_nth_highest_salary.py
+++ 0001-0500/0177_nth_highest_salary.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
     salaries = sorted(employee['salary'].unique(), key = lambda x: -x)
-    return pd.DataFrame({f'getNthHighestSalary({N})': [salaries[N - 1]] if len(salaries) > 1 else [None]})
+    return pd.DataFrame({f'getNthHighestSalary({N})': [salaries[N - 1]] if len(salaries) > N - 1 else [None]})

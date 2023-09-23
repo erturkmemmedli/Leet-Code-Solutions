e64afcb20762720c13be5c4f94ec3f9b577c6827
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-09-23 23:16:56 +0400

    Update 0176_second_highest_salary.py

--- 0001-0500/0176_second_highest_salary.py
+++ 0001-0500/0176_second_highest_salary.py
@@ -1,3 +1,10 @@
+'''
+Pandas Schema:
+data = [[1, 100], [2, 200], [3, 300]]
+employee = pd.DataFrame(data, columns=['id', 'salary']).astype({'id':'int64', 'salary':'int64'})
+'''
+
+
 import pandas as pd
 
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:

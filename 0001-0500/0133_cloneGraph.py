f39bde13d4ad9c6a16de5b371ecdd8c78dd34c9b
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-09-28 17:38:42 +0400

    Update 0133_cloneGraph.py

--- 0001-0500/0133_cloneGraph.py
+++ 0001-0500/0133_cloneGraph.py
@@ -29,3 +29,36 @@ class Solution:
             for neighbor in node.neighbors:
                 self.hashmap[node.val].add(neighbor.val)
                 self.dfs(neighbor, node)
+
+# Alternative solution
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return
+
+        self.graph_dict = {}
+        self.dfs(node)
+        
+        node_map = {i : Node(i) for i in self.graph_dict.keys()}
+
+        for key, val in self.graph_dict.items():
+            node_map[key].neighbors = [node_map[i] for i in val]
+
+        return node_map[node.val]
+
+    def dfs(self, node):
+        if node.val not in self.graph_dict:
+            self.graph_dict[node.val] = [n.val for n in node.neighbors]
+            
+            for node in node.neighbors:
+                self.dfs(node)

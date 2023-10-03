da1ea56f561ac99916cf25a21c0350279ad4b3e3
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-10-03 10:55:35 +0400

    Update 0323_countComponents.py

--- 0001-0500/0323_countComponents.py
+++ 0001-0500/0323_countComponents.py
@@ -23,3 +23,64 @@ class Solution:
             if i == uf.parent[i]:
                 roots += 1
         return roots
+
+# Alternative solution
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [1 for _ in range(n)]
+        self.cc = n
+
+    def find(self, a):
+        if a != self.parent[a]:
+            self.parent[a] = self.find(self.parent[a])
+        return self.parent[a]
+
+    def union(self, a, b):
+        root_a = self.find(a)
+        root_b = self.find(b)
+        if root_a != root_b:
+            if self.rank[root_b] < self.rank[root_a]:
+                self.parent[root_b] = self.parent[root_a]
+                self.rank[root_a] += self.rank[root_b]
+            else:
+                self.parent[root_a] = self.parent[root_b]
+                self.rank[root_b] += self.rank[root_a]
+            self.cc -= 1
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        uf = UnionFind(n)
+
+        for a, b in edges:
+            uf.union(a, b)
+
+        return uf.cc
+
+# Alternative solution
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        self.graph = collections.defaultdict(list)
+
+        for a, b in edges:
+            self.graph[a].append(b)
+            self.graph[b].append(a)
+
+        self.visited = set()
+        connected_components = 0
+
+        for i in range(n):
+            if i not in self.visited:
+                self.dfs(i)
+                connected_components += 1
+
+        return connected_components
+            
+    def dfs(self, node):
+        self.visited.add(node)
+
+        for neighbor in self.graph[node]:
+            if neighbor not in self.visited:
+                self.dfs(neighbor)

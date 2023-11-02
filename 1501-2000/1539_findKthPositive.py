73dafd719713e13b69daeca277febd7f498f73e8
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-11-02 11:57:56 +0400

    Update 1539_findKthPositive.py

--- 1501-2000/1539_findKthPositive.py
+++ 1501-2000/1539_findKthPositive.py
@@ -15,3 +15,19 @@ class Solution:
             else:
                 right = mid - 1
         return last + k - missing
+
+# Alternative solution
+
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        i = 0
+        num = 0
+
+        while i < len(arr) and k:
+            if arr[i] == num + 1:
+                i += 1
+            else:
+                k -= 1
+            num += 1
+
+        return num + k

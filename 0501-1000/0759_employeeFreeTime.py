16daa0061648e16824e7d8e62fc3794fceab0856
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-11-08 21:07:31 +0400

    Update 0759_employeeFreeTime.py

--- 0501-1000/0759_employeeFreeTime.py
+++ 0501-1000/0759_employeeFreeTime.py
@@ -31,3 +31,53 @@ class Solution:
             freeTime = Interval(start, end)
             freeIntervals.append(freeTime)
         return freeIntervals
+
+# Alternative solution
+
+class Solution:
+    def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
+        merged_schedule = schedule[0]
+        output = []
+
+        for i in range(1, len(schedule)):
+            merged_schedule = self.merge_intervals(merged_schedule, schedule[i])
+            print([(i.start, i.end ) for i in merged_schedule])
+
+        for i in range(1, len(merged_schedule)):
+            output.append(Interval(merged_schedule[i-1].end, merged_schedule[i].start))
+
+        return output
+
+
+    def merge_intervals(self, first, second):
+        result = []
+        i = j = 0
+
+        while i < len(first) and j < len(second):
+            if first[i].start > second[j].end:
+                result.append(second[j])
+                j += 1
+            
+            elif first[i].end < second[j].start:
+                result.append(first[i])
+                i += 1
+            
+            elif first[i].end > second[j].end:
+                first[i].start = min(first[i].start, second[j].start)
+                first[i].end = max(first[i].end, second[j].end)
+                j += 1
+
+            else:
+                second[j].start = min(first[i].start, second[j].start)
+                second[j].end = max(first[i].end, second[j].end)
+                i += 1
+
+        while i < len(first):
+            result.append(first[i])
+            i += 1
+
+        while j < len(second):
+            result.append(second[j])
+            j += 1   
+
+        return result  

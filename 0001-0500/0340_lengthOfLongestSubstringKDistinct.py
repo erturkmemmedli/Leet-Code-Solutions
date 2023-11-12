726c36add89d686d4f0c7856a9689d5816ea50e8
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-11-12 11:56:36 +0400

    Update 0340_lengthOfLongestSubstringKDistinct.py

--- 0001-0500/0340_lengthOfLongestSubstringKDistinct.py
+++ 0001-0500/0340_lengthOfLongestSubstringKDistinct.py
@@ -20,3 +20,28 @@ class Solution:
                     left += 1
                 window[s[right]] = 1
         return longest
+
+# Alternative solution
+
+class Solution:
+    def lengthOfLongestSubstringKDistinct(self, s, k):
+        if not k:
+            return 0
+        
+        window = {}
+        longest = 0
+        i = j = 0
+        
+        while i < len(s):
+            while len(window) == k and s[i] not in window:
+                window[s[j]] -= 1
+                if not window[s[j]]:
+                    del window[s[j]]
+                j += 1
+                
+            
+            window[s[i]] = window.get(s[i], 0) + 1
+            longest = max(longest, i - j + 1)
+            i += 1
+            
+        return longest

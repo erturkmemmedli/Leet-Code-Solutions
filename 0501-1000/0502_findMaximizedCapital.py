cc9bcebc8b6852058b0140292b3200f303705e1f
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-12-04 14:46:59 +0400

    Update 0502_findMaximizedCapital.py

--- 0501-1000/0502_findMaximizedCapital.py
+++ 0501-1000/0502_findMaximizedCapital.py
@@ -39,3 +39,25 @@ class Solution:
                 heappush(usableHeap, (-profit, capital))
 
         return w
+
+# Alternative solution
+
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        ipo = sorted([(c, p) for c, p in zip(capital, profits)], key = lambda x: [x[0], -x[1]])
+        heap = []
+        i = 0
+
+        while k:
+            if i < len(ipo) and ipo[i][0] <= w:
+                heappush(heap, -ipo[i][1])
+                i += 1
+                continue
+            
+            if not heap:
+                break
+
+            w -= heappop(heap)
+            k -= 1
+                        
+        return w

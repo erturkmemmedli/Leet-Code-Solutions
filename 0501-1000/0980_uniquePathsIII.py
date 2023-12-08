24b8836dbe8d42b83afcd701a45e12e1b6315e88
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-12-08 15:07:10 +0400

    Update 0980_uniquePathsIII.py

--- 0501-1000/0980_uniquePathsIII.py
+++ 0501-1000/0980_uniquePathsIII.py
@@ -28,3 +28,37 @@ class Solution:
                 if 0 <= row < m and 0 <= col < n and grid[row][col] not in visited and grid[row][col] != -1:
                     self.dfs((row,col), end, total, visited, grid, m, n)
             visited.discard(start)
+
+# Alternative solution
+
+class Solution:
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        row = col = None
+        obs = 0
+
+        for i in range(m):
+            for j in range(n):                
+                if grid[i][j] == 1:
+                    row, col = i, j 
+                if grid[i][j] == -1:
+                    obs += 1
+
+        self.result = 0
+        self.dfs(grid, m, n, row, col, obs)
+        return self.result
+
+    def dfs(self, grid, m, n, i, j, count):
+        if grid[i][j] == 2:
+            if count == m * n - 1:
+                self.result += 1
+            return
+
+        temp = grid[i][j]
+        grid[i][j] = -1
+
+        for row, col in (i-1, j), (i, j-1), (i+1, j), (i, j+1):
+            if m > row >= 0 <= col < n and grid[row][col] != -1:
+                self.dfs(grid, m, n, row, col, count + 1)
+
+        grid[i][j] = temp

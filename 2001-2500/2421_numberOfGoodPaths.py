63994daa82344b9a87023ac5f00dfee5f17058cb
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-01-20 11:33:13 +0400

    Update 2421_numberOfGoodPaths.py

--- 2001-2500/2421_numberOfGoodPaths.py
+++ 2001-2500/2421_numberOfGoodPaths.py
@@ -1,3 +1,53 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+    
+    def find(self, node):
+        while node != self.parent[node]:
+            node = self.parent[node]
+        return node
+
+    def union(self, node_a, node_b):
+        root_a = self.find(node_a)
+        root_b = self.find(node_b)
+        if self.rank[root_a] < self.rank[root_b]:
+            self.parent[root_a] = root_b
+        if self.rank[root_a] == self.rank[root_b]:
+            self.parent[root_b] = root_a
+            self.rank[root_a] += 1
+        if self.rank[root_a] > self.rank[root_b]:
+            self.parent[root_b] = root_a
+
+class Solution:
+    def numberOfGoodPaths(self, vals: List[int], edges: List[List[int]]) -> int:
+        uf = UnionFind(len(vals))
+        graph = defaultdict(list)
+        val_map = defaultdict(set)
+        num_of_good_paths = len(vals)
+
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+
+        for i, val in enumerate(vals):
+            val_map[val].add(i)
+        
+        for val in sorted(val_map.keys()):
+            for node in val_map[val]:
+                for neighbor in graph[node]:
+                    if vals[neighbor] <= val:
+                        uf.union(node, neighbor)
+
+            connected_components = defaultdict(int)
+            for node in val_map[val]:
+                connected_components[uf.find(node)] += 1
+
+            for root, count in connected_components.items():
+                num_of_good_paths += count * (count - 1) // 2
+
+        return num_of_good_paths
+
 # Alternative solution (which gives TLE error)
 
 class Solution:

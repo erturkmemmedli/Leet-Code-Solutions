e6c85dfca2ca4925d935559db0295eb1a51dd073
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-02-02 19:17:22 +0400

    Update 0140_wordBreak.py

--- 0001-0500/0140_wordBreak.py
+++ 0001-0500/0140_wordBreak.py
@@ -59,3 +59,72 @@ class Solution:
                 queue.append((string[index:], words + [string[:index]]))
 
         return result
+
+# Alternative solution
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+
+            node = node.children[char]
+
+        node.is_end = True
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        trie = Trie()
+
+        for word in wordDict:
+            trie.insert(word)
+
+        self.sentences = []
+        self.search(trie.root, trie.root, s, [])
+        return [" ".join(sentence) for sentence in self.sentences]
+
+    def search(self, root, node, word, path):
+        if not word:
+            if path:
+                self.sentences.append(path)
+            return
+
+        for i, char in enumerate(word):
+            if char not in node.children:
+                return
+            
+            node = node.children[char]
+
+            if node.is_end:
+                self.search(root, root, word[i+1:], path + [word[:i+1]])
+
+# Alternative solution
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        wordDict = set(wordDict)
+        output = []
+
+        def dp(s, sentence):
+            if not s:
+                output.append(sentence)
+                return
+
+            for i in range(len(s)):
+                head = s[:i+1]
+                tail = s[i+1:]
+                if head in wordDict:
+                    dp(tail, sentence + " " + head if sentence else head)
+
+        dp(s, "")
+        return output

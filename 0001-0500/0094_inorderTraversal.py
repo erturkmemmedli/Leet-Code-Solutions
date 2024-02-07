f87601dcbe359ca9d7c6d3d496250ed9877e12e7
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-02-07 20:10:34 +0400

    Update 0094_inorderTraversal.py

--- 0001-0500/0094_inorderTraversal.py
+++ 0001-0500/0094_inorderTraversal.py
@@ -60,3 +60,32 @@ class Solution:
                 else:
                     stack.extend([(node.right, False), (node, True), (node.left, False)])
         return inorderList
+
+# Alternative solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = [(root, False)]
+        inorder = []
+
+        while stack:
+            node, flag = stack.pop()
+
+            if not node:
+                continue
+
+            if not flag:
+                stack.append((node.right, flag))
+                stack.append((node, not flag))
+                stack.append((node.left, flag))
+            
+            else:
+                inorder.append(node.val)
+
+        return inorder

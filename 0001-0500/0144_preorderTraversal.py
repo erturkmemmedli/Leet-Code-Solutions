4fe1a7a3f23352577ccd6d6bd0c9f36862fc1684
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-02-07 20:11:21 +0400

    Update 0144_preorderTraversal.py

--- 0001-0500/0144_preorderTraversal.py
+++ 0001-0500/0144_preorderTraversal.py
@@ -28,3 +28,26 @@ class Solution1:
             preorder(node.right)
         preorder(root)
         return L
+
+# Alternative solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = [root]
+        preorder = []
+
+        while stack:
+            node = stack.pop()
+
+            if node:
+                preorder.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+            
+        return preorder

3a02d03cdfa40bbee00f9978cb6aaec9125644ce
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-02-07 20:11:53 +0400

    Update 0145_postorderTraversal.py

--- 0001-0500/0145_postorderTraversal.py
+++ 0001-0500/0145_postorderTraversal.py
@@ -15,3 +15,32 @@ class Solution:
         self.postorderTraversal(root.right)
         self.List.append(root.val)
         return self.List
+
+# Alternative solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = [(root, False)]
+        postorder = []
+
+        while stack:
+            node, flag = stack.pop()
+
+            if not node:
+                continue
+            
+            if not flag:
+                stack.append((node, not flag))
+                stack.append((node.right, flag))
+                stack.append((node.left, flag))
+
+            else:
+                postorder.append(node.val)
+            
+        return postorder

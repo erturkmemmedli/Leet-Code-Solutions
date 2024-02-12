2a83f8f0152ad65138fb3e6cffc98de2cd694e57
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-02-12 18:45:09 +0400

    Update 2437_countTime.py

--- 2001-2500/2437_countTime.py
+++ 2001-2500/2437_countTime.py
@@ -28,3 +28,45 @@ class Solution:
             if time[4] == '?':
                 answer *= 10
         return answer
+
+# Alternative solution
+
+class Solution:
+    def countTime(self, time: str) -> int:
+        hour, minute = time.split(':')
+
+        hour_front = hour[0]
+        hour_end = hour[1]
+        minute_front = minute[0]
+        minute_end = minute[1]
+
+        if minute_front != '?' and minute_front in ['6', '7', '8', '9']:
+            return 0
+        if hour_front != '?' and hour_front not in ['0', '1', '2']:
+            return 0
+        if hour_front == '2' and hour_end not in ['?', '0', '1', '2', '3']:
+            return 0
+        
+        count = 1
+
+        if hour_front == '?':
+            if hour_end == '?':
+                count *= 24
+            elif hour_end in ['0', '1', '2', '3']:
+                count *= 3
+            else:
+                count *= 2
+        
+        if hour_end == '?':
+            if hour_front in ['0', '1']:
+                count *= 10
+            elif hour_front == '2':
+                count *= 4
+
+        if minute_front == '?':
+            count *= 6
+
+        if minute_end == '?':
+            count *= 10
+
+        return count

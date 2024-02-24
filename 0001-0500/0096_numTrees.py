0e082c76ab1dfb789e28461c9ec4b692b3c42847
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-02-25 01:06:19 +0400

    Update 0096_numTrees.py

--- 0001-0500/0096_numTrees.py
+++ 0001-0500/0096_numTrees.py
@@ -14,3 +14,35 @@ class Solution:
                 right -= 1
             dp.append(addition)
         return dp[-1]
+
+# Alternative solution
+
+class Solution:
+    def numTrees(self, n: int) -> int:
+        memo = {}
+
+        def dp(n):
+            if n == 0:
+                return 1
+            
+            if n in memo:
+                return memo[n]
+
+            res = 0
+
+            for i in range(n):
+                if i != n - i - 1:
+                    res += dp(i) * dp(n - i - 1)
+                else:
+                    res += dp(i) ** 2
+            
+            memo[n] = res
+            return res
+        
+        return dp(n)
+
+# Alternative solution
+
+class Solution:
+    def numTrees(self, n: int) -> int:
+        return factorial(2 * n) // factorial(n + 1) // factorial(n)

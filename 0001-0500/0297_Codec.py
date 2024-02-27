c8e05cfe2a12c7672f4518e0b7ae1cf2c4b144c2
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-02-27 22:17:47 +0400

    Update 0297_Codec.py

--- 0001-0500/0297_Codec.py
+++ 0001-0500/0297_Codec.py
@@ -74,3 +74,68 @@ class Codec:
 # ser = Codec()
 # deser = Codec()
 # ans = deser.deserialize(ser.serialize(root))
+
+# Alternative solution
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        queue = deque([root])
+        tree = ""
+
+        while queue:
+            size = len(queue)
+            have_non_null = False
+
+            for i in range(size):
+                node = queue.popleft()
+                if node == None:
+                    tree += "*" if not tree else "#*"
+                else:
+                    tree += str(node.val) if not tree else "#" + str(node.val)
+                    if node.left:
+                        queue.append(node.left)
+                        have_non_null = True
+                    elif not node.left:
+                        queue.append(None)
+                    if node.right:
+                        queue.append(node.right)
+                        have_non_null = True
+                    elif not node.right:
+                        queue.append(None)
+                    
+            if not have_non_null:
+                break
+
+        return tree
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if data == '*':
+            return
+
+        data = data.split('#')
+        root = TreeNode(int(data[0]))
+        queue = deque([root])
+        idx = 1
+
+        while queue:
+            node = queue.popleft()
+            if idx < len(data) and data[idx] != '*':
+                node.left = TreeNode(int(data[idx]))
+                queue.append(node.left)
+            if idx + 1 < len(data) and data[idx + 1] != '*':
+                node.right = TreeNode(int(data[idx + 1]))
+                queue.append(node.right)
+            idx += 2
+
+        return root

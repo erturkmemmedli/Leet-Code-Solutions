a2e26fdc96ebb98b30eddd6af3a9be486bdb63be
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-02-27 15:06:31 +0400

    Update 0132_minCut.py

--- 0001-0500/0132_minCut.py
+++ 0001-0500/0132_minCut.py
@@ -32,3 +32,27 @@ class Solution:
 
         self.check[(left, right)] = self.isPalindrome(s, left + 1, right - 1)
         return self.check[(left, right)]
+
+# Alternative solution
+
+class Solution:
+    def minCut(self, s: str) -> int:
+        @lru_cache(None)
+        def is_palindrome(left, right):
+            if left >= right:
+                return True
+            if s[left] != s[right]:
+                return False
+            return is_palindrome(left + 1, right - 1)
+
+        @lru_cache(None)
+        def dp(idx):
+            if idx == len(s):
+                return 0
+            minimum = float('inf')
+            for i in range(idx, len(s)):
+                if is_palindrome(idx, i):
+                    minimum = min(minimum, dp(i + 1) + 1)
+            return minimum
+
+        return dp(0) - 1

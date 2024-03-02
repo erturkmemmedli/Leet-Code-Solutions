66e2d87bcc142feb93b2840f6b0939bb927a8c7e
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-03-02 22:38:06 +0400

    Update 1129_shortestAlternatingPaths.py

--- 1001-1500/1129_shortestAlternatingPaths.py
+++ 1001-1500/1129_shortestAlternatingPaths.py
@@ -23,3 +23,75 @@ class Solution:
             minimum = min(distance[i])
             distance[i] = minimum if minimum != float('inf') else -1
         return distance
+
+# Alternative solution
+
+class Solution:
+    def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
+        graph = [[] for _ in range(n)]
+
+        for a, b in redEdges:
+            graph[a].append((b, 'red'))
+        
+        for a, b in blueEdges:
+            graph[a].append((b, 'blue'))
+
+        answer = [None] * n
+
+        def dijkstra(source, destination):
+            heap = [(0, source, None)]
+            visited = set()
+
+            while heap:
+                distance, node, state = heappop(heap)
+                visited.add((node, state))
+
+                if node == destination:
+                    return distance
+
+                for neighbor, color in graph[node]:
+                    if color != state and (neighbor, color) not in visited:
+                        heappush(heap, (distance + 1, neighbor, color))
+
+            return -1
+
+        for i in range(n):
+            answer[i] = dijkstra(0, i)
+
+        return answer
+
+# Alternative solution
+
+class Solution:
+    def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
+        # contruct graph with n nodes
+            # key in graph -> neighbors (blue, red)
+            # bfs or dfs
+            # in each step, we must be in different color
+
+        graph = {i : [] for i in range(n)}
+
+        for node1, node2 in redEdges:
+            graph[node1].append((node2, 'red'))
+        
+        for node1, node2 in blueEdges:
+            graph[node1].append((node2, 'blue'))
+
+        # shortest path from 0 to each node
+            # dijkstra (heap)
+
+        shortest_paths = [-1] * n
+        heap = [(0, 0, None)]
+        distance = [[float('inf'), float('inf')] for _ in range(n)]
+        distance[0] = [0, 0]
+
+        while heap:
+            dist, node, curr_color = heappop(heap)
+            shortest_paths[node] = min(distance[node])
+            
+            for neighbor, color in graph[node]:
+                if color != curr_color and distance[neighbor][0 if color == 'red' else 1] > dist + 1:
+                    distance[neighbor][0 if color=='red' else 1] = dist + 1
+                    heappush(heap, (dist + 1, neighbor, color))
+
+        return shortest_paths

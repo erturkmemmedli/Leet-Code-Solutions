c39eeb7e96f3865dde64505abd9418f4930a295a
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-03-04 14:49:57 +0400

    Update 0715_RangeModule.py

--- 0501-1000/0715_RangeModule.py
+++ 0501-1000/0715_RangeModule.py
@@ -32,3 +32,34 @@ class RangeModule:
 # obj.addRange(left,right)
 # param_2 = obj.queryRange(left,right)
 # obj.removeRange(left,right)
+
+# Alternative solution
+
+from sortedcontainers import SortedList
+
+class RangeModule:
+    def __init__(self):
+        self.track = SortedList()
+
+    def addRange(self, left: int, right: int) -> None:
+        start = self.track.bisect_left(left)
+        end = self.track.bisect_right(right)
+        for _ in range(start, end):
+            # because each time we pop same index when we pop starting index
+            self.track.pop(start)
+        if start % 2 == 0: self.track.add(left)
+        if end % 2 == 0: self.track.add(right)
+
+    def queryRange(self, left: int, right: int) -> bool:
+        start = self.track.bisect_right(left)
+        end = self.track.bisect_left(right)
+        return start == end and start % 2
+
+    def removeRange(self, left: int, right: int) -> None:
+        start = self.track.bisect_left(left)
+        end = self.track.bisect_right(right)
+        for _ in range(start, end):
+            # because each time we pop same index when we pop starting index
+            self.track.pop(start)
+        if start % 2 == 1: self.track.add(left)
+        if end % 2 == 1: self.track.add(right) 

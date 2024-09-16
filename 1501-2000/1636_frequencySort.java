51d326b3011f8451e04e1c00cbb11f89e9ae92cf
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-09-16 23:56:02 +0400

    Update 1636_frequencySort.java

--- 1501-2000/1636_frequencySort.java
+++ 1501-2000/1636_frequencySort.java
@@ -26,3 +26,63 @@ class Solution {
         return nums;
     }
 }
+
+// Alternative solution
+
+class Tuple implements Comparator<Tuple> {
+    private int number;
+    private int count;
+
+    public Tuple(int number, int count) {
+        this.number = number;
+        this.count = count;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    @Override
+    public int compare(Tuple o1, Tuple o2) {
+        if (o1.count > o2.count || o1.count < o2.count) {
+            return o1.count - o2.count;
+        } else {
+            return o2.number - o1.number;
+        }
+    }
+}
+
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        HashMap<Integer, Integer> counter = new HashMap<>();
+        PriorityQueue<Tuple> pQueue = new PriorityQueue<>(new Tuple (Integer.MAX_VALUE, Integer.MIN_VALUE));
+
+        for (int num: nums) {
+            counter.put(num, counter.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry: counter.entrySet()) {
+            Integer number = entry.getKey();
+            Integer count = entry.getValue();
+
+            pQueue.add(new Tuple(number, count));
+        }
+
+        int i = 0;
+
+        while (!pQueue.isEmpty()) {
+            Tuple tuple = pQueue.poll();
+
+            for (int j = 0; j < tuple.getCount(); j++) {
+                nums[i] = tuple.getNumber();
+                i++;
+            }
+        }
+
+        return nums;
+    }
+}

b6a231d13ceb7a43708f23d74d6d052fbf59a8e7
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-09-16 23:29:03 +0400

    Create 0283_moveZeroes.java

---
+++ 0001-0500/0283_moveZeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        int j = 0;
+        int temp;
+
+        while (j < nums.length) {
+            if (nums[j] == 0) {
+                j++;
+            } else {
+                temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j++;
+            }
+        }
+    }
+}

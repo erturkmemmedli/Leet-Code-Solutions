c8ba9538c5a134590a77c546882ee3d6c88315f0
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-09-17 00:07:10 +0400

    Update 0092_reverseBetween.java

--- 0001-0500/0092_reverseBetween.java
+++ 0001-0500/0092_reverseBetween.java
@@ -57,3 +57,66 @@ class Solution {
         return prev;
     }
 }
+
+// Alternative solution
+
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode firstTail = null;
+        ListNode firstHead = head;
+
+        int current = 1;
+
+        while (firstHead != null) {
+            if (current < left ) {
+                firstTail = firstHead;
+                firstHead = firstHead.next;
+            } else {
+                break;
+            }
+            current++;
+        }
+
+        ListNode secondTail = firstTail;
+        ListNode secondHead = firstHead;
+
+        while (secondHead != null) {
+            if (current <= right) {
+                secondTail = secondHead;
+                secondHead = secondHead.next;
+            } else {
+                break;
+            }
+            current++;
+        }
+
+
+
+        secondTail.next = null;
+        ListNode reversed = reverseList(firstHead);
+        if (firstTail != null) {
+            firstTail.next = reversed;
+        } else {
+            head = reversed;
+        }
+        if (firstHead != secondHead) {
+            firstHead.next = secondHead;
+        }
+        return head;
+    }
+
+    public ListNode reverseList(ListNode node) {
+        ListNode prev = null;
+        ListNode curr = node;
+        ListNode next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+}

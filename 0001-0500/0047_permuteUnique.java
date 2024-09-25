bc0d99f69496f0da6b92771e154bc6fc2ca9c688
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-09-25 16:23:16 +0400

    Create 0047_permuteUnique.java

---
+++ 0001-0500/0047_permuteUnique.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> output = new ArrayList<>();
+        backtrack(nums, output, new ArrayList<>(), new boolean[nums.length]);
+        return output;
+    }
+    
+    public void backtrack(int[] nums, List<List<Integer>> output, List<Integer> path, boolean[] used) {
+        if (path.size() == nums.length) {
+            output.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i] && !used[i - 1]) {
+                continue;
+            }
+            if (used[i] == true) {
+                continue;
+            }
+            used[i] = true;
+            path.add(nums[i]);
+            backtrack(nums, output, path, used);
+            path.remove(path.size() - 1);
+            used[i] = false;
+        }
+    }
+}

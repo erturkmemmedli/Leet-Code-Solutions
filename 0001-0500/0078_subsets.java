55f91693327053c9a304ddcc100aaa1f15eca5fa
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-09-25 15:54:29 +0400

    Create 0078_subsets.java

---
+++ 0001-0500/0078_subsets.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> output = new ArrayList<>();
+        backtrack(nums, output, new ArrayList<>(), 0);
+        return output;
+    }
+
+    public void backtrack(int[] nums, List<List<Integer>> output, List<Integer> path, int index) {
+        output.add(new ArrayList<>(path));
+
+        if (index == nums.length) {
+            return;
+        }
+
+        for (int i = index; i < nums.length; i++) {
+            List<Integer> newPath = new ArrayList<>(path);
+            newPath.add(nums[i]);
+            backtrack(nums, output, newPath, i + 1);
+            newPath.remove(newPath.size() - 1); 
+        }
+    }
+}

2b4b8628da3dc8d4c0be1a30e33098526ffa7eb3
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2025-03-01 22:58:11 +0400

    Create 2460_applyOperations.py

---
+++ 2001-2500/2460_applyOperations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        for i in range(n - 1):
+            if nums[i] == nums[i + 1]:
+                nums[i] *= 2
+                nums[i + 1] = 0
+
+        i = j = 0
+        while j < n:
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+
+            if nums[i] != 0:
+                i += 1
+            j += 1
+                  
+        return nums

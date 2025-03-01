151fef6c23f952de948b7721150bae8d4e7e5808
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2025-03-02 00:53:51 +0400

    Create 3028_returnToBoundaryCount.py

---
+++ 3001-3500/3028_returnToBoundaryCount.py
@@ -0,0 +1,11 @@
+class Solution:
+    def returnToBoundaryCount(self, nums: List[int]) -> int:
+        count = 0
+        total = 0
+
+        for num in nums:
+            total += num
+            if total == 0:
+                count += 1
+                
+        return count

73e1ea81abe1e7ee812ad1325cbe1474e6ed2b54
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2025-03-07 18:52:14 +0400

    Create 2523_closestPrimes.py

---
+++ 2501-3000/2523_closestPrimes.py
@@ -0,0 +1,30 @@
+class Solution:
+    def closestPrimes(self, left: int, right: int) -> List[int]:
+        numbers = set(range(2, right + 1))
+
+        for i in range(2, int(right ** 0.5) + 1):
+            if i in numbers:
+                for j in range(i * i, right + 1, i):
+                    numbers.discard(j)
+        
+        primes = sorted(list(numbers))
+
+        for i in range(len(primes)):
+            if primes[i] < left:
+                continue
+            else:
+                primes = primes[i:]
+                break
+
+        if len(primes) < 2:
+            return [-1, -1]
+        
+        gap = math.inf
+        answer = [None, None]
+
+        for i in range(1, len(primes)):
+            if primes[i] - primes[i - 1] < gap:
+                gap = primes[i] - primes[i - 1]
+                answer = [primes[i - 1], primes[i]]
+
+        return answer

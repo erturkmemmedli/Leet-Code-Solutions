a4ff2ebc5a7b6a4290e776f738b8cb7359ad5aaf
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2025-03-08 23:06:59 +0400

    Create 1070_sales_analysis.py

---
+++ 1001-1500/1070_sales_analysis.py
@@ -0,0 +1,24 @@
+'''
+Pandas Schema:
+data = [[1, 100, 2008, 10, 5000], [2, 100, 2009, 12, 5000], [7, 200, 2011, 15, 9000]]
+sales = pd.DataFrame(data, columns=['sale_id', 'product_id', 'year', 'quantity', 'price']).astype({'sale_id':'Int64', 'product_id':'Int64', 'year':'Int64', 'quantity':'Int64', 'price':'Int64'})
+data = [[100, 'Nokia'], [200, 'Apple'], [300, 'Samsung']]
+product = pd.DataFrame(data, columns=['product_id', 'product_name']).astype({'product_id':'Int64', 'product_name':'object'})
+'''
+
+import pandas as pd
+
+def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
+    sales.sort_values(by=['year'], inplace=True)
+    sales.drop(columns=['sale_id'], inplace=True)
+    prod_id_set = {}
+    result = []
+
+    for idx, row in sales.iterrows():
+        if row['product_id'] not in prod_id_set or prod_id_set[row['product_id']] == row['year']:
+            prod_id_set[row['product_id']] = row['year']
+            result.append(row)
+    
+    result = pd.DataFrame(result)
+    result.rename(columns={"year": "first_year"}, inplace=True)
+    return result

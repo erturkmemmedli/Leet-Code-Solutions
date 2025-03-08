057a555db76de1dc5c93b0d113e71b25cc968255
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2025-03-08 23:24:44 +0400

    Create 1075_project_employees_i.py

---
+++ 1001-1500/1075_project_employees_i.py
@@ -0,0 +1,16 @@
+'''
+Pandas Schema:
+data = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 4]]
+project = pd.DataFrame(data, columns=['project_id', 'employee_id']).astype({'project_id':'Int64', 'employee_id':'Int64'})
+data = [[1, 'Khaled', 3], [2, 'Ali', 2], [3, 'John', 1], [4, 'Doe', 2]]
+employee = pd.DataFrame(data, columns=['employee_id', 'name', 'experience_years']).astype({'employee_id':'Int64', 'name':'object', 'experience_years':'Int64'})
+'''
+
+import pandas as pd
+
+def project_employees_i(project: pd.DataFrame, employee: pd.DataFrame) -> pd.DataFrame:
+    df = pd.merge(project, employee, on='employee_id', how='left')
+    df_final = df.groupby('project_id')[['project_id', 'experience_years']].agg('mean')
+    df_final.rename(columns={'experience_years': 'average_years'}, inplace=True)
+    df_final['average_years'] = df_final['average_years'].apply(lambda x: round(x, 2))
+    return df_final

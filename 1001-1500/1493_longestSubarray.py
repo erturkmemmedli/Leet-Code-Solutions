3ab7556fe87be472dd1192228241c58febd2f17f
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2025-03-16 16:13:04 +0400

    Update 1493_longestSubarray.py

--- 1001-1500/1493_longestSubarray.py
+++ 1001-1500/1493_longestSubarray.py
@@ -23,3 +23,23 @@ class Solution:
         for i in range(2, len(stack)):
             maximum = max(maximum, stack[i] + stack[i-2])
         return maximum
+
+# Alternative solution
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        zero_count = 0
+        left = 0
+        answer = 0
+
+        for right in range(len(nums)):
+            while zero_count == 1 and nums[right] == 0:
+                if nums[left] == 0:
+                    zero_count -= 1
+                left += 1
+
+            if nums[right] == 0:
+                zero_count += 1
+            answer = max(answer, right - left)
+        
+        return answer

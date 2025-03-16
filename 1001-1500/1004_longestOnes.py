9d077a482eef0d4ae930c45c4cd36e1b55b61f59
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2025-03-16 13:04:59 +0400

    Update 1004_longestOnes.py

--- 1001-1500/1004_longestOnes.py
+++ 1001-1500/1004_longestOnes.py
@@ -78,3 +78,26 @@ class Solution:
                     left += 1
         maxLength = max(maxLength, right - left + 1)    
         return maxLength
+
+# Alternative solution
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        start = 0
+        end = 0
+        answer = 0
+        
+        while end < len(nums):
+            if nums[end] == 1:
+                end += 1
+            elif k > 0:
+                k -= 1
+                end += 1
+            elif nums[start] == 0:
+                k += 1
+                start += 1
+            else:
+                start += 1
+            answer = max(answer, end - start)
+
+        return answer

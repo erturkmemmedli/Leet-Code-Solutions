e9324356584c5b06002841d97eebd9f21b794b58
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2025-03-18 17:17:40 +0400

    Update 2401_longestNiceSubarray.py

--- 2001-2500/2401_longestNiceSubarray.py
+++ 2001-2500/2401_longestNiceSubarray.py
@@ -1,15 +1,15 @@
 class Solution:
     def longestNiceSubarray(self, nums: List[int]) -> int:
         result = 0
-        AND = 0
+        window = 0
         start = 0
 
         for end, num in enumerate(nums):
-            while AND & num:
-                AND ^= nums[start]
+            while window & num:
+                window ^= nums[start]
                 start += 1
             
-            AND |= num
+            window |= num
             result = max(result, end - start + 1)
         
         return result

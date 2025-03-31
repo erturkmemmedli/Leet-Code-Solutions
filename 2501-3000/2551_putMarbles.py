66084cc1827c04f94aca9e39c8a75e06aa605467
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2025-03-31 14:05:23 +0400

    Update 2551_putMarbles.py

--- 2501-3000/2551_putMarbles.py
+++ 2501-3000/2551_putMarbles.py
@@ -7,3 +7,29 @@ class Solution:
         largest = sum(nlargest(k - 1, pairwised))
         smallest = sum(nsmallest(k - 1, pairwised))
         return largest - smallest
+
+# Alternative solution
+
+class Solution:
+    def putMarbles(self, weights: List[int], k: int) -> int:
+        large = []
+        small = []
+
+        for i in range(1, len(weights)):
+            heappush(small, weights[i] + weights[i - 1])
+            heappush(large, - weights[i] - weights[i - 1])
+
+        largest = 0
+        smallest = 0
+        step_small = k - 1
+        step_large = k - 1
+
+        while step_small > 0:
+            smallest += heappop(small)
+            step_small -= 1
+        
+        while step_large > 0:
+            largest += -heappop(large)
+            step_large -= 1
+
+        return largest - smallest

b718e1a6adb3852193eea7cf3bd3d36cc0db9bd1
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2025-06-09 17:41:27 +0400

    Update 3164_numberOfPairs.py

--- 3001-3500/3164_numberOfPairs.py
+++ 3001-3500/3164_numberOfPairs.py
@@ -2,9 +2,10 @@ class Solution:
     def numberOfPairs(self, nums1: List[int], nums2: List[int], k: int) -> int:
         frequence = Counter(num * k for num in nums2)
         counter = {}
+        max_range = max(nums1) + 1
 
         for num, count in frequence.items():
-            for val in range(num, max(nums1) + 1, num):
+            for val in range(num, max_range, num):
                 counter[val] = counter.get(val, 0) + count
 
         return sum(counter[num] for num in nums1 if num in counter)

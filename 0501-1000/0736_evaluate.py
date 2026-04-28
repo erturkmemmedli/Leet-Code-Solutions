db8ccd088d53465f82160fc9b5ddd6efd52de73b
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2026-04-29 00:16:23 +0400

    Update 0736_evaluate.py

--- 0501-1000/0736_evaluate.py
+++ 0501-1000/0736_evaluate.py
@@ -1,33 +1,32 @@
 class Solution:
     def evaluate(self, expression: str) -> int:
-        tokens, st, d = [''], [], {}
-        
+        stack = []
+        tokens = ['']
+        dictionary = {}
+
         def calculate(tokens):
             if tokens[0] in ['add', 'mult']:
-                n1, n2 = int(d.get(tokens[1], tokens[1])), int(d.get(tokens[2], tokens[2]))
-                return str(n1+n2) if tokens[0] == 'add' else str(n1*n2)
+                v1 = int(dictionary.get(tokens[1], tokens[1]))
+                v2 = int(dictionary.get(tokens[2], tokens[2]))
+                return str(v1+v2) if tokens[0] == 'add' else str(v1*v2)
             else:
                 for i in range(1, len(tokens)-2, 2):
-                    d[tokens[i]] = d.get(tokens[i+1], tokens[i+1])
-                return d.get(tokens[-1], tokens[-1]) 
-            
-        for c in expression:
-            if c == '(':
+                    dictionary[tokens[i]] = dictionary.get(tokens[i+1], tokens[i+1])
+                return dictionary.get(tokens[-1], tokens[-1])
+        
+        for char in expression:
+            if char == '(':
                 if tokens[0] == 'let':
                     calculate(tokens)
-                st.append((tokens, dict(d)))
+                stack.append([tokens, dict(dictionary)])
                 tokens = ['']
-                # print('case 1:','stack:',st,'tokens:',tokens,'dict:',d)
-            elif c == ')':
-                val = calculate(tokens)
-                tokens, d = st.pop()
-                tokens[-1] += val
-                # print('case 2:','stack:',st,'tokens:',tokens,'dict:',d)
-            elif c == ' ':
+            elif char == ')':
+                result = calculate(tokens)
+                tokens, dictionary = stack.pop()
+                tokens[-1] += result
+            elif char == ' ':
                 tokens.append('')
-                # print('case 3:','stack:',st,'tokens:',tokens,'dict:',d)
             else:
-                tokens[-1] += c
-                # print('case 4:','stack:',st,'tokens:',tokens,'dict:',d)
+                tokens[-1] += char
 
         return int(tokens[0])

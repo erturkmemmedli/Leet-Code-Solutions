9070859f0275c52d66c16c1fb73b2c533d1f3d40
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2026-05-03 17:58:49 +0400

    Update 0796_rotateString.py

--- 0501-1000/0796_rotateString.py
+++ 0501-1000/0796_rotateString.py
@@ -6,3 +6,19 @@ class Solution:
             s = s[-1] + s[:-1]
             if s == goal: return True
         return False
+
+# Alternative solution
+
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+        
+        n = len(s)
+        goal += goal
+        
+        for i in range(n):
+            if goal[i:i+n] == s:
+                return True
+            
+        return False

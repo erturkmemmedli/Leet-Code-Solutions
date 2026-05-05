c85d92057ef9ee676696d64842f75a7930f1ed79
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2026-05-05 19:25:29 +0400

    Update 0230_kthSmallest.py

--- 0001-0500/0230_kthSmallest.py
+++ 0001-0500/0230_kthSmallest.py
@@ -19,3 +19,26 @@ class Solution:
         if len(self.list) == k: return
         self.list.append(node.val)
         self.inorder(node.right, k)
+
+# Alternative solution
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        self.count_nodes(root)
+        return self.dfs(root, k)
+    
+    def count_nodes(self, root):
+        if not root:
+            return 0
+        root.count = 1 + self.count_nodes(root.left) + self.count_nodes(root.right)
+        return root.count
+
+    def dfs(self, root, k):
+        if not root.left and k - 1 == 0:
+            return root.val
+        if root.left and root.left.count == k - 1:
+            return root.val
+        if root.left and root.left.count > k - 1:
+            return self.dfs(root.left, k)
+        if root.right:
+            return self.dfs(root.right, k - root.count + root.right.count)

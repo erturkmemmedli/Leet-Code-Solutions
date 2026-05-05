c0afd0b64cf8d97c656428ff25fa25f7dc34dc35
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2026-05-05 17:25:41 +0400

    Update 1381_CustomStack.py

--- 1001-1500/1381_CustomStack.py
+++ 1001-1500/1381_CustomStack.py
@@ -43,3 +43,62 @@ class CustomStack1:
     def increment(self, k: int, val: int) -> None:
         if self.stack:
             self.inc[min(k, len(self.stack)) - 1] += val
+
+# Alternative solution
+
+class CustomStack:
+    def __init__(self, maxSize: int):
+        self.stack = []
+        self.capacity = maxSize
+
+    def push(self, x: int) -> None:
+        if self.capacity:
+            self.stack.append([x, 0])
+            self.capacity -= 1
+
+    def pop(self) -> int:
+        if not self.stack:
+            return -1
+        pop = self.stack.pop()
+        self.capacity += 1
+        if self.stack:
+            self.stack[-1][1] += pop[1]
+        return sum(pop)
+
+    def increment(self, k: int, val: int) -> None:
+        if self.stack:
+            self.stack[min(len(self.stack) - 1, k - 1)][1] += val
+
+# Alternative solution
+
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        self.stack = []
+        self.max_size = maxSize
+        self.heap = []
+
+    def push(self, x: int) -> None:
+        if len(self.stack) < self.max_size:
+            self.stack.append(x)
+
+    def pop(self) -> int:
+        if not self.stack:
+            return -1
+        
+        heap_val = 0
+        if self.heap:
+            heap_idx = -self.heap[0][0]
+            if heap_idx == len(self.stack):
+                while self.heap and self.heap[0][0] == -heap_idx:
+                    heap_val += heappop(self.heap)[1]
+                if self.heap and self.heap[0][0] - 1 == -heap_idx:
+                    self.heap[0][1] += heap_val
+                else:
+                    heappush(self.heap, [-heap_idx + 1, heap_val])
+
+        val = self.stack.pop() + heap_val
+        return val
+
+    def increment(self, k: int, val: int) -> None:
+        heappush(self.heap, [max(-k, -len(self.stack)), val])

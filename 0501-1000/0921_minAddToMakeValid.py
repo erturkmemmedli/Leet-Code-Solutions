432a413ce806dbb02975023326458922d7c0fba9
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2026-05-18 16:46:33 +0400

    Update 0921_minAddToMakeValid.py

--- 0501-1000/0921_minAddToMakeValid.py
+++ 0501-1000/0921_minAddToMakeValid.py
@@ -12,3 +12,19 @@ class Solution:
                     moves += 1
         moves += len(stack)
         return moves
+
+# Alternative solution
+
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        stack = 0
+        count = 0
+        for char in s:
+            if char == '(':
+                stack += 1
+            elif stack:
+                stack -= 1
+            else:
+                count += 1
+        count += stack
+        return count

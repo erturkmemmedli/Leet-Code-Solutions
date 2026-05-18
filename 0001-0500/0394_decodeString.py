cb7ad862c932b5f675bb4a5dc5b671af569dc78b
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2026-05-18 19:34:42 +0400

    Update 0394_decodeString.py

--- 0001-0500/0394_decodeString.py
+++ 0001-0500/0394_decodeString.py
@@ -20,3 +20,23 @@ class Solution:
                 i = j - l - 1 + m * (i - j - 1)
             i += 1
         return s
+
+# Alternative solution
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = [["", ""]]
+        for i, char in enumerate(s):
+            if char.isdigit():
+                if i == 0 or not s[i-1].isdigit():
+                    stack.append(["", ""])
+                stack[-1][0] += char
+            elif char.isalpha():
+                stack[-1][1] += char
+            elif char == ']':
+                m, t = stack.pop()
+                if stack:
+                    stack[-1][1] += t * int(m)
+                else:
+                    stack = [["", ""]]
+        return stack[0][1]
